ee58cec4fa4bf9e499c84bda04a85269159568f4
Sergey Edunov <edunov@apache.org>
2017-09-14 15:42:22 -0700

    Fix test case (#85)

--- tests/test_label_smoothing.py
+++ tests/test_label_smoothing.py
@@ -8,26 +8,27 @@
 
 import torch
 import unittest
-from modules import label_smoothed_cross_entropy
+from fairseq.criterions.label_smoothed_cross_entropy import LabelSmoothedCrossEntropy
 from torch.autograd import Variable, gradcheck
 
 
 torch.set_default_tensor_type('torch.DoubleTensor')
 
 
-class TestFConv(unittest.TestCase):
+class TestLabelSmoothing(unittest.TestCase):
 
     def test_label_smoothing(self):
         input = Variable(torch.randn(3, 5), requires_grad=True)
         idx = torch.rand(3) * 4
         target = Variable(idx.long())
+        criterion = LabelSmoothedCrossEntropy()
         self.assertTrue(gradcheck(
-            lambda x, y: label_smoothed_cross_entropy(x, y, eps=0.1, padding_idx=2), (input, target)
+            lambda x, y: criterion.apply(x, y, 0.1, 2, None), (input, target)
         ))
         weights = torch.ones(5)
         weights[2] = 0
-        self.assertTrue(gradcheck(lambda x, y: label_smoothed_cross_entropy(x, y, weights=weights), (input, target)))
-        self.assertTrue(gradcheck(lambda x, y: label_smoothed_cross_entropy(x, y), (input, target)))
+        self.assertTrue(gradcheck(lambda x, y: criterion.apply(x, y, 0.1, None, weights), (input, target)))
+        self.assertTrue(gradcheck(lambda x, y: criterion.apply(x, y, 0.1, None, None), (input, target)))
 
 
 if __name__ == '__main__':
