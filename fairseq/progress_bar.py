c48ecd2d0343bd073580ee8a4d18e72e3c7b1713
Myle Ott <myleott@fb.com>
2017-11-12 15:59:54 -0700

    Flush non-TTY logging output after each log interval

--- fairseq/progress_bar.py
+++ fairseq/progress_bar.py
@@ -90,7 +90,7 @@ class json_progress_bar(progress_bar):
                     self.log_interval is not None and i % self.log_interval == 0:
                 update = self.epoch + float(i / size) if self.epoch is not None else None
                 stats = self._format_stats(self.stats, epoch=self.epoch, update=update)
-                print("sweep_log: " + json.dumps(stats))
+                print("sweep_log: " + json.dumps(stats), flush=True)
 
     def log(self, stats):
         """Log intermediate stats according to log_interval."""
@@ -99,7 +99,7 @@ class json_progress_bar(progress_bar):
     def print(self, stats):
         """Print end-of-epoch stats."""
         stats = self._format_stats(self.stats, epoch=self.epoch)
-        print("sweep_log: " + json.dumps(stats))
+        print("sweep_log: " + json.dumps(stats), flush=True)
 
     def _format_stats(self, stats, epoch=None, update=None):
         postfix = OrderedDict()
@@ -152,8 +152,7 @@ class simple_progress_bar(progress_bar):
             if self.stats is not None and i > 0 and \
                     self.log_interval is not None and i % self.log_interval == 0:
                 postfix = self._str_commas(self.stats)
-                print(f'{self.prefix}:  {i:5d} / {size:d} {postfix}')
-                sys.stdout.flush()
+                print(f'{self.prefix}:  {i:5d} / {size:d} {postfix}', flush=True)
 
     def log(self, stats):
         """Log intermediate stats according to log_interval."""
@@ -162,8 +161,7 @@ class simple_progress_bar(progress_bar):
     def print(self, stats):
         """Print end-of-epoch stats."""
         postfix = self._str_pipes(self._format_stats(stats))
-        print(f'{self.prefix} | {postfix}')
-        sys.stdout.flush()
+        print(f'{self.prefix} | {postfix}', flush=True)
 
 
 class tqdm_progress_bar(progress_bar):
