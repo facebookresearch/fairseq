762956a559e65e1e48df8f8b4df515d23b66fddb
Mehdi Drissi <mdrissi@hmc.edu>
2018-06-21 10:55:44 -0700

    Two tiny changes to train/eval_lm. For train fix an off by one, while for eval_lm make it work when the task is translation'

--- train.py
+++ train.py
@@ -82,7 +82,7 @@ def main(args):
     train_meter.start()
     valid_losses = [None]
     valid_subsets = args.valid_subset.split(',')
-    while lr > args.min_lr and epoch_itr.epoch <= max_epoch and trainer.get_num_updates() < max_update:
+    while lr > args.min_lr and epoch_itr.epoch < max_epoch and trainer.get_num_updates() < max_update:
         # train for one epoch
         train(args, trainer, task, epoch_itr)
 
