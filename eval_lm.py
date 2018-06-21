Two tiny changes to train/eval_lm. For train fix an off by one, while for eval_lm make it work when the task is translation'

eval_lm.py
@@ -17,8 +17,7 @@ from fairseq.sequence_scorer import SequenceScorer
 def main(args):
     assert args.path is not None, '--path required for evaluation!'
 
-    if args.tokens_per_sample is None:
-        args.tokens_per_sample = 1024
+    args.tokens_per_sample = getattr(args, 'tokens_per_sample', 1024)
     print(args)
 
     use_cuda = torch.cuda.is_available() and not args.cpu