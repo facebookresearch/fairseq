14036b6fa1c303c9f0891a723a5dc0b1c5a969c7
Myle Ott <myleott@fb.com>
2018-08-10 10:34:29 -0700

    Diverse Beam Search

--- fairseq/options.py
+++ fairseq/options.py
@@ -305,6 +305,10 @@ def add_generation_args(parser):
                        help='sample from top K likely next words instead of all words')
     group.add_argument('--sampling-temperature', default=1, type=float, metavar='N',
                        help='temperature for random sampling')
+    group.add_argument('--diverse-beam-groups', default=1, type=int, metavar='N',
+                       help='number of groups for Diverse Beam Search')
+    group.add_argument('--diverse-beam-strength', default=0.5, type=float, metavar='N',
+                       help='strength of diversity penalty for Diverse Beam Search')
     group.add_argument('--print-alignment', action='store_true',
                        help='if set, uses attention feedback to compute and print alignment to source tokens')
     group.add_argument('--model-overrides', default="{}", type=str, metavar='DICT',
