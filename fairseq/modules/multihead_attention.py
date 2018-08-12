alternate padding softmax fix

fairseq/modules/multihead_attention.py
@@ -174,11 +174,7 @@ class MultiheadAttention(nn.Module):
             ).type_as(attn_weights)  # FP16 support: cast to float and back
             attn_weights = attn_weights.view(bsz * self.num_heads, tgt_len, src_len)
 
-        if self.add_zero_attn:
-            attn_weights = torch.cat([attn_weights, attn_weights.new_zeros(attn_weights.size()[:-1] + (1,))], dim=-1)
         attn_weights = F.softmax(attn_weights.float(), dim=-1).type_as(attn_weights)
-        if self.add_zero_attn:
-            attn_weights = attn_weights[:,:,:-1]
         attn_weights = F.dropout(attn_weights, p=self.dropout, training=self.training)
 
         attn = torch.bmm(attn_weights, v)
@@ -305,6 +301,15 @@ class BidirectionalMultiheadSelfAttention(nn.Module):
         q = q.contiguous().view(tgt_len, bsz * self.num_heads, self.head_dim).transpose(0, 1)
         k = k.contiguous().view(src_len, bsz * self.num_heads, self.head_dim).transpose(0, 1)
         v = v.contiguous().view(src_len, bsz * self.num_heads, self.head_dim).transpose(0, 1)
+
+        if self.add_zero_attn:
+            src_len += 1
+            k = torch.cat([k, k.new_zeros((k.size(0), 1) + k.size()[2:])], dim=1)
+            v = torch.cat([v, v.new_zeros((v.size(0), 1) + v.size()[2:])], dim=1)
+            if attn_mask is not None:
+                attn_mask = torch.cat([attn_mask, attn_mask.new_zeros(attn_mask.size(0), 1)], dim=1)
+            if key_padding_mask is not None:
+                key_padding_mask = torch.cat([key_padding_mask, key_padding_mask.new_zeros(key_padding_mask.size(0), 1)], dim=1)
 
         attn_weights = torch.bmm(q, k.transpose(1, 2))
         assert list(attn_weights.size()) == [bsz * self.num_heads, tgt_len, src_len]