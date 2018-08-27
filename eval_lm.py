c7c567a7750605f2168af4d7ed5af918a3cd6702
Alexei Baevski <alexei.b@gmail.com>
2018-08-26 18:43:14 -0700

    word stats in eval_lm

--- eval_lm.py
+++ eval_lm.py
@@ -14,6 +14,21 @@ from fairseq.meters import StopwatchMeter, TimeMeter
 from fairseq.sequence_scorer import SequenceScorer
 
 
+class WordStat(object):
+    def __init__(self, word, is_bpe):
+        self.word = word
+        self.is_bpe = is_bpe
+        self.log_prob = 0
+        self.count = 0
+
+    def add(self, log_prob):
+        self.log_prob += log_prob
+        self.count += 1
+
+    def __str__(self):
+        return '{}\t{}\t{}\t{}'.format(self.word, self.count, self.log_prob / self.count, self.is_bpe)
+
+
 def main(parsed_args):
     assert parsed_args.path is not None, '--path required for evaluation!'
 
@@ -70,6 +85,8 @@ def main(parsed_args):
         bpe_toks = None
         bpe_len = 0
 
+    word_stats = dict()
+
     with progress_bar.build_progress_bar(args, itr) as t:
         results = scorer.score_batched_itr(t, cuda=use_cuda, timer=gen_timer)
         wps_meter = TimeMeter()
@@ -93,18 +110,23 @@ def main(parsed_args):
                 score_sum += pos_scores.sum()
                 count += pos_scores.numel() - skipped_toks
 
-                if args.output_word_probs:
+                if args.output_word_probs or args.output_word_stats:
                     w = ''
                     word_prob = []
+                    is_bpe = False
                     for i in range(len(hypo['tokens'])):
                         w_ind = hypo['tokens'][i].item()
                         w += task.dictionary[w_ind]
                         if bpe_toks is not None and w_ind in bpe_toks:
                             w = w[:-bpe_len]
+                            is_bpe = True
                         else:
                             word_prob.append((w, pos_scores[i].item()))
+                            word_stats.setdefault(w, WordStat(w, is_bpe)).add(pos_scores[i].item())
+                            is_bpe = False
                             w = ''
-                    print('\t'.join('{} [{:2f}]'.format(x[0], x[1]) for x in word_prob))
+                    if args.output_word_probs:
+                        print('\t'.join('{} [{:2f}]'.format(x[0], x[1]) for x in word_prob))
 
             wps_meter.update(src_tokens.size(0))
             t.log({'wps': round(wps_meter.avg)})
@@ -112,6 +134,10 @@ def main(parsed_args):
     avg_nll_loss = -score_sum / count
     print('| Evaluated {} tokens in {:.1f}s ({:.2f} tokens/s)'.format(gen_timer.n, gen_timer.sum, 1. / gen_timer.avg))
     print('| Loss: {:.4f}, Perplexity: {:.2f}'.format(avg_nll_loss, np.exp(avg_nll_loss)))
+
+    if args.output_word_stats:
+        for ws in sorted(word_stats.values(), key=lambda x: x.count, reverse=True):
+            print(ws)
 
 
 if __name__ == '__main__':
