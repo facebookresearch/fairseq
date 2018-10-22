09e0599b4058540462af1543961ad086b01a486d
alexeib <alexei.b@gmail.com>
2018-10-21 18:30:52 -0700

    fix merge

--- fairseq/criterions/fairseq_criterion.py
+++ fairseq/criterions/fairseq_criterion.py
@@ -30,8 +30,8 @@ class FairseqCriterion(_Loss):
         """
         raise NotImplementedError
 
-
-    def aggregate_logging_outputs(self, logging_outputs):
+    @staticmethod
+    def aggregate_logging_outputs(logging_outputs):
         """Aggregate logging outputs from data parallel training."""
         raise NotImplementedError
 
