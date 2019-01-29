bf0fec07c5e9217914be36432562e829d1dd9285
Myle Ott <myleott@fb.com>
2019-01-28 19:54:49 -0800

    Style

--- fairseq/utils.py
+++ fairseq/utils.py
@@ -438,14 +438,12 @@ def resolve_max_positions(*args):
 
 
 def import_user_module(args):
-    if hasattr(args, 'user_dir'):
-        module_path = args.user_dir
+    module_path = getattr(args, 'user_dir', None)
+    if module_path is not None:
+        module_path = os.path.abspath(args.user_dir)
+        module_parent, module_name = os.path.split(module_path)
 
-        if module_path is not None:
-            module_path = os.path.abspath(args.user_dir)
-            module_parent, module_name = os.path.split(module_path)
-
-            if module_name not in sys.modules:
-                sys.path.insert(0, module_parent)
-                importlib.import_module(module_name)
-                sys.path.pop(0)
+        if module_name not in sys.modules:
+            sys.path.insert(0, module_parent)
+            importlib.import_module(module_name)
+            sys.path.pop(0)
