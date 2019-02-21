73a8bb2346e8340c2a89e7ca99e017f446897eb4
Myle Ott <myleott@fb.com>
2019-02-21 07:59:58 -0800

    Fix lint

--- examples/translation/prepare-iwslt17-multilingual.sh
+++ examples/translation/prepare-iwslt17-multilingual.sh
@@ -1,10 +1,9 @@
+#!/bin/bash
 # Copyright (c) Facebook, Inc. and its affiliates.
 # All rights reserved.
 #
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
-#
-#!/bin/bash
 
 SRCS=(
     "de"
@@ -20,7 +19,7 @@ SPM_ENCODE=$SCRIPTS/spm_encode.py
 BPESIZE=16384
 ORIG=$ROOT/iwslt17_orig
 DATA=$ROOT/iwslt17.de_fr.en.bpe16k
-mkdir -p $ORIG $DATA
+mkdir -p "$ORIG" "$DATA"
 
 TRAIN_MINLEN=1  # remove sentences with <1 BPE token
 TRAIN_MAXLEN=250  # remove sentences with >250 BPE tokens
@@ -41,30 +40,30 @@ VALID_SETS=(
 # download and extract data
 for ((i=0;i<${#URLS[@]};++i)); do
     ARCHIVE=$ORIG/${ARCHIVES[i]}
-    if [ -f $ARCHIVE ]; then
+    if [ -f "$ARCHIVE" ]; then
         echo "$ARCHIVE already exists, skipping download"
     else
         URL=${URLS[i]}
-        wget -P $ORIG "$URL"
-        if [ -f $ARCHIVE ]; then
+        wget -P "$ORIG" "$URL"
+        if [ -f "$ARCHIVE" ]; then
             echo "$URL successfully downloaded."
         else
             echo "$URL not successfully downloaded."
-            exit -1
+            exit 1
         fi
     fi
     FILE=${ARCHIVE: -4}
-    if [ -e $FILE ]; then
+    if [ -e "$FILE" ]; then
         echo "$FILE already exists, skipping extraction"
     else
-        tar -C $ORIG -xzvf $ARCHIVE
+        tar -C "$ORIG" -xzvf "$ARCHIVE"
     fi
 done
 
 echo "pre-processing train data..."
 for SRC in "${SRCS[@]}"; do
     for LANG in "${SRC}" "${TGT}"; do
-        cat $ORIG/${SRC}-${TGT}/train.tags.${SRC}-${TGT}.${LANG} \
+        cat "$ORIG/${SRC}-${TGT}/train.tags.${SRC}-${TGT}.${LANG}" \
             | grep -v '<url>' \
             | grep -v '<talkid>' \
             | grep -v '<keywords>' \
@@ -79,7 +78,7 @@ for SRC in "${SRCS[@]}"; do
             | sed -e 's/<\/description>//g' \
             | sed 's/^\s*//g' \
             | sed 's/\s*$//g' \
-            > $DATA/train.${SRC}-${TGT}.${LANG}
+            > "$DATA/train.${SRC}-${TGT}.${LANG}"
     done
 done
 
@@ -89,11 +88,11 @@ for ((i=0;i<${#SRCS[@]};++i)); do
     VALID_SET=${VALID_SETS[i]}
     for FILE in ${VALID_SET[@]}; do
         for LANG in "$SRC" "$TGT"; do
-            grep '<seg id' $ORIG/${SRC}-${TGT}/${FILE}.${LANG}.xml \
+            grep '<seg id' "$ORIG/${SRC}-${TGT}/${FILE}.${LANG}.xml" \
                 | sed -e 's/<seg id="[0-9]*">\s*//g' \
                 | sed -e 's/\s*<\/seg>\s*//g' \
                 | sed -e "s/\’/\'/g" \
-                > $DATA/valid.${SRC}-${TGT}.${LANG}
+                > "$DATA/valid.${SRC}-${TGT}.${LANG}"
         done
     done
 done
@@ -101,7 +100,7 @@ done
 # learn BPE with sentencepiece
 TRAIN_FILES=$(for SRC in "${SRCS[@]}"; do echo $DATA/train.${SRC}-${TGT}.${SRC}; echo $DATA/train.${SRC}-${TGT}.${TGT}; done | tr "\n" ",")
 echo "learning joint BPE over ${TRAIN_FILES}..."
-python $SPM_TRAIN \
+python "$SPM_TRAIN" \
     --input=$TRAIN_FILES \
     --model_prefix=$DATA/sentencepiece.bpe \
     --vocab_size=$BPESIZE \
@@ -112,16 +111,16 @@ python $SPM_TRAIN \
 echo "encoding train/valid with learned BPE..."
 for SRC in "${SRCS[@]}"; do
     for LANG in "$SRC" "$TGT"; do
-        python $SPM_ENCODE \
-            --model $DATA/sentencepiece.bpe.model \
+        python "$SPM_ENCODE" \
+            --model "$DATA/sentencepiece.bpe.model" \
             --output_format=piece \
-            --inputs $DATA/train.${SRC}-${TGT}.${SRC} $DATA/train.${SRC}-${TGT}.${TGT} \
-            --outputs $DATA/train.bpe.${SRC}-${TGT}.${SRC} $DATA/train.bpe.${SRC}-${TGT}.${TGT} \
+            --inputs "$DATA/train.${SRC}-${TGT}.${SRC} $DATA/train.${SRC}-${TGT}.${TGT}" \
+            --outputs "$DATA/train.bpe.${SRC}-${TGT}.${SRC} $DATA/train.bpe.${SRC}-${TGT}.${TGT}" \
             --min-len $TRAIN_MINLEN --max-len $TRAIN_MAXLEN
-        python $SPM_ENCODE \
-            --model $DATA/sentencepiece.bpe.model \
+        python "$SPM_ENCODE" \
+            --model "$DATA/sentencepiece.bpe.model" \
             --output_format=piece \
-            --inputs $DATA/valid.${SRC}-${TGT}.${SRC} $DATA/valid.${SRC}-${TGT}.${TGT} \
-            --outputs $DATA/valid.bpe.${SRC}-${TGT}.${SRC} $DATA/valid.bpe.${SRC}-${TGT}.${TGT}
+            --inputs "$DATA/valid.${SRC}-${TGT}.${SRC} $DATA/valid.${SRC}-${TGT}.${TGT}" \
+            --outputs "$DATA/valid.bpe.${SRC}-${TGT}.${SRC} $DATA/valid.bpe.${SRC}-${TGT}.${TGT}"
     done
 done
--- scripts/spm_train.py
+++ scripts/spm_train.py
@@ -1,10 +1,9 @@
+#!/usr/bin/env python
 # Copyright (c) Facebook, Inc. and its affiliates.
 # All rights reserved.
 #
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
-#
-#!/usr/bin/env python
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
