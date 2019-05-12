287d31e210bea92ad09aa30db6243f08245c51f6
Myle Ott <myleott@fb.com>
2019-05-12 16:28:51 -0700

    Add scripts for working with txt files containing document boundaries
    Summary: Pull Request resolved: https://github.com/pytorch/fairseq/pull/736
    
    Differential Revision: D15314626
    
    Pulled By: myleott
    
    fbshipit-source-id: 1e0c32529afee57e43fe5d6c7991cd13eb8a52c4

---
+++ scripts/shard_docs.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the LICENSE file in
+# the root directory of this source tree. An additional grant of patent rights
+# can be found in the PATENTS file in the same directory.
+"""
+Split a large file into shards while respecting document boundaries. Documents
+should be separated by a single empty line.
+"""
+
+import argparse
+import contextlib
+import random
+import sys
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input')
+    parser.add_argument('--num-shards', type=int)
+    args = parser.parse_args()
+
+    assert args.num_shards is not None and args.num_shards > 1
+
+    with open(args.input, 'r', encoding='utf-8') as h:
+        with contextlib.ExitStack() as stack:
+            outputs = [
+                stack.enter_context(open(args.input + ".shard" + str(i), "w", encoding="utf-8"))
+                for i in range(args.num_shards)
+            ]
+
+            doc = []
+            first_doc = [True]*args.num_shards
+            def output_doc(i):
+                if not first_doc[i]:
+                    outputs[i].write("\n")
+                first_doc[i] = False
+                for line in doc:
+                    outputs[i].write(line)
+                doc.clear()
+
+            num_docs = 0
+            for line in h:
+                if line.strip() == "":  # empty line indicates new document
+                    output_doc(num_docs % args.num_shards)
+                    num_docs += 1
+                else:
+                    doc.append(line)
+            output_doc(num_docs % args.num_shards)
+
+
+if __name__ == '__main__':
+    main()
