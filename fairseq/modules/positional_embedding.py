12abdb27807422b71ec215bf3ab1d9d06ee3d2c1
Kartikay Khandelwal <kartikayk@fb.com>
2019-05-18 15:15:51 -0700

    Make Fairseq compatible with pre-computed position tensors (#570)
    Summary:
    Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/570
    
    Pull Request resolved: https://github.com/pytorch/fairseq/pull/731
    
    Currently the LearnedPositionalEmbedding module computes the position tensor based on the input data. However this really doesnt work for XLM where we have different behavior based on the Masked LM and Translation LM. In this diff I keep the same default behavior for LearnedPositionalEmbedding as before but add the ability for these models to work with pre-computed position tensors.
    
    Reviewed By: myleott
    
    Differential Revision: D15305474
    
    fbshipit-source-id: 50bd4df2b3277432737b8146e40333ddb45db1d9

--- fairseq/modules/positional_embedding.py
+++ fairseq/modules/positional_embedding.py
@@ -11,13 +11,23 @@ from .learned_positional_embedding import LearnedPositionalEmbedding
 from .sinusoidal_positional_embedding import SinusoidalPositionalEmbedding
 
 
-def PositionalEmbedding(num_embeddings, embedding_dim, padding_idx, learned=False):
+def PositionalEmbedding(
+        num_embeddings: int,
+        embedding_dim: int,
+        padding_idx: int,
+        learned: bool = False,
+):
     if learned:
-        m = LearnedPositionalEmbedding(
-            num_embeddings + padding_idx + 1, embedding_dim, padding_idx,
-        )
+        # if padding_idx is specified then offset the embedding ids by
+        # this index and adjust num_embeddings appropriately
+        # TODO: The right place for this offset would be inside
+        # LearnedPositionalEmbedding. Move this there for a cleaner implementation.
+        if padding_idx is not None:
+            num_embeddings = num_embeddings + padding_idx + 1
+        m = LearnedPositionalEmbedding(num_embeddings, embedding_dim, padding_idx)
         nn.init.normal_(m.weight, mean=0, std=embedding_dim ** -0.5)
-        nn.init.constant_(m.weight[padding_idx], 0)
+        if padding_idx is not None:
+            nn.init.constant_(m.weight[padding_idx], 0)
     else:
         m = SinusoidalPositionalEmbedding(
             embedding_dim, padding_idx, init_size=num_embeddings + padding_idx + 1,
