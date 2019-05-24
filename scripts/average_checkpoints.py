8ce2c35d8e2dfb2b6dd220058710f81df5eb5729
Yongqiang Wang <yqw@fb.com>
2019-05-24 12:09:18 -0700

    Implement reducing footprint of average checkpoint correctly (#747)
    Summary:
    Pull Request resolved: https://github.com/pytorch/fairseq/pull/747
    
    In https://github.com/pytorch/fairseq/pull/647, checkpoint averaging
    is not Implemented correctly when it comes to shared parameters. This diff
    has the right Implementation and a test case to guard future change.
    
    Reviewed By: myleott
    
    Differential Revision: D15402943
    
    fbshipit-source-id: 8004836d5c2571814ea54844650618008a9ee522

--- scripts/average_checkpoints.py
+++ scripts/average_checkpoints.py
@@ -27,6 +27,8 @@ def average_checkpoints(inputs):
     params_dict = collections.OrderedDict()
     params_keys = None
     new_state = None
+    num_models = len(inputs)
+
     for f in inputs:
         state = torch.load(
             f,
@@ -50,20 +52,19 @@ def average_checkpoints(inputs):
             )
 
         for k in params_keys:
-            if k not in params_dict:
-                params_dict[k] = []
             p = model_params[k]
             if isinstance(p, torch.HalfTensor):
                 p = p.float()
-            params_dict[k].append(p)
+            if k not in params_dict:
+                params_dict[k] = p.clone()
+                # NOTE: clone() is needed in case of p is a shared parameter
+            else:
+                params_dict[k] += p
 
     averaged_params = collections.OrderedDict()
-    # v should be a list of torch Tensor.
     for k, v in params_dict.items():
-        summed_v = None
-        for x in v:
-            summed_v = summed_v + x if summed_v is not None else x
-        averaged_params[k] = summed_v / len(v)
+        averaged_params[k] = v
+        averaged_params[k].div_(num_models)
     new_state['model'] = averaged_params
     return new_state
 
