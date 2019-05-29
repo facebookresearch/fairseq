bc19dccc73f85ddb1a64c203af77f01f97b740dc
Myle Ott <myleott@fb.com>
2019-05-29 15:26:48 -0400

    [fairseq] Add fb_train.py (#607)

---
+++ fb_train.py
@@ -0,0 +1,72 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the LICENSE file in
+# the root directory of this source tree. An additional grant of patent rights
+# can be found in the PATENTS file in the same directory.
+
+import contextlib
+import os
+import sys
+
+import torch.fb.rendezvous.zeus  # noqa: F401
+
+from fairseq import options
+
+from train import distributed_main, main
+
+
+def zeus_distributed_main(device_id, args, start_rank, log_path=None):
+    args.distributed_rank = start_rank + device_id
+    if log_path is not None and start_rank == 0 and device_id == 0:
+        with open(log_path, "a") as h:
+            with contextlib.redirect_stdout(tee(sys.stdout, h)):
+                with contextlib.redirect_stderr(tee(sys.stderr, h)):
+                    distributed_main(device_id, args)
+    else:
+        distributed_main(device_id, args)
+
+
+if __name__ == '__main__':
+    parser = options.get_training_parser()
+    args = options.parse_args_and_arch(parser)
+    if args.distributed_init_method is not None:
+        # distributed training
+        log_path = os.path.join(args.save_dir, 'train.log')
+        if torch.cuda.device_count() > 1:
+            start_rank = args.distributed_rank
+            args.distributed_rank = None
+            torch.multiprocessing.spawn(
+                fn=zeus_distributed_main,
+                args=(args, start_rank, log_path),
+                nprocs=torch.cuda.device_count(),
+            )
+        else:
+            zeus_distributed_main(args.device_id, args, 0, log_path)
+    else:
+        # single GPU training
+        main(args)
+
+
+class tee(object):
+    """Source: http://shallowsky.com/blog/programming/python-tee.html"""
+
+    def __init__(self, _fd1, _fd2):
+        self.fd1 = _fd1
+        self.fd2 = _fd2
+
+    def __del__(self):
+        if self.fd1 != sys.stdout and self.fd1 != sys.stderr:
+            self.fd1.close()
+        if self.fd2 != sys.stdout and self.fd2 != sys.stderr:
+            self.fd2.close()
+
+    def write(self, text):
+        self.fd1.write(text)
+        self.fd1.flush()
+        self.fd2.write(text)
+        self.fd2.flush()
+
+    def flush(self):
+        self.fd1.flush()
+        self.fd2.flush()
