977e36e56aa9be4b114a85de90df52378df2c3dd
Myle Ott <myleott@fb.com>
2019-05-29 14:03:28 -0700

    Support multiple seeds in data_utils.numpy_seed
    Summary: Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/610
    
    Differential Revision: D15541261
    
    Pulled By: myleott
    
    fbshipit-source-id: f0b823cf4f04c5ef3205f6d259c6dcad4cc329b1

--- fairseq/data/data_utils.py
+++ fairseq/data/data_utils.py
@@ -44,12 +44,14 @@ def collate_tokens(values, pad_idx, eos_idx=None, left_pad=False, move_eos_to_be
 
 
 @contextlib.contextmanager
-def numpy_seed(seed):
+def numpy_seed(seed, *addl_seeds):
     """Context manager which seeds the NumPy PRNG with the specified seed and
     restores the state afterward"""
     if seed is None:
         yield
         return
+    if len(addl_seeds) > 0:
+        seed = int(hash((seed, *addl_seeds)) % 1e6)
     state = np.random.get_state()
     np.random.seed(seed)
     try:
