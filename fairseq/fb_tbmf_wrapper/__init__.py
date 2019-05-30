0095c6e46b41f5340fb94a12777d5a572a5a3638
Sujit Verma <sujitv@devfair020.maas>
2019-05-29 19:32:44 -0700

    Added support for plotting scalars through palaas tbwriter interface. (#580)
    Summary: Changes for supporting tensorboard scalar plotting.
    
    Reviewed By: myleott
    
    Differential Revision: D15456534
    
    Pulled By: myleott
    
    fbshipit-source-id: a012a4eea028aae764ce11786570b7d96841c4a5

---
+++ fairseq/fb_tbmf_wrapper/__init__.py
@@ -0,0 +1,5 @@
+from .fb_tbmf_wrapper import fb_tbmf_wrapper
+
+__all__ = [
+        'fb_tbmf_wrapper',
+]
---
+++ fairseq/fb_tbmf_wrapper/fb_tbmf_wrapper.py
@@ -0,0 +1,120 @@
+"""
+Wrapper around tbwriter api for for writing to manifold-tensorboard.
+FB Internal (not to be open-sourced)
+"""
+
+from fairseq.meters import AverageMeter
+from fairseq import progress_bar
+
+try:
+    from palaas import tbwriter, register_manifold
+except ImportError:
+    pass
+
+from numbers import Number
+import os
+import datetime
+
+
+class LogCounter:
+
+    def __init__(self, interval):
+        self.log_interval = interval
+        self.log_counter = 1
+
+    def advance(self):
+        self.log_counter += 1
+        return self.log_counter % self.log_interval == 0
+
+
+class fb_tbmf_wrapper(progress_bar.progress_bar):
+    """Log to tensorboard."""
+
+    # manifold sub folder to used by all instances.
+    manifold_job_path = ""
+
+    def _get_job_path(self):
+        # get slurm job name
+        job_id = os.environ.get('SLURM_JOB_NAME')
+        if job_id is None:
+            # TODO
+            # try to get fb learner job name
+            job_id = ""
+
+        # get date-time str
+        time = datetime.datetime.now()
+        time_str = "{}-{}-{}-{}:{}".format(
+                    time.year,
+                    time.month,
+                    time.day,
+                    time.hour,
+                    time.minute)
+
+        if job_id is not None and job_id != "":
+            return time_str + "/" + job_id
+        else:
+            return time_str
+
+    def __init__(self, wrapped_bar, args, log_interval):
+        self.wrapped_bar = wrapped_bar
+        if fb_tbmf_wrapper.manifold_job_path == "":
+            fb_tbmf_wrapper.manifold_job_path = self._get_job_path()
+        self.log_interval = log_interval
+        # We need a log counter for every variable.
+        self.counters = {}
+        self.counter_disabled_list = []
+
+        self.log_counter = 1
+        self.args = args
+        self._tbwriter = None
+        try:
+            self._tbwriter = tbwriter.get_tbwriter(fb_tbmf_wrapper.manifold_job_path)
+            register_manifold()
+        except Exception:
+            pass
+
+        self.disable_buffering('valid')
+
+        self._writers = {}
+
+    def __iter__(self):
+        return iter(self.wrapped_bar)
+
+    def log(self, stats, tag='', step=None):
+        """Log intermediate stats to tensorboard."""
+        self._log_to_tensorboard(stats, tag, step)
+        self.wrapped_bar.log(stats, tag=tag, step=step)
+
+    def print(self, stats, tag='', step=None):
+        """Print end-of-epoch stats."""
+        self._log_to_tensorboard(stats, tag, step)
+        self.wrapped_bar.print(stats, tag=tag, step=step)
+
+    def __exit__(self, *exc):
+        if self._tbwriter is not None:
+            self._tbwriter.close()
+        return False
+
+    def disable_buffering(self, tag):
+        if tag is not None:
+            self.counter_disabled_list.append(tag)
+
+    def _log_to_tensorboard(self, stats, tag='', step=None):
+        writer = self._tbwriter
+        if writer is None:
+            return
+
+        # Get LogCounter for this variable
+        if tag not in self.counter_disabled_list:
+            if tag not in self.counters:
+                self.counters[tag] = LogCounter(self.log_interval)
+            if not self.counters[tag].advance():
+                return
+
+        if step is None:
+            step = stats['num_updates']
+        for key in stats.keys() - {'num_updates'}:
+            if isinstance(stats[key], AverageMeter):
+                writer.add_scalar(tag, key, stats[key].val, step)
+            elif isinstance(stats[key], Number):
+                writer.add_scalar(tag, key, stats[key], step)
