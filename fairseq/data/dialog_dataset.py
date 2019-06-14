c4b55464d285edd81ae233e712734c0f167b124d
vineetk1 <vineetchandhok@gmail.com>
2019-06-13 20:54:09 -0400

    major additions to change from Translation to Dialog

--- fairseq/data/dialog_dataset.py
+++ fairseq/data/dialog_dataset.py
@@ -14,57 +14,81 @@ from . import data_utils, FairseqDataset
 
 
 def collate(
-    samples, pad_idx, eos_idx, left_pad_source=True, left_pad_target=False,
+    dlgs, pad_idx, eos_idx, left_pad_source=True, left_pad_target=False,
     input_feeding=True,
 ):
-    if len(samples) == 0:
+    if len(dlgs) == 0:
         return {}
 
-    def merge(key, left_pad, move_eos_to_beginning=False):
+    def one_seq_per_dlg(key, seq_num_in_dlgs):
+        one_seq_per_dlg = []
+        for dlg in dlgs:
+            try:
+                one_seq_per_dlg.append(dlg[key][seq_num_in_dlgs])
+            except IndexError:
+                one_seq_per_dlg.append(dlg[key][0].new([eos_idx]))\
+                      if dlg[key][0][-1] == eos_idx\
+                      else one_seq_per_dlg.append(dlg[key][0].new([]))
+        return one_seq_per_dlg
+
+    def merge(key, seq_num_in_dlgs, left_pad, move_eos_to_beginning=False):
         return data_utils.collate_tokens(
-            [s[key] for s in samples],
-            pad_idx, eos_idx, left_pad, move_eos_to_beginning,
+            one_seq_per_dlg(key, seq_num_in_dlgs), pad_idx, eos_idx,
+            left_pad, move_eos_to_beginning,
         )
 
-    id = torch.LongTensor([s['id'] for s in samples])
-    src_tokens = merge('source', left_pad=left_pad_source)
-    # sort by descending source length
-    src_lengths = torch.LongTensor([s['source'].numel() for s in samples])
-    src_lengths, sort_order = src_lengths.sort(descending=True)
-    id = id.index_select(0, sort_order)
-    src_tokens = src_tokens.index_select(0, sort_order)
+    batch = []
+    dlg_ids = torch.LongTensor([dlg['dlg_id'] for dlg in dlgs])
+    max_num_seqs_in_dlgs = max([len(dlg['source']) for dlg in dlgs])
+    assert max_num_seqs_in_dlgs == max([len(dlg['target']) for dlg in dlgs])
+    for seq_num_in_dlgs in range(max_num_seqs_in_dlgs):
+        src_tokens = merge('source', seq_num_in_dlgs, left_pad=left_pad_source)
+        # sort by descending source length
+        src_lengths = torch.LongTensor([seq.numel() for seq in
+                                       one_seq_per_dlg
+                                       ('source', seq_num_in_dlgs)])
+        src_lengths, sort_order = src_lengths.sort(descending=True)
+        dlg_ids_sorted = dlg_ids.index_select(0, sort_order)
+        src_tokens = src_tokens.index_select(0, sort_order)
 
-    prev_output_tokens = None
-    target = None
-    if samples[0].get('target', None) is not None:
-        target = merge('target', left_pad=left_pad_target)
-        target = target.index_select(0, sort_order)
-        ntokens = sum(len(s['target']) for s in samples)
+        prev_output_tokens = None
+        target = None
+        if dlgs[0].get('target', None) is not None:
+            target = merge('target', seq_num_in_dlgs, left_pad=left_pad_target)
+            target = target.index_select(0, sort_order)
+            ntokens = sum(seq.numel() for seq in
+                          one_seq_per_dlg('target', seq_num_in_dlgs))
 
-        if input_feeding:
-            # we create a shifted version of targets for feeding the
-            # previous output token(s) into the next decoder step
-            prev_output_tokens = merge(
-                'target',
-                left_pad=left_pad_target,
-                move_eos_to_beginning=True,
-            )
-            prev_output_tokens = prev_output_tokens.index_select(0, sort_order)
-    else:
-        ntokens = sum(len(s['source']) for s in samples)
+            if input_feeding:
+                # we create a shifted version of targets for feeding the
+                # previous output token(s) into the next decoder step
+                prev_output_tokens = merge(
+                    'target',
+                    seq_num_in_dlgs,
+                    left_pad=left_pad_target,
+                    move_eos_to_beginning=True,
+                )
+                prev_output_tokens =\
+                    prev_output_tokens.index_select(0, sort_order)
+        else:
+            ntokens = sum(seq.numel() for seq in
+                          one_seq_per_dlg('source', seq_num_in_dlgs))
 
-    batch = {
-        'id': id,
-        'nsentences': len(samples),
-        'ntokens': ntokens,
-        'net_input': {
-            'src_tokens': src_tokens,
-            'src_lengths': src_lengths,
-        },
-        'target': target,
-    }
-    if prev_output_tokens is not None:
-        batch['net_input']['prev_output_tokens'] = prev_output_tokens
+        batch.append({
+            'id': dlg_ids_sorted,
+            'nsentences': len(dlgs),
+            'ntokens': ntokens,
+            'net_input': {
+                'src_tokens': src_tokens,
+                'src_lengths': src_lengths,
+                'sort_order': sort_order,
+                'start_dlg': True if seq_num_in_dlgs == 0 else False
+            },
+            'target': target,
+        })
+        if prev_output_tokens is not None:
+            batch[seq_num_in_dlgs]['net_input']['prev_output_tokens'] =\
+                    prev_output_tokens
     return batch
 
 
@@ -96,7 +120,7 @@ class dialogDataset(FairseqDataset):
             of source if it's present (default: False).
         append_eos_to_target (bool, optional): if set, appends eos to end of
             target if it's absent (default: False).
-        dialog_index (List[int]): indices where dialogs start
+        dialog_index (List[int]): maps dialog_id to the first sequence_id
     """
 
     def __init__(
@@ -104,7 +128,8 @@ class dialogDataset(FairseqDataset):
         tgt=None, tgt_sizes=None, tgt_dict=None,
         left_pad_source=True, left_pad_target=False,
         max_source_positions=1024, max_target_positions=1024,
-        shuffle=True, input_feeding=True, remove_eos_from_source=False, append_eos_to_target=False, dialog_index=None
+        shuffle=True, input_feeding=True, remove_eos_from_source=False,
+        append_eos_to_target=False, dialog_index=None
     ):
         if tgt_dict is not None:
             assert src_dict.pad() == tgt_dict.pad()
@@ -126,29 +151,30 @@ class dialogDataset(FairseqDataset):
         self.append_eos_to_target = append_eos_to_target
         self.dialog_index = dialog_index
 
-    def _seq_indices(self, dlg_idx):
-        # Return sequence indices associated with dialog index
-        return [seq_idx for seq_idx in range(self.dialog_index[dlg_idx],
-                self.dialog_index[dlg_idx+1]
-                if dlg_idx+1 != len(self.dialog_index)
+    def _seq_ids(self, dlg_id):
+        # Return sequences that belong to the dialog
+        return [seq_id for seq_id in range(self.dialog_index[dlg_id],
+                self.dialog_index[dlg_id+1]
+                if dlg_id+1 != len(self.dialog_index)
                 else len(self.src))]
 
-    def __getitem__(self, dlg_idx):
-        src_items, tgt_items = [], []
-        for seq_idx in self._seq_indices(dlg_idx):
-            src_items.append(self.src[seq_idx])
-            tgt_items.append(self.tgt[seq_idx]
-                             if self.tgt is not None else None)
+    def __getitem__(self, dlg_id):
+        src_items = []
+        tgt_items = [] if self.tgt is not None else None
+        for seq_id in self._seq_ids(dlg_id):
+            src_items.append(self.src[seq_id])
+            tgt_items.append(self.tgt[seq_id]) \
+                if self.tgt is not None else None
 
         # Append EOS to end of tgt sentence if it does not have an EOS and
         # remove EOS from end of src sentence if it exists. This is useful when
         # we use existing datasets for opposite directions i.e., when we want
         # to use tgt_dataset as src_dataset and vice versa
-        if self.append_eos_to_target:
+        if self.append_eos_to_target and self.tgt:
             tgt_items1 = tgt_items
             eos = self.tgt_dict.eos() if self.tgt_dict else self.src_dict.eos()
             tgt_items = [torch.cat([tgt_item, torch.LongTensor([eos])])
-                         if self.tgt and tgt_item[-1] != eos else tgt_item
+                         if tgt_item[-1] != eos else tgt_item
                          for tgt_item in tgt_items1]
 
         if self.remove_eos_from_source:
@@ -158,7 +184,7 @@ class dialogDataset(FairseqDataset):
                          for src_item in src_items1]
 
         return {
-            'id': dlg_idx,
+            'dlg_id': dlg_id,
             'source': src_items,
             'target': tgt_items,
         }
@@ -181,33 +207,35 @@ class dialogDataset(FairseqDataset):
 
                   - `src_tokens` (LongTensor): a padded 2D Tensor of tokens in
                     the source sentence of shape `(bsz, src_len)`. Padding will
-                    appear on the left if *left_pad_source* is ``True``.
+                   appear on the left if *left_pad_source* is ``True``.
                   - `src_lengths` (LongTensor): 1D Tensor of the unpadded
                     lengths of each source sentence of shape `(bsz)`
                   - `prev_output_tokens` (LongTensor): a padded 2D Tensor of
-                    tokens in the target sentence, shifted right by one position
-                    for input feeding/teacher forcing, of shape `(bsz,
+                    tokens in the target sentence, shifted right by one
+                    position for input feeding/teacher forcing, of shape `(bsz,
                     tgt_len)`. This key will not be present if *input_feeding*
                     is ``False``. Padding will appear on the left if
                     *left_pad_target* is ``True``.
 
                 - `target` (LongTensor): a padded 2D Tensor of tokens in the
-                  target sentence of shape `(bsz, tgt_len)`. Padding will appear
-                  on the left if *left_pad_target* is ``True``.
+                  target sentence of shape `(bsz, tgt_len)`. Padding will
+                  appear on the left if *left_pad_target* is ``True``.
         """
         return collate(
             samples, pad_idx=self.src_dict.pad(), eos_idx=self.src_dict.eos(),
-            left_pad_source=self.left_pad_source, left_pad_target=self.left_pad_target,
+            left_pad_source=self.left_pad_source,
+            left_pad_target=self.left_pad_target,
             input_feeding=self.input_feeding,
         )
 
-    def _num_tokens_src_tgt_dialogs(self, dlg_idx):
+    def _num_tokens_src_tgt_dialogs(self, dlg_id):
         """Return the number of tokens in the dialog. This value is used to
         enforce ``--max-tokens`` during batching."""
         src_dialog_size = tgt_dialog_size = 0
-        for seq_idx in self._seq_indices(dlg_idx):
-            src_dialog_size += self.src_sizes[seq_idx]
-            tgt_dialog_size += self.tgt_sizes[seq_idx]
+        for seq_id in self._seq_ids(dlg_id):
+            src_dialog_size += self.src_sizes[seq_id]
+            tgt_dialog_size += self.tgt_sizes[seq_id] \
+                if self.tgt_sizes is not None else 0
         return src_dialog_size, tgt_dialog_size
 
     def num_tokens(self, index):
@@ -236,12 +264,13 @@ class dialogDataset(FairseqDataset):
     def supports_prefetch(self):
         return (
             getattr(self.src, 'supports_prefetch', False)
-            and (getattr(self.tgt, 'supports_prefetch', False) or self.tgt is None)
+            and (getattr(self.tgt, 'supports_prefetch', False)
+                 or self.tgt is None)
         )
 
-    def prefetch(self, indices):
-        seq_indices = [seq_idx for dlg_idx in indices
-                       for seq_idx in self._seq_indices(dlg_idx)]
-        self.src.prefetch(seq_indices)
+    def prefetch(self, dlg_ids):
+        seq_ids = [seq_id for dlg_id in dlg_ids
+                   for seq_id in self._seq_ids(dlg_id)]
+        self.src.prefetch(seq_ids)
         if self.tgt is not None:
-            self.tgt.prefetch(seq_indices)
+            self.tgt.prefetch(seq_ids)
