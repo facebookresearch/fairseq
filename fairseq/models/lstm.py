8c2139a16d3add0bf3bc126a3e492d892ef29736
vineetk1 <vineetchandhok@gmail.com>
2019-07-02 11:10:29 -0400

    updated again the comment on requirement for right padding

--- fairseq/models/lstm.py
+++ fairseq/models/lstm.py
@@ -209,8 +209,8 @@ class LSTMEncoder(FairseqEncoder):
 
     def forward(self, src_tokens, src_lengths):
         if self.left_pad:
-            # pack_padded_sequence requires right-padding; convert left-padding
-            # to right-padding
+            # nn.utils.rnn.pack_padded_sequence requires right-padding; convert
+            # left-padding to right-padding
             src_tokens = utils.convert_padding_direction(
                 src_tokens,
                 self.padding_idx,
