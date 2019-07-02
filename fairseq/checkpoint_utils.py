3e70ba5a0cc48ecb59b13d41e8ea8d91305d46fe
maple <fllubo@qq.com>
2019-07-02 17:08:11 +0800

    fix issue #848

--- fairseq/checkpoint_utils.py
+++ fairseq/checkpoint_utils.py
@@ -121,6 +121,7 @@ def load_checkpoint(args, trainer):
     if extra_state is not None and not args.reset_dataloader:
         # restore iterator from checkpoint
         itr_state = extra_state['train_iterator']
+        itr_state['epoch'] += 1
         epoch_itr = trainer.get_train_iterator(epoch=itr_state['epoch'])
         epoch_itr.load_state_dict(itr_state)
     else:
