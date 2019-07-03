aa0e4ae3d66fd40e29723e2e3ad1866aac52da7f
Guanheng Zhang <zhangguanheng@devfair0129.h2.fair>
2019-07-03 10:41:34 -0700

    Fix lint errors.

--- fairseq/modules/multihead_attention.py
+++ fairseq/modules/multihead_attention.py
@@ -109,26 +109,26 @@ class MultiheadAttention(nn.Module):
 
         if self.enable_torch_version and not self.onnx_trace and incremental_state is None and not static_kv:
             if self.qkv_same_dim:
-                return  F.multi_head_attention_forward(query, key, value,
-                                                       self.embed_dim, self.num_heads,
-                                                       self.in_proj_weight,
-                                                       self.in_proj_bias, self.bias_k, self.bias_v,
-                                                       self.add_zero_attn, self.dropout,
-                                                       self.out_proj.weight, self.out_proj.bias,
-                                                       self.training, key_padding_mask, need_weights,
-                                                       attn_mask)
+                return F.multi_head_attention_forward(query, key, value,
+                                                      self.embed_dim, self.num_heads,
+                                                      self.in_proj_weight,
+                                                      self.in_proj_bias, self.bias_k, self.bias_v,
+                                                      self.add_zero_attn, self.dropout,
+                                                      self.out_proj.weight, self.out_proj.bias,
+                                                      self.training, key_padding_mask, need_weights,
+                                                      attn_mask)
             else:
-                return  F.multi_head_attention_forward(query, key, value,
-                                                       self.embed_dim, self.num_heads,
-                                                       torch.empty([0]),
-                                                       self.in_proj_bias, self.bias_k, self.bias_v,
-                                                       self.add_zero_attn, self.dropout,
-                                                       self.out_proj.weight, self.out_proj.bias,
-                                                       self.training, key_padding_mask, need_weights,
-                                                       attn_mask, use_separate_proj_weight=True,
-                                                       q_proj_weight=self.q_proj_weight,
-                                                       k_proj_weight=self.k_proj_weight,
-                                                       v_proj_weight=self.v_proj_weight)
+                return F.multi_head_attention_forward(query, key, value,
+                                                      self.embed_dim, self.num_heads,
+                                                      torch.empty([0]),
+                                                      self.in_proj_bias, self.bias_k, self.bias_v,
+                                                      self.add_zero_attn, self.dropout,
+                                                      self.out_proj.weight, self.out_proj.bias,
+                                                      self.training, key_padding_mask, need_weights,
+                                                      attn_mask, use_separate_proj_weight=True,
+                                                      q_proj_weight=self.q_proj_weight,
+                                                      k_proj_weight=self.k_proj_weight,
+                                                      v_proj_weight=self.v_proj_weight)
 
         if incremental_state is not None:
             saved_state = self._get_input_buffer(incremental_state)
