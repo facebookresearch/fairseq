b69e61c5bc874968821b3ec229bf2fd002944e30
Myle Ott <myleott@fb.com>
2019-07-28 05:15:28 -0700

    Fix compatibility with PyTorch 1.0.x

--- fairseq/data/base_wrapper_dataset.py
+++ fairseq/data/base_wrapper_dataset.py
@@ -5,7 +5,7 @@
 # the root directory of this source tree. An additional grant of patent rights
 # can be found in the PATENTS file in the same directory.
 
-from torch.utils.data._utils.collate import default_collate
+from torch.utils.data.dataloader import default_collate
 
 from . import FairseqDataset
 
