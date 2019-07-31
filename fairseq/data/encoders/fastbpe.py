c3e2203097cce81816f398876bab17cd5c78d697
Nathan Ng <n.ng555@gmail.com>
2019-07-31 03:41:41 -0700

    Wmt19 models (#767)
    Summary:
    Release of the WMT 19 pretrained models
    Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/767
    
    Reviewed By: edunov
    
    Differential Revision: D16472717
    
    Pulled By: nng555
    
    fbshipit-source-id: acf0fa3548c33f2bf2b5f71e551c782ad8c31a42

---
+++ fairseq/data/encoders/fastbpe.py
@@ -0,0 +1,34 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from fairseq import file_utils
+from fairseq.data.encoders import register_bpe
+
+@register_bpe('fastbpe')
+class fastBPE(object):
+
+    @staticmethod
+    def add_args(parser):
+        # fmt: off
+        parser.add_argument('--bpe-codes', type=str,
+                            help='path to fastBPE BPE')
+        # fmt: on
+
+    def __init__(self, args):
+        if args.bpe_codes is None:
+            raise ValueError('--bpe-codes is required for --bpe=subword_nmt')
+        codes = file_utils.cached_path(args.bpe_codes)
+        try:
+            import fastBPE
+            self.bpe = fastBPE.fastBPE(codes)
+            self.bpe_symbol = "@@ "
+        except ImportError:
+            raise ImportError('Please install fastbpe at https://github.com/glample/fastBPE')
+
+    def encode(self, x: str) -> str:
+        return self.bpe.apply([x])[0]
+
+    def decode(self, x: str) -> str:
+        return (x + ' ').replace(self.bpe_symbol, '').rstrip()
--- fairseq/models/transformer.py
+++ fairseq/models/transformer.py
@@ -53,6 +53,10 @@ class TransformerModel(FairseqEncoderDecoderModel):
             'transformer.wmt14.en-fr': 'https://dl.fbaipublicfiles.com/fairseq/models/wmt14.en-fr.joined-dict.transformer.tar.bz2',
             'transformer.wmt16.en-de': 'https://dl.fbaipublicfiles.com/fairseq/models/wmt16.en-de.joined-dict.transformer.tar.bz2',
             'transformer.wmt18.en-de': 'https://dl.fbaipublicfiles.com/fairseq/models/wmt18.en-de.ensemble.tar.gz',
+            'transformer.wmt19.en-de': 'https://dl.fbaipublicfiles.com/fairseq/models/wmt19.en-de.joined-dict.ensemble.tar.bz2',
+            'transformer.wmt19.en-ru': 'https://dl.fbaipublicfiles.com/fairseq/models/wmt19.en-ru.ensemble.tar.bz2',
+            'transformer.wmt19.de-en': 'https://dl.fbaipublicfiles.com/fairseq/models/wmt19.de-en.joined-dict.ensemble.tar.bz2',
+            'transformer.wmt19.ru-en': 'https://dl.fbaipublicfiles.com/fairseq/models/wmt19.ru-en.ensemble.tar.bz2',
         }
 
     def __init__(self, encoder, decoder):
