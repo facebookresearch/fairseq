0563d879245d3c6bb04f2302782c935794635924
Ilia Kulikov <kulikov@fb.com>
2019-08-12 15:36:41 -0700

    ignore files starting with . e.g. .ipynb_checkpoints (#819)
    Summary:
    .ipynb_checkpoints folder in models folders crashed the importlib
    now there is a check for this
    Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/819
    
    Differential Revision: D16772192
    
    Pulled By: myleott
    
    fbshipit-source-id: 01c956aef4ed312bc7645c31c83dbf98af89d931

--- fairseq/models/__init__.py
+++ fairseq/models/__init__.py
@@ -123,7 +123,7 @@ def register_model_architecture(model_name, arch_name):
 models_dir = os.path.dirname(__file__)
 for file in os.listdir(models_dir):
     path = os.path.join(models_dir, file)
-    if not file.startswith('_') and (file.endswith('.py') or os.path.isdir(path)):
+    if not file.startswith('_') and not file.startswith('.') and (file.endswith('.py') or os.path.isdir(path)):
         model_name = file[:file.find('.py')] if file.endswith('.py') else file
         module = importlib.import_module('fairseq.models.' + model_name)
 
