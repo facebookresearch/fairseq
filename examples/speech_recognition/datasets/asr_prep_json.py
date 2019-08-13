27d3120ffb4e9d1578340686145b5cfab491c46a
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2019-08-12 21:23:48 -0700

    Use multiprocessing for asr_prep_json

--- examples/speech_recognition/datasets/asr_prep_json.py
+++ examples/speech_recognition/datasets/asr_prep_json.py
@@ -12,7 +12,6 @@ from itertools import chain
 import argparse
 import os
 import json
-import sentencepiece as spm
 import multiprocessing
 import torchaudio
 
@@ -21,19 +20,34 @@ from fairseq.data import Dictionary
 MILLISECONDS_TO_SECONDS = 0.001
 
 
-def process_sample(aud_path, lable, utt_id, sp, tgt_dict):
-    input = {}
-    output = {}
-    si, ei = torchaudio.info(aud_path)
-    input["length_ms"] = int(si.length / si.channels / si.rate / MILLISECONDS_TO_SECONDS)
-    input["path"] = aud_path
+def process_samples(samples, labels, sp_path, tgt_dict):
+    utts = {}
 
-    token = " ".join(sp.EncodeAsPieces(lable))
-    ids = tgt_dict.encode_line(token, append_eos=False)
-    output["text"] = lable
-    output["token"] = token
-    output["tokenid"] = ', '.join(map(str, [t.tolist() for t in ids]))
-    return {utt_id: {"input": input, "output": output}}
+    import sentencepiece as spm
+    sp = spm.SentencePieceProcessor()
+    sp.Load(sp_path)
+    print("done loading")
+
+    for s in samples:
+        lable = labels[s.utt_id]
+        input = {}
+        output = {}
+        si, ei = torchaudio.info(s.aud_path)
+        input["length_ms"] = int(
+            si.length / si.channels / si.rate / MILLISECONDS_TO_SECONDS)
+        input["path"] = s.aud_path
+
+        token = " ".join(sp.EncodeAsPieces(lable))
+        ids = tgt_dict.encode_line(token, append_eos=False)
+        output["text"] = lable
+        output["token"] = token
+        output["tokenid"] = ', '.join(map(str, [t.tolist() for t in ids]))
+        utts[s.utt_id] = {"input": input, "output": output}
+    print("done processing")
+    return utts
+
+
+Sample = namedtuple('Sample', 'aud_path utt_id')
 
 
 def main():
@@ -44,18 +58,16 @@ def main():
                         help="aggregated input labels with format <ID LABEL> per line",
                         type=argparse.FileType('r', encoding='UTF-8'))
     parser.add_argument("--spm-model", required=True,
-                        help="sentencepiece model to use for encoding",
-                        type=argparse.FileType('r', encoding='UTF-8'))
+                        help="sentencepiece model to use for encoding")
     parser.add_argument("--dictionary", required=True,
                         help="file to load fairseq dictionary from",
                         type=argparse.FileType('r', encoding='UTF-8'))
-    parser.add_argument("--audio-format", choices=["flac", "wav"], default="wav")
+    parser.add_argument(
+        "--audio-format", choices=["flac", "wav"], default="wav")
     parser.add_argument("--output", required=True, type=argparse.FileType('w'),
                         help="path to save json output")
+    parser.add_argument("--num-cpu", default=1, type=int, required=True)
     args = parser.parse_args()
-
-    sp = spm.SentencePieceProcessor()
-    sp.Load(args.spm_model.name)
 
     tgt_dict = Dictionary.load(args.dictionary)
 
@@ -66,22 +78,38 @@ def main():
     if len(labels) == 0:
         raise Exception('No labels found in ', args.labels_path)
 
-    Sample = namedtuple('Sample', 'aud_path utt_id')
     samples = []
+    i = 0
     for path, _, files in chain.from_iterable(os.walk(path) for path in args.audio_dirs):
         for f in files:
             if f.endswith(args.audio_format):
                 if len(os.path.splitext(f)) != 2:
-                    raise Exception('Expect <utt_id.extension> file name. Got: ', f)
+                    raise Exception(
+                        'Expect <utt_id.extension> file name. Got: ', f)
                 utt_id = os.path.splitext(f)[0]
                 if utt_id not in labels:
                     continue
                 samples.append(Sample(os.path.join(path, f), utt_id))
+                i += 1
 
     utts = {}
-    num_cpu = multiprocessing.cpu_count()
-    with concurrent.futures.ThreadPoolExecutor(max_workers=num_cpu) as executor:
-        future_to_sample = {executor.submit(process_sample, s.aud_path, labels[s.utt_id], s.utt_id, sp, tgt_dict): s for s in samples}
+    num_cpu = args.num_cpu
+    chunk_size = int(1 + (len(samples) / num_cpu))
+    print("Using {} CPUs".format(num_cpu))
+    print("start")
+    import time
+    t0 = time.time()
+    with concurrent.futures.ProcessPoolExecutor(max_workers=num_cpu) as executor:
+        # future_to_sample = {executor.submit(
+        #     process_sample, s.aud_path, labels[s.utt_id], s.utt_id, sp, tgt_dict): s for s in samples}
+        samples_chunks = [samples[i:i + chunk_size]
+                          for i in range(0, len(samples), chunk_size)]
+        print("len(samples_chunks): {}".format(len(samples_chunks)))
+        future_to_sample = {executor.submit(
+            process_samples, chunk, labels, args.spm_model, tgt_dict): chunk for chunk in samples_chunks}
+        # process_samples(samples_chunks[0], labels, args.spm_model, tgt_dict)
+        # import sys
+        # sys.exit(1)
         for future in concurrent.futures.as_completed(future_to_sample):
             try:
                 data = future.result()
@@ -90,6 +118,7 @@ def main():
             else:
                 utts.update(data)
     json.dump({"utts": utts}, args.output, indent=4)
+    print("Done: {}".format(time.time() - t0))
 
 
 if __name__ == "__main__":
