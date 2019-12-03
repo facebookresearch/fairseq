Fixing Gradient calculation in BMUF
Summary:
Gradient calculation:
1. Accumulate graidents from all the machines.
2. Divide the gradient by the total samples seen by all the machines.

In DDP: gradients are calculated:
1. They are accumulated from all the machines and divide by the total number of GPUs.
2. Later, they are multiplied by #GPU / #total_sample_size

For BMUF case: graidents are calulated:
1. During non-sync, gradients should be divide by samples seen by the local machine.
2. During bmuf-sync, gradients are accumiulated by bmuf sync which is divided by #GPUs. And, later they are multiplied by #GPU / #total_sample_size

Other small fix:
1. Set average local momentum sync to False.
2. Set nesterov block sync to False.

Reviewed By: jay-mahadeokar

Differential Revision: D18773892

fbshipit-source-id: f7d01713d57a3faeb8670654ec55c99845bab037

fairseq/trainer.py
@@ -425,9 +425,15 @@ class Trainer(object):
         try:
             # normalize grads by sample size
             if sample_size > 0:
-                self.optimizer.multiply_grads(
-                    self.args.distributed_world_size / float(sample_size)
-                )
+                # In DDP: multiply gradients by #GPUs/#sample_size because
+                # gradients are accumulated and divided by #GPUs before.
+                # In BMUF: during non-sync gradients are divided by #sample_size
+                # whereas during sync (while calculating global model): sync accumulate
+                # gradients and divided by #GPUs and now multiply by #GPUs/#sample_size
+                if self._sync_stats():
+                    self.optimizer.multiply_grads(self.args.distributed_world_size / float(sample_size))
+                else:
+                    self.optimizer.multiply_grads(1 / float(sample_size))
 
             # clip grads
             grad_norm = self.optimizer.clip_grad_norm(self.args.clip_norm)
@@ -643,11 +649,14 @@ class Trainer(object):
             torch.cuda.manual_seed(seed)
 
     def _sync_stats(self):
+        # Return True if it's using multiple GPUs and DDP or multiple GPUs with
+        # BMUF and it's a bmuf sync with warmup iterations completed before.
         return self.args.distributed_world_size > 1 and (
             (not self.args.use_bmuf)
             or (
                 self.args.use_bmuf
                 and (self.get_num_updates() + 1) % self.args.global_sync_iter == 0
+                and (self.get_num_updates() + 1) > self.args.warmup_iterations
             )
         )
 