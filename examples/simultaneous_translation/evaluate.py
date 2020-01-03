784a36dc779fed4c44b6bb077491870dc7c7d365
Xutai Ma <xutaima@gmail.com>
2020-01-02 22:31:14 -0800

    Add speech agent in evaluate.py

--- examples/simultaneous_translation/evaluate.py
+++ examples/simultaneous_translation/evaluate.py
@@ -1,23 +1,26 @@
 import argparse
 
 from examples.simultaneous_translation.eval.client import SimulSTEvaluationService
-from examples.simultaneous_translation.eval.agent import DummyWaitAgent, SimulTransAgent
+from examples.simultaneous_translation.eval.agent import DummyWaitAgent, SimulTransAgentBuilder
 
 DEFAULT_HOSTNAME = 'localhost'
 DEFAULT_PORT = 12321
 
-parser = SimulTransAgent.argument_parser()
+parser = argparse.ArgumentParser()
 
 parser.add_argument('--hostname', type=str, default=DEFAULT_HOSTNAME,
                     help='server hostname')
 parser.add_argument('--port', type=int, default=DEFAULT_PORT,
                     help='server port number')
-parser.add_argument('--model-path', type=str, default=None, 
-                    help='path to your pretrained model.')
+parser.add_argument('--data-type', type=str, default="text",
+                    help='Type of data to evaluate')
 
-agent = SimulTransAgent(parser)
+agent_builder = SimulTransAgentBuilder() 
 
-args = agent.args
+parser = agent_builder.add_args(parser)
+args = parser.parse_args()
+
+agent = agent_builder(args)
 
 with SimulSTEvaluationService(args.hostname, args.port) as session:
     agent.decode(session)
