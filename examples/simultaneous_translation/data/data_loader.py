27633389b3b1c1a663f70afd2220de4893d6b83f
Xutai Ma <xutaima@gmail.com>
2020-01-02 22:28:33 -0800

    Add audio dataloader

--- examples/simultaneous_translation/data/data_loader.py
+++ examples/simultaneous_translation/data/data_loader.py
@@ -1,4 +1,6 @@
 import os.path as op
+import json
+import soundfile as sf
 
 class TextDataLoader(object):
     def __init__(self, data_root):
@@ -12,4 +14,14 @@ class TextDataLoader(object):
 
 class AudioDataLoader(object):
     def __init__(self, data_root):
-        raise NotImplementedError
+        self.data_root = data_root
+        self.source = open(data_root)
+
+    def load(self):
+        for sent_id, utterence in enumerate(json.load(self.source)["utts"].values()):
+            wav_path = utterence["input"]["path"]
+            assert op.isfile(wav_path) and wav_path.endswith('.wav')
+            frames_10ms = 16000 // 100
+            wav_blocks = [b.tolist() for b in sf.blocks(wav_path, blocksize=frames_10ms)]
+            yield sent_id, wav_blocks
+        self.source.close()
