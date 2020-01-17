e198482e71838747de75e756d74eef2d39fb85f5
Myle Ott <myleott@fb.com>
2020-01-17 13:04:47 -0800

    Fix binaries in root dir (#995)
    Summary:
    Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/995
    
    The symlinks approach didn't work with `python train.py`.
    
    Differential Revision: D19451900
    
    fbshipit-source-id: 2988eb48077cf8e0e078b9fca527a675132187db

--- generate.py
+++ generate.py
@@ -1,1 +1,11 @@
-fairseq_cli/generate.py
+#!/usr/bin/env python3 -u
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from fairseq_cli.generate import cli_main
+
+
+if __name__ == '__main__':
+    cli_main()
