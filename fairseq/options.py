08dc34095ea5cea696cadc2fd105887a1176f939
Zeming Lin <ebetica0@gmail.com>
2020-01-17 06:28:03 -0800

    Add modify_args to parse_args_and_arch (#983)
    Summary:
    Add modify_args to parse_args_and_arch. We find this useful in using cli_main as a library function. This is necessary since parse_args_and_arch does a double parse of the args, and never gives the intermediate parser for users to modify before parsing args.
    Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/983
    
    Differential Revision: D19392931
    
    Pulled By: myleott
    
    fbshipit-source-id: 1afeffc2143b349e392d38a99a933f72a8e581bd

--- fairseq/options.py
+++ fairseq/options.py
@@ -4,22 +4,23 @@
 # LICENSE file in the root directory of this source tree.
 
 import argparse
+import sys
+from typing import Callable, List, Optional
 
 import torch
-import sys
 
 from fairseq import utils
 from fairseq.data.indexed_dataset import get_available_dataset_impl
 
 
-def get_preprocessing_parser(default_task='translation'):
-    parser = get_parser('Preprocessing', default_task)
+def get_preprocessing_parser(default_task="translation"):
+    parser = get_parser("Preprocessing", default_task)
     add_preprocess_args(parser)
     return parser
 
 
-def get_training_parser(default_task='translation'):
-    parser = get_parser('Trainer', default_task)
+def get_training_parser(default_task="translation"):
+    parser = get_parser("Trainer", default_task)
     add_dataset_args(parser, train=True)
     add_distributed_training_args(parser)
     add_model_args(parser)
@@ -28,8 +29,8 @@ def get_training_parser(default_task='translation'):
     return parser
 
 
-def get_generation_parser(interactive=False, default_task='translation'):
-    parser = get_parser('Generation', default_task)
+def get_generation_parser(interactive=False, default_task="translation"):
+    parser = get_parser("Generation", default_task)
     add_dataset_args(parser, gen=True)
     add_generation_args(parser)
     if interactive:
@@ -37,21 +38,21 @@ def get_generation_parser(interactive=False, default_task='translation'):
     return parser
 
 
-def get_interactive_generation_parser(default_task='translation'):
+def get_interactive_generation_parser(default_task="translation"):
     return get_generation_parser(interactive=True, default_task=default_task)
 
 
-def get_eval_lm_parser(default_task='language_modeling'):
-    parser = get_parser('Evaluate Language Model', default_task)
+def get_eval_lm_parser(default_task="language_modeling"):
+    parser = get_parser("Evaluate Language Model", default_task)
     add_dataset_args(parser, gen=True)
     add_eval_lm_args(parser)
     return parser
 
 
 def get_validation_parser(default_task=None):
-    parser = get_parser('Validation', default_task)
+    parser = get_parser("Validation", default_task)
     add_dataset_args(parser, train=True)
-    group = parser.add_argument_group('Evaluation')
+    group = parser.add_argument_group("Evaluation")
     add_common_eval_args(group)
     return parser
 
@@ -76,7 +77,23 @@ def eval_bool(x, default=False):
         return default
 
 
-def parse_args_and_arch(parser, input_args=None, parse_known=False, suppress_defaults=False):
+def parse_args_and_arch(
+    parser: argparse.ArgumentParser,
+    input_args: List[str] = None,
+    parse_known: bool = False,
+    suppress_defaults: bool = False,
+    modify_parser: Optional[Callable[[argparse.ArgumentParser], None]] = None,
+):
+    """
+    Args:
+        parser (ArgumentParser): the parser
+        input_args (List[str]): strings to parse, defaults to sys.argv
+        parse_known (bool): only parse known arguments, similar to
+            `ArgumentParser.parse_known_args`
+        suppress_defaults (bool): parse while ignoring all default values
+        modify_parser (Optional[Callable[[ArgumentParser], None]]):
+            function to modify the parser, e.g., to set default values
+    """
     if suppress_defaults:
         # Parse args without any default values. This requires us to parse
         # twice, once to identify all the necessary task/model args, and a second
@@ -90,13 +107,14 @@ def parse_args_and_arch(parser, input_args=None, parse_known=False, suppress_def
         suppressed_parser = argparse.ArgumentParser(add_help=False, parents=[parser])
         suppressed_parser.set_defaults(**{k: None for k, v in vars(args).items()})
         args = suppressed_parser.parse_args(input_args)
-        return argparse.Namespace(**{
-            k: v
-            for k, v in vars(args).items()
-            if v is not None
-        })
+        return argparse.Namespace(
+            **{k: v for k, v in vars(args).items() if v is not None}
+        )
 
     from fairseq.models import ARCH_MODEL_REGISTRY, ARCH_CONFIG_REGISTRY
+
+    if modify_parser is not None:
+        modify_parser(parser)
 
     # The parser doesn't know about model/criterion/optimizer-specific args, so
     # we parse twice. First we parse the model/criterion/optimizer, then we
@@ -105,9 +123,9 @@ def parse_args_and_arch(parser, input_args=None, parse_known=False, suppress_def
     args, _ = parser.parse_known_args(input_args)
 
     # Add model-specific args to parser.
-    if hasattr(args, 'arch'):
+    if hasattr(args, "arch"):
         model_specific_group = parser.add_argument_group(
-            'Model-specific configuration',
+            "Model-specific configuration",
             # Only include attributes which are explicitly given as command-line
             # arguments or which have default values.
             argument_default=argparse.SUPPRESS,
@@ -116,19 +134,26 @@ def parse_args_and_arch(parser, input_args=None, parse_known=False, suppress_def
 
     # Add *-specific args to parser.
     from fairseq.registry import REGISTRIES
+
     for registry_name, REGISTRY in REGISTRIES.items():
         choice = getattr(args, registry_name, None)
         if choice is not None:
-            cls = REGISTRY['registry'][choice]
-            if hasattr(cls, 'add_args'):
+            cls = REGISTRY["registry"][choice]
+            if hasattr(cls, "add_args"):
                 cls.add_args(parser)
-    if hasattr(args, 'task'):
+    if hasattr(args, "task"):
         from fairseq.tasks import TASK_REGISTRY
+
         TASK_REGISTRY[args.task].add_args(parser)
-    if getattr(args, 'use_bmuf', False):
+    if getattr(args, "use_bmuf", False):
         # hack to support extra args for block distributed data parallelism
         from fairseq.optim.bmuf import FairseqBMUF
+
         FairseqBMUF.add_args(parser)
+
+    # Modify the parser a second time, since defaults may have been reset
+    if modify_parser is not None:
+        modify_parser(parser)
 
     # Parse a second time.
     if parse_known:
@@ -138,15 +163,15 @@ def parse_args_and_arch(parser, input_args=None, parse_known=False, suppress_def
         extra = None
 
     # Post-process args.
-    if hasattr(args, 'max_sentences_valid') and args.max_sentences_valid is None:
+    if hasattr(args, "max_sentences_valid") and args.max_sentences_valid is None:
         args.max_sentences_valid = args.max_sentences
-    if hasattr(args, 'max_tokens_valid') and args.max_tokens_valid is None:
+    if hasattr(args, "max_tokens_valid") and args.max_tokens_valid is None:
         args.max_tokens_valid = args.max_tokens
-    if getattr(args, 'memory_efficient_fp16', False):
+    if getattr(args, "memory_efficient_fp16", False):
         args.fp16 = True
 
     # Apply architecture configuration.
-    if hasattr(args, 'arch'):
+    if hasattr(args, "arch"):
         ARCH_CONFIG_REGISTRY[args.arch](args)
 
     if parse_known:
@@ -155,11 +180,11 @@ def parse_args_and_arch(parser, input_args=None, parse_known=False, suppress_def
         return args
 
 
-def get_parser(desc, default_task='translation'):
+def get_parser(desc, default_task="translation"):
     # Before creating the true parser, we need to import optional user module
     # in order to eagerly import custom tasks, optimizers, architectures, etc.
     usr_parser = argparse.ArgumentParser(add_help=False, allow_abbrev=False)
-    usr_parser.add_argument('--user-dir', default=None)
+    usr_parser.add_argument("--user-dir", default=None)
     usr_args, _ = usr_parser.parse_known_args()
     utils.import_user_module(usr_args)
 
@@ -216,7 +241,7 @@ def get_parser(desc, default_task='translation'):
 
 
 def add_preprocess_args(parser):
-    group = parser.add_argument_group('Preprocessing')
+    group = parser.add_argument_group("Preprocessing")
     # fmt: off
     group.add_argument("-s", "--source-lang", default=None, metavar="SRC",
                        help="source language")
@@ -262,7 +287,7 @@ def add_preprocess_args(parser):
 
 
 def add_dataset_args(parser, train=False, gen=False):
-    group = parser.add_argument_group('Dataset and data loading')
+    group = parser.add_argument_group("Dataset and data loading")
     # fmt: off
     group.add_argument('--num-workers', default=1, type=int, metavar='N',
                        help='how many subprocesses to use for data loading')
@@ -309,7 +334,7 @@ def add_dataset_args(parser, train=False, gen=False):
 
 
 def add_distributed_training_args(parser):
-    group = parser.add_argument_group('Distributed training')
+    group = parser.add_argument_group("Distributed training")
     # fmt: off
     group.add_argument('--distributed-world-size', type=int, metavar='N',
                        default=max(1, torch.cuda.device_count()),
@@ -351,7 +376,7 @@ def add_distributed_training_args(parser):
 
 
 def add_optimization_args(parser):
-    group = parser.add_argument_group('Optimization')
+    group = parser.add_argument_group("Optimization")
     # fmt: off
     group.add_argument('--max-epoch', '--me', default=0, type=int, metavar='N',
                        help='force stop training at specified epoch')
@@ -378,7 +403,7 @@ def add_optimization_args(parser):
 
 
 def add_checkpoint_args(parser):
-    group = parser.add_argument_group('Checkpointing')
+    group = parser.add_argument_group("Checkpointing")
     # fmt: off
     group.add_argument('--save-dir', metavar='DIR', default='checkpoints',
                        help='path to save checkpoints')
@@ -442,7 +467,7 @@ def add_common_eval_args(group):
 
 
 def add_eval_lm_args(parser):
-    group = parser.add_argument_group('LM Evaluation')
+    group = parser.add_argument_group("LM Evaluation")
     add_common_eval_args(group)
     # fmt: off
     group.add_argument('--output-word-probs', action='store_true',
@@ -459,7 +484,7 @@ def add_eval_lm_args(parser):
 
 
 def add_generation_args(parser):
-    group = parser.add_argument_group('Generation')
+    group = parser.add_argument_group("Generation")
     add_common_eval_args(group)
     # fmt: off
     group.add_argument('--beam', default=5, type=int, metavar='N',
@@ -535,7 +560,7 @@ def add_generation_args(parser):
 
 
 def add_interactive_args(parser):
-    group = parser.add_argument_group('Interactive')
+    group = parser.add_argument_group("Interactive")
     # fmt: off
     group.add_argument('--buffer-size', default=0, type=int, metavar='N',
                        help='read this many sentences into a buffer before processing them')
@@ -545,7 +570,7 @@ def add_interactive_args(parser):
 
 
 def add_model_args(parser):
-    group = parser.add_argument_group('Model configuration')
+    group = parser.add_argument_group("Model configuration")
     # fmt: off
 
     # Model definitions can be found under fairseq/models/
