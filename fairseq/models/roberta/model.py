e6db16b72c6367b7d5d53bd0dd2b61dbe4ae0995
Myle Ott <myleott@fb.com>
2020-01-21 06:53:05 -0800

    Fix logging when finetuning roberta
    Summary: Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/1000
    
    Differential Revision: D19471951
    
    Pulled By: myleott
    
    fbshipit-source-id: 60f5ae5bddaa37f9299da3e79ca6ffc09912ec0c

--- fairseq/models/roberta/model.py
+++ fairseq/models/roberta/model.py
@@ -195,7 +195,7 @@ class RobertaModel(FairseqLanguageModel):
             cur_state = self.classification_heads.state_dict()
             for k, v in cur_state.items():
                 if prefix + 'classification_heads.' + k not in state_dict:
-                    logger.info('Overwriting', prefix + 'classification_heads.' + k)
+                    logger.info('Overwriting ' + prefix + 'classification_heads.' + k)
                     state_dict[prefix + 'classification_heads.' + k] = v
 
 
