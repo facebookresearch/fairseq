3b72cff10704a3c344fb3ea143c101f49ba30fcc
Joshua Meier <jmeier@fb.com>
2020-01-24 13:14:46 -0800

    Standalone LSTM decoder language model (#934)
    Summary:
    Currently, the LSTM models in Fairseq master can only be used in an encoder/decoder setting, for example, in `class LSTMModel(FairseqEncoderDecoderModel)`. This PR adds a standalone LSTM decoder language model.
    
    Changes:
    - adds support for `LSTMDecoder` in cases where an encoder is not present, for instance, where `encoder_output_units=0`.
    - fixes bugs in `LSTMDecoder` that only become apparent when using it in a standalone fashion, for example, not handling `src_lengths` as an optional argument.
    - adds `class LSTMLanguageModel(FairseqLanguageModel)` for training LSTM language models.
    - tests for the `LSTMLanguageModel`. Changes to the `LSTMDecoder` are handled by existing test cases.
    Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/934
    
    Reviewed By: myleott
    
    Differential Revision: D18816310
    
    Pulled By: joshim5
    
    fbshipit-source-id: 4773695a7f5d36aa773da8a45db2e02f76c968a9

--- tests/test_binaries.py
+++ tests/test_binaries.py
@@ -503,6 +503,21 @@ class TestLanguageModeling(unittest.TestCase):
                     '--tokens-per-sample', '500',
                 ])
 
+    def test_lstm_lm(self):
+        with contextlib.redirect_stdout(StringIO()):
+            with tempfile.TemporaryDirectory('test_lstm_lm') as data_dir:
+                create_dummy_data(data_dir)
+                preprocess_lm_data(data_dir)
+                train_language_model(
+                    data_dir, 'lstm_lm', ['--add-bos-token'], run_validation=True,
+                )
+                eval_lm_main(data_dir)
+                generate_main(data_dir, [
+                    '--task', 'language_modeling',
+                    '--sample-break-mode', 'eos',
+                    '--tokens-per-sample', '500',
+                ])
+
 
 class TestMaskedLanguageModel(unittest.TestCase):
 
