22c0c082543840e3e673bc0cf90fac1d9b9bc3b7
Myle Ott <myleott@fb.com>
2020-02-11 09:19:46 -0800

    Only start measuring wps/ups after 10 steps
    Summary: Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/1023
    
    Reviewed By: ngoyal2707
    
    Differential Revision: D19834656
    
    Pulled By: myleott
    
    fbshipit-source-id: d4b701f8404630185253e3e944e76b80fb787f2c

--- fairseq/metrics.py
+++ fairseq/metrics.py
@@ -137,18 +137,25 @@ def log_derived(key: str, fn: Callable[[MetersDict], float], priority: int = 20)
             agg.add_meter(key, MetersDict._DerivedMeter(fn), priority)
 
 
-def log_speed(key: str, value: float, priority: int = 30, round: Optional[int] = None):
+def log_speed(
+    key: str,
+    value: float,
+    ignore_first: int = 0,
+    priority: int = 30,
+    round: Optional[int] = None,
+):
     """Log the rate of some quantity per second.
 
     Args:
         key (str): name of the field to log
         value (float): value to log
+        ignore_first (int): ignore the first few values
         priority (int): smaller values are logged earlier in the output
         round (Optional[int]): number of digits to round to when displaying
     """
     for agg in get_active_aggregators():
         if key not in agg:
-            agg.add_meter(key, TimeMeter(round=round), priority)
+            agg.add_meter(key, TimeMeter(ignore_first=ignore_first, round=round), priority)
             agg[key].reset()  # reset meter on the first call
         else:
             agg[key].update(value)
