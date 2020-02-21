08dcd08d9c442ec2c35bb041356d2b768ffcb922
Myle Ott <myleott@fb.com>
2020-02-21 07:05:18 -0800

    Add utils.with_torch_seed (#1044)
    Summary: Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/1044
    
    Differential Revision: D20030263
    
    Pulled By: myleott
    
    fbshipit-source-id: 374af53646acd6d138227b088e023e168a7ed647

--- fairseq/utils.py
+++ fairseq/utils.py
@@ -406,6 +406,17 @@ def set_torch_seed(seed):
     torch.cuda.manual_seed(seed)
 
 
+@contextlib.contextmanager
+def with_torch_seed(seed):
+    assert isinstance(seed, int)
+    rng_state = torch.get_rng_state()
+    cuda_rng_state = torch.cuda.get_rng_state()
+    set_torch_seed(seed)
+    yield
+    torch.set_rng_state(rng_state)
+    torch.cuda.set_rng_state(cuda_rng_state)
+
+
 def parse_alignment(line):
     """
     Parses a single line from the alingment file.
