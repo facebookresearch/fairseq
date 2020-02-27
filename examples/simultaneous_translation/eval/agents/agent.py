e34a865ec8104b666f7a1a2388f2c02cf07d3ef7
Xutai Ma <xutaima@gmail.com>
2020-02-26 21:07:53 -0800

    make number of worker configurable

--- examples/simultaneous_translation/eval/agents/agent.py
+++ examples/simultaneous_translation/eval/agents/agent.py
@@ -21,18 +21,22 @@ class Agent(object):
     def reset(self):
         raise NotImplementedError
         
-    def decode(self, session, low=0, high=100000):
+    def decode(self, session, low=0, high=100000, num_thread=10):
         corpus_info = session.get_src()
         high = min(corpus_info["num_sentences"] - 1, high)
         if low >= high:
             return
 
         t0 = time.time()
-        with Pool(10) as p:
-            p.map(
-                partial(self._decode_one, session),
-                [sent_id for sent_id in range(low, high + 1)]
-            )
+        if num_thread > 1:
+            with Pool(10) as p:
+                p.map(
+                    partial(self._decode_one, session),
+                    [sent_id for sent_id in range(low, high + 1)]
+                )
+        else:
+            for sent_id in range(low, high + 1):
+                self._decode_one(session, sent_id)
 
         print(f'Finsh {low} to {high} in {time.time() - t0}s')
 
