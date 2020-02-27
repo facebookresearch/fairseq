ad02b451980bbcbd6d19923cbeae5d0feaf7950b
Xutai Ma <xutaima@gmail.com>
2020-02-26 21:12:58 -0800

    add must c en_de speech dev set configs

---
+++ examples/simultaneous_translation/scripts/configs/must-c-en_de-speech-dev.sh
@@ -0,0 +1,9 @@
+export src="/private/home/xutaima/experiments/ast/must-c/must_c_1_0/en-de/unigram-de-10000-3000/dev.json"
+export tgt="/private/home/xutaima/experiments/ast/must-c/must_c_1_0/en-de/unigram-de-10000-3000/dev.json"
+export tokenizer=13a
+export src_splitter_type=SentencePieceModel
+export tgt_splitter_type=SentencePieceModel
+export src_splitter_path=/private/home/xutaima/experiments/ast/must-c/must_c_1_0/en-de/unigram-en-10000-3000/spm.model
+export tgt_splitter_path=/private/home/xutaima/experiments/ast/must-c/must_c_1_0/en-de/unigram-de-10000-3000/spm.model
+export data_bin=/private/home/xutaima/experiments/ast/must-c/must_c_1_0/en-de/data-bin/mustc_en_de
+export scorer_type=speech
