af99ca29306b95d6c41d455fbcf7b336be277bd0
Xutai Ma <xutaima@gmail.com>
2020-02-29 18:27:49 -0800

    add iwslt15 en-vi config

---
+++ examples/simultaneous_translation/scripts/configs/iwslt15envi-test.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+export data_bin=./experiments/data/iwslt15.tokenized.en-vi/data-bin/iwslt15.tokenized.en-vi
+export src=./experiments/data/iwslt15.tokenized.en-vi/tst2013.en
+export tgt=./experiments/data/iwslt15.tokenized.en-vi/tst2013.vi
+export agent_type="simul_trans_text"
+export tokenizer="none"
+export src_splitter_type="None"
+export src_splitter_path="None"
+export tgt_splitter_type="None"
+export tgt_splitter_path="None"
+export scorer_type="text"
+export agent_type="simul_trans_text"
