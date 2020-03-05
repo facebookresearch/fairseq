cac23f4f057c4d2e5bec698a1f794fa47f734702
Zoe Ma <mazhiyi@fb.com>
2020-03-05 05:51:50 -0800

    Fix overflow nandetector (#1064)
    Summary:
    https://github.com/fairinternal/fairseq-py/issues/1029 added update_num as a required argument to train_step function. Adding this argument to the function call for nan detector.
    
    Before I got:
    ```
    Traceback (most recent call last):
      File "/private/home/mazhiyi/.conda/envs/fairseq-master/lib/python3.6/site-packages/torch/multiprocessing/spawn.py", line 19, in _wrap
        fn(i, *args)
      File "/private/home/mazhiyi/master/fairseq-py-docrepair/fairseq_cli/train.py", line 281, in distributed_main
        main(args, init_distributed=True)
      File "/private/home/mazhiyi/master/fairseq-py-docrepair/fairseq_cli/train.py", line 100, in main
        train(args, trainer, task, epoch_itr)
      File "/private/home/mazhiyi/.conda/envs/fairseq-master/lib/python3.6/contextlib.py", line 52, in inner
        return func(*args, **kwds)
      File "/private/home/mazhiyi/master/fairseq-py-docrepair/fairseq_cli/train.py", line 176, in train
        log_output = trainer.train_step(samples)
      File "/private/home/mazhiyi/.conda/envs/fairseq-master/lib/python3.6/contextlib.py", line 52, in inner
        return func(*args, **kwds)
      File "/private/home/mazhiyi/master/fairseq-py-docrepair/fairseq/trainer.py", line 405, in train_step
        ignore_grad=False
    TypeError: train_step() missing 1 required positional argument: 'update_num'
    ```
    Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/1064
    
    Differential Revision: D20249234
    
    Pulled By: mazhiyi
    
    fbshipit-source-id: 0ee85c719a17cc253cbdc7183dc4243193b08d17

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -401,7 +401,7 @@ class Trainer(object):
             # re-run the forward and backward pass with hooks attached to print out where it fails
             with NanDetector(self.model):
                 self.task.train_step(
-                    sample, self.model, self.criterion, self.optimizer,
+                    sample, self.model, self.criterion, self.optimizer, self.get_num_updates(),
                     ignore_grad=False
                 )
             raise
