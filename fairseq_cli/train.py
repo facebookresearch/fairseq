b5bab07bfb5b6fe49ea3a1d039f431cb3604c58d
freewym <freewym@gmail.com>
2020-03-09 14:43:57 -0700

    fix epoch check for curriculum learning (#1798)
    Summary:
    # Before submitting
    
    - [ ] Was this discussed/approved via a Github issue? (no need for typos, doc improvements)
    - Did you read the [contributor guideline](https://github.com/pytorch/fairseq/blob/master/CONTRIBUTING.md)?
    - [ ] Did you make sure to update the docs?
    - Did you write any new necessary tests?
    
    ## What does this PR do?
    Fixes epoch check for curriculum learning. `epoch_itr.epoch` has not been incremented before calling `next_epoch_itr()`. So the check for curriculum learning should look like the proposed fix.
    
    ## PR review
    Anyone in the community is free to review the PR once the tests have passed.
    If we didn't discuss your PR in Github issues there's a high chance it will not be merged.
    
    ## Did you have fun?
    Make sure you had fun coding �
    Pull Request resolved: https://github.com/pytorch/fairseq/pull/1798
    
    Differential Revision: D20330784
    
    Pulled By: myleott
    
    fbshipit-source-id: 2d78c70ce84a7ef054a2f700184e71ac1011d165

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -89,11 +89,7 @@ def main(args, init_distributed=False):
     valid_subsets = args.valid_subset.split(',')
     while (
         lr > args.min_lr
-        and (
-            epoch_itr.epoch <= max_epoch
-            # allow resuming training from the final checkpoint
-            or epoch_itr._next_epoch_itr is not None
-        )
+        and epoch_itr.next_epoch_idx <= max_epoch
         and trainer.get_num_updates() < max_update
     ):
         # train for one epoch
@@ -117,7 +113,7 @@ def main(args, init_distributed=False):
             break
 
         epoch_itr = trainer.get_train_iterator(
-            epoch_itr.epoch,
+            epoch_itr.next_epoch_idx,
             # sharded data: get train iterator for next epoch
             load_dataset=(os.pathsep in getattr(args, 'data', '')),
         )
@@ -148,7 +144,7 @@ def train(args, trainer, task, epoch_itr):
     # Initialize data iterator
     itr = epoch_itr.next_epoch_itr(
         fix_batches_to_gpus=args.fix_batches_to_gpus,
-        shuffle=(epoch_itr.epoch > args.curriculum),
+        shuffle=(epoch_itr.next_epoch_idx > args.curriculum),
     )
     update_freq = (
         args.update_freq[epoch_itr.epoch - 1]
