d647e2e7a3e2368e6481be4fac8b81768013776c
David Příhoda <david.prihoda1@merck.com>
2020-03-12 19:00:53 +0100

    Add support for multiple regression targets in sentence prediction

--- fairseq/tasks/sentence_prediction.py
+++ fairseq/tasks/sentence_prediction.py
@@ -181,9 +181,14 @@ class SentencePredictionTask(FairseqTask):
         else:
             label_path = "{0}.label".format(get_path('label', split))
             if os.path.exists(label_path):
+                def parse_regression_target(i, line):
+                    values = line.split()
+                    assert len(values) == self.args.num_classes, \
+                        f'expected num_classes={self.args.num_classes} regression target values on line {i}, found: "{line}"'
+                    return [float(x) for x in values]
                 dataset.update(
                     target=RawLabelDataset([
-                        float(x.strip()) for x in open(label_path).readlines()
+                        parse_regression_target(i, line.strip()) for i, line in enumerate(open(label_path).readlines())
                     ])
                 )
 
