13c540e9f9c57bd7b83c052201395ddae73b47ac
Changhan Wang <changhan@fb.com>
2020-03-31 09:49:49 -0700

    fix VGGTransformer
    Summary: A recent change in `fairseq.modules.transformer_layer.TransformerDecoderLayer.forward()` return type broke L659. Specifically, the return type is now either a tuple of 2 or a tuple of 3, while L659 supposed only the latter. This diff provides a fix to this.
    
    Reviewed By: myleott
    
    Differential Revision: D20747601
    
    fbshipit-source-id: 966040569f07bf41325f517e6c6a41675fc3a52d

--- examples/speech_recognition/models/vggtransformer.py
+++ examples/speech_recognition/models/vggtransformer.py
@@ -656,7 +656,7 @@ class TransformerDecoder(FairseqIncrementalDecoder):
         # decoder layers
         for layer in self.layers:
             if isinstance(layer, TransformerDecoderLayer):
-                x, _, _ = layer(
+                x, *_ = layer(
                     x,
                     (encoder_out["encoder_out"] if encoder_out is not None else None),
                     (
