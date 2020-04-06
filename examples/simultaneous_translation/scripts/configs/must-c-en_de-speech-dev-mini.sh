385bf7f06be02e7857ed3bf9ff5e7ac0b634c99f
Xutai Ma <xutaima@gmail.com>
2020-04-06 08:03:43 -0700

    minor updates

--- examples/simultaneous_translation/scripts/configs/must-c-en_de-speech-dev-mini.sh
+++ examples/simultaneous_translation/scripts/configs/must-c-en_de-speech-dev-mini.sh
@@ -1,5 +1,5 @@
-export src="./experiments/data/must_c_1_0/en-de/unigram-de-10000-3000/dev.json"
-export tgt="./experiments/data/must_c_1_0/en-de/unigram-de-10000-3000/dev.json"
+export src="./experiments/data/must_c_1_0/en-de/unigram-de-10000-3000/dev.10.json"
+export tgt="./experiments/data/must_c_1_0/en-de/unigram-de-10000-3000/dev.10.json"
 export tokenizer=13a
 export src_splitter_type=SentencePieceModel
 export tgt_splitter_type=SentencePieceModel
--- examples/simultaneous_translation/scripts/configs/must-c-en_de-speech-test.sh
+++ examples/simultaneous_translation/scripts/configs/must-c-en_de-speech-test.sh
@@ -8,3 +8,4 @@ export tgt_splitter_path=./experiments/data/must_c_1_0/en-de/unigram-de-10000-30
 export data_bin=./experiments/data/must_c_1_0/en-de/unigram-de-10000-3000
 export scorer_type=speech
 export agent_type=simul_trans_speech
+export split=test
