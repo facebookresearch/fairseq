2dc878958fc8ddf2bd2cf49c356218a6bc566cdc
Myle Ott <myleott@fb.com>
2020-04-07 14:34:53 -0700

    Fix breakage after upstream change in sacrebleu (#1137)
    Summary: Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/1137
    
    Reviewed By: ngoyal2707
    
    Differential Revision: D20895317
    
    Pulled By: myleott
    
    fbshipit-source-id: 39242263c832c303f27fa7ef1e48005010cb61be

--- fairseq/tasks/translation.py
+++ fairseq/tasks/translation.py
@@ -368,5 +368,7 @@ class TranslationTask(FairseqTask):
         if self.args.eval_bleu_print_samples:
             logger.info('example hypothesis: ' + hyps[0])
             logger.info('example reference: ' + refs[0])
-        tokenize = sacrebleu.DEFAULT_TOKENIZER if not self.args.eval_tokenized_bleu else 'none'
-        return sacrebleu.corpus_bleu(hyps, [refs], tokenize=tokenize)
+        if self.args.eval_tokenized_bleu:
+            return sacrebleu.corpus_bleu(hyps, [refs], tokenize='none')
+        else:
+            return sacrebleu.corpus_bleu(hyps, [refs])
