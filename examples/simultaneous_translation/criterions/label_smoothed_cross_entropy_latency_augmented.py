fa39653f8a0db6f9f2064603bb94af4ce13d857a
Xutai Ma <xutaima@gmail.com>
2020-04-28 00:33:32 -0700

    refactored criterion

--- examples/simultaneous_translation/criterions/label_smoothed_cross_entropy_latency_augmented.py
+++ examples/simultaneous_translation/criterions/label_smoothed_cross_entropy_latency_augmented.py
@@ -5,8 +5,6 @@
 # the root directory of this source tree. An additional grant of patent rights
 # can be found in the PATENTS file in the same directory.
 
-from fairseq import utils
-
 from fairseq.criterions import register_criterion
 from fairseq.criterions.label_smoothed_cross_entropy import (
     LabelSmoothedCrossEntropyCriterion
@@ -15,6 +13,7 @@ from fairseq.criterions.label_smoothed_cross_entropy import (
 from examples.simultaneous_translation.utils.latency import (
     LatencyTraining
 )
+
 
 @register_criterion('latency_augmented_label_smoothed_cross_entropy')
 class LatencyAugmentedLabelSmoothedCrossEntropyCriterion(
@@ -51,68 +50,45 @@ class LatencyAugmentedLabelSmoothedCrossEntropyCriterion(
         """Add criterion-specific arguments to the parser."""
         # fmt: off
         parser.add_argument("--latency-weight-avg", default=0., type=float, metavar='D',
-                            help="")
+                            help="Average loss weight")
         parser.add_argument("--latency-weight-var", default=0., type=float, metavar='D',
-                            help="")
+                            help="Variance loss weight")
         parser.add_argument("--latency-weight-avg-type", default="differentiable_average_lagging",
-                            help="")
+                            help="Statistics for Average loss type")
         parser.add_argument("--latency-weight-var-type", default="variance_delay",
-                            help="")
-        parser.add_argument("--latency-weight-non-trivial", default=0., type=float, metavar='D',
-                            help="")
+                            help="Statistics for variance loss type")
         parser.add_argument("--average-method", default="weighted_average",
-                            help="")
+                            help="Average loss type")
         parser.add_argument("--var-power", type=float, default=2.0,
-                            help="")
+                            help="Variance loss power")
         parser.add_argument("--var-span", type=float, default=1.0,
-                            help="")
+                            help="Variance loss span")
         # fmt: on
 
-    def forward(self, model, sample, reduce=True):
-        """Compute the loss for the given sample.
-
-        Returns a tuple with three elements:
-        1) the loss
-        2) the sample size, which is used as the denominator for the gradient
-        3) logging outputs to display while training
-        """
-        net_output = model(**sample['net_input'])
-        loss, nll_loss, latency_loss = self.compute_loss(model, net_output, sample, reduce=reduce)
-        sample_size = sample['target'].size(0) if self.args.sentence_avg else sample['ntokens']
-        logging_output = {
-            'loss': utils.item(loss.data) if reduce else loss.data,
-            'nll_loss': utils.item(nll_loss.data) if reduce else nll_loss.data,
-            'ntokens': sample['ntokens'],
-            'nsentences': sample['target'].size(0),
-            'sample_size': sample_size,
-            'latency_loss': latency_loss
-        }
-        return loss, sample_size, logging_output
-
     def compute_loss(self, model, net_output, sample, reduce=True):
+        # Compute cross entropy loss first
         loss, nll_loss = super().compute_loss(model, net_output, sample, reduce)
-        if type(net_output[-1]) is dict:
-            attn_list = [item["alpha"] for item in net_output[-1]["attn_list"]]
 
-            target_padding_mask = (
-                model
-                .get_targets(sample, net_output)
-                .eq(self.padding_idx)
-            )
-            source_padding_mask = (
-                net_output[-1].get("encoder_padding_mask", None)
-            )
+        # Obtain the expected alignment
+        attn_list = [item["alpha"] for item in net_output[-1]["attn_list"]]
 
-            latency_loss = self.latency_train.loss(
-                attn_list,
-                source_padding_mask,
-                target_padding_mask,
-            )
-            loss += latency_loss
+        target_padding_mask = (
+            model
+            .get_targets(sample, net_output)
+            .eq(self.padding_idx)
+        )
 
-        else:
-            latency_term = net_output[-1]
-            latency_loss = self.latency_weight_avg * latency_term.sum()
-            loss += latency_loss
+        source_padding_mask = (
+            net_output[-1].get("encoder_padding_mask", None)
+        )
+
+        # Get latency loss
+        latency_loss = self.latency_train.loss(
+            attn_list,
+            source_padding_mask,
+            target_padding_mask,
+        )
+
+        loss += latency_loss
 
         return loss, nll_loss, latency_loss
