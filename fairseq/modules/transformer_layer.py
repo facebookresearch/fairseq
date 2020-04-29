684849d664ceb4c381bad43c4a7f1a83cec7b48a
Ning Dong <dnn@fb.com>
2020-04-29 14:36:59 -0700

    Override reorder_incremental_states in fairseq transformer (#2073)
    Summary:
    Pull Request resolved: https://github.com/pytorch/fairseq/pull/2073
    
    We identified issues in TorchScript builtin hasattr occasionally has inconsistency so reorder_incremental_state is not always triggered in exported model. We had these overrides once before named_module / id were supported in TorchScript and Chen removed in D20896200. Plan to have these until hasattr issue gets fixed.
    
    With this we observed ~1 bleu score regression is gone, see test plan.
    
    Reviewed By: jhcross
    
    Differential Revision: D21273014
    
    fbshipit-source-id: bcf4c8dd8d81a02c108c21097116461f4e2046a0

--- fairseq/modules/transformer_layer.py
+++ fairseq/modules/transformer_layer.py
@@ -379,6 +379,18 @@ class TransformerDecoderLayer(nn.Module):
     def make_generation_fast_(self, need_attn: bool = False, **kwargs):
         self.need_attn = need_attn
 
+    @torch.jit.export
+    def reorder_incremental_state(
+        self,
+        incremental_state: Dict[str, Dict[str, Optional[Tensor]]],
+        new_order: Tensor,
+    ):
+        """Scriptable reorder incremental state in transformer layers."""
+        self.self_attn.reorder_incremental_state(incremental_state, new_order)
+
+        if self.encoder_attn is not None:
+            self.encoder_attn.reorder_incremental_state(incremental_state, new_order)
+
 
 def Linear(in_features, out_features, bias=True):
     m = nn.Linear(in_features, out_features, bias)
