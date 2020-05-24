703eb9ae7579846dbe112e45fac0c789927c27ad
Myle Ott <myleott@fb.com>
2020-05-23 19:37:11 -0700

    Small usability improvements for huggingface models

--- fairseq/models/huggingface/__init__.py
+++ fairseq/models/huggingface/__init__.py
@@ -3,4 +3,18 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .hf_gpt2 import *  # noqa
+import importlib
+import os
+
+
+# automatically import any Python files in the models/huggingface/ directory
+models_dir = os.path.dirname(__file__)
+for file in os.listdir(models_dir):
+    path = os.path.join(models_dir, file)
+    if (
+        not file.startswith('_')
+        and not file.startswith('.')
+        and (file.endswith('.py') or os.path.isdir(path))
+    ):
+        model_name = file[:file.find('.py')] if file.endswith('.py') else file
+        module = importlib.import_module('fairseq.models.huggingface.' + model_name)
