db7701398fce53f1aa43042d51a2560af2d9119f
Xu Song <xusong.vip@gmail.com>
2020-05-27 06:23:49 -0700

    refactor superclass of MaskedLMModel (#2170)
    Summary:
    masked_lm is actually encoder-only model
    
    # Before submitting
    
    - [ ] Was this discussed/approved via a Github issue? (no need for typos, doc improvements)
    - [x] Did you read the [contributor guideline](https://github.com/pytorch/fairseq/blob/master/CONTRIBUTING.md)?
    - [x] Did you make sure to update the docs?
    - [ ] Did you write any new necessary tests?
    
    ## What does this PR do?
    Class Refactor
    
    ## Did you have fun?
    Make sure you had fun coding �
    Pull Request resolved: https://github.com/pytorch/fairseq/pull/2170
    
    Reviewed By: ngoyal2707
    
    Differential Revision: D21725071
    
    Pulled By: myleott
    
    fbshipit-source-id: 75fd36008f3e3425f8f5180472734394046dfb77

--- fairseq/models/masked_lm.py
+++ fairseq/models/masked_lm.py
@@ -11,7 +11,7 @@ import torch.nn.functional as F
 
 from fairseq import utils
 from fairseq.models import (
-    BaseFairseqModel,
+    FairseqEncoderModel,
     FairseqEncoder,
     register_model,
     register_model_architecture,
@@ -28,15 +28,14 @@ logger = logging.getLogger(__name__)
 
 
 @register_model('masked_lm')
-class MaskedLMModel(BaseFairseqModel):
+class MaskedLMModel(FairseqEncoderModel):
     """
     Class for training a Masked Language Model. It also supports an
     additional sentence level prediction if the sent-loss argument is set.
     """
     def __init__(self, args, encoder):
-        super().__init__()
+        super().__init__(encoder)
         self.args = args
-        self.encoder = encoder
 
         # if specified then apply bert initialization on the model. We need
         # to explictly call this to make sure that the output embeddings
