16e9661bd968cf66b02d7870c038d7219da3a5b9
Mandeep Baines <msb@fb.com>
2020-07-08 14:46:02 -0700

    avoid fp16 unscales and multiply_grads (#1201)
    Summary:
    The object of this patch is to avoid fp16 unscale calls which can
    potentially under/over-flow by 1) scaling grad_narm instead of
    unscaling grads before calculating grad_norm and 2) using scale
    argument to step (if supported by optimizer). By letting the
    optimizer scale we avoid multiply_grads (saving on GPU compute/mem).
    We also get better precision since the unscale occurs in the kernel
    resulting in an FP32 unscaled grad instead of an FP16 unscaled grad.
    
    A side-effect of this patch is a noticeable WPS win due to a
    multi-tensor kernel being used for grad_norm and because we
    avoid multiply_grads.
    
    Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/1201
    
    Test Plan:
    Verified grad_norm and loss before and after.
    
    Before:
    
    epoch 001 | loss 19.506 | ppl 744403 | wps 13966.7 | ups 0.21 | wpb 65536 | bsz 128 | num_updates 50 | lr 6.34875e-06 | gnorm 8.173 | loss_scale 10 | train_wall 250 | wall 259
    
    After:
    
    epoch 001 | loss 19.506 | ppl 744363 | wps 14003 | ups 0.21 | wpb 65536 | bsz 128 | num_updates 50 | lr 6.34875e-06 | gnorm 8.173 | loss_scale 10 | train_wall 250 | wall 258
    
    # Before submitting
    
    - [ ] Was this discussed/approved via a Github issue? (no need for typos, doc improvements)
    - [ ] Did you read the [contributor guideline](https://github.com/pytorch/fairseq/blob/master/CONTRIBUTING.md)?
    - [ ] Did you make sure to update the docs?
    - [ ] Did you write any new necessary tests?
    
    ## What does this PR do?
    Fixes # (issue).
    
    ## PR review
    Anyone in the community is free to review the PR once the tests have passed.
    If we didn't discuss your PR in Github issues there's a high chance it will not be merged.
    
    ## Did you have fun?
    Make sure you had fun coding �
    
    Reviewed By: myleott
    
    Differential Revision: D22251842
    
    Pulled By: msbaines
    
    fbshipit-source-id: e6d82cdd3c95e7770835abe054db4b50e6ad569e

--- fairseq/utils.py
+++ fairseq/utils.py
@@ -23,6 +23,12 @@ from fairseq.modules import gelu, gelu_accurate
 from fairseq.modules.multihead_attention import MultiheadAttention
 from torch import Tensor
 
+try:
+    from amp_C import multi_tensor_l2norm
+    multi_tensor_l2norm_available = True
+except ImportError:
+    multi_tensor_l2norm_available = False
+
 
 logger = logging.getLogger(__name__)
 
@@ -250,6 +256,30 @@ def item(tensor):
     return tensor
 
 
+def multi_tensor_total_norm(grads, chunk_size=2048*32) -> torch.Tensor:
+    per_device_grads = {}
+    norms = []
+    for grad in grads:
+        device = grad.device
+        cur_device_grads = per_device_grads.get(device)
+        if cur_device_grads is None:
+            cur_device_grads = []
+            per_device_grads[device] = cur_device_grads
+        cur_device_grads.append(grad)
+    for device in per_device_grads.keys():
+        cur_device_grads = per_device_grads[device]
+        if device.type == "cuda":
+            # TODO(msb) return has_inf
+            has_inf = torch.zeros((1, 1), dtype=torch.int, device=device)
+            with torch.cuda.device(device):
+                norm = multi_tensor_l2norm(chunk_size, has_inf, [cur_device_grads], False)
+                norms.append(norm[0])
+        else:
+            norms += [torch.norm(g, p=2, dtype=torch.float32) for g in cur_device_grads]
+    total_norm = torch.norm(torch.stack(norms))
+    return total_norm
+
+
 def clip_grad_norm_(params, max_norm, aggregate_norm_fn=None) -> torch.Tensor:
     if isinstance(params, torch.Tensor):
         params = [params]
@@ -264,9 +294,16 @@ def clip_grad_norm_(params, max_norm, aggregate_norm_fn=None) -> torch.Tensor:
     if len(grads) == 1:
         total_norm = torch.norm(grads[0], p=2, dtype=torch.float32)
     else:
-        total_norm = torch.norm(
-            torch.stack([torch.norm(g, p=2, dtype=torch.float32) for g in grads])
-        )
+        if multi_tensor_l2norm_available:
+            total_norm = multi_tensor_total_norm(grads)
+        else:
+            warnings.warn(
+                "amp_C fused kernels unavailable, disabling multi_tensor_l2norm; "
+                "you may get better performance by installing NVIDIA's apex library"
+            )
+            total_norm = torch.norm(
+                torch.stack([torch.norm(g, p=2, dtype=torch.float32) for g in grads])
+            )
 
     if aggregate_norm_fn is not None:
         total_norm = aggregate_norm_fn(total_norm)
