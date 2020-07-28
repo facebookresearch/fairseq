df5bf427348f8a9ccd8dbfbd70362648b82b6ae5
Aditya Pillai <adityapillai@fb.com>
2020-07-28 14:53:45 -0700

    Create safe num_updates access on retrying failed training
    Summary:
    The num_updates reference is unsafe since it is only defined in the for-loop without any assertion that the loop will execute at least once. We could either try to add this assertion to make it more clear as to what is happening, or we could set num_updates to some default value if the loop is never executed.
    
    I'm open to doing either one.
    
    Reviewed By: chtran
    
    Differential Revision: D22598029
    
    fbshipit-source-id: 4d20b6a415a783fe8c450f3998b7767861da8c06

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -209,6 +209,7 @@ def train(args, trainer, task, epoch_itr):
 
     valid_subsets = args.valid_subset.split(",")
     should_stop = False
+    num_updates = trainer.get_num_updates()
     for i, samples in enumerate(progress):
         with metrics.aggregate("train_inner"), torch.autograd.profiler.record_function(
             "train_step-%d" % i
