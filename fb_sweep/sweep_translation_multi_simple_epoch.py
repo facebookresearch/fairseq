2d0b0cbe094fc2e4fc1419fe8916507b60aae3b2
Yuqing Tang <yuqtang@fb.com>
2020-07-31 18:44:05 -0700

    Update sweep_translation_multi_simple_epoch.py to support more options
    Summary: Enable more options to be passed from commandlines, e.g. lang-dict, arch, max-tokens and others
    
    Reviewed By: akinh
    
    Differential Revision: D22752388
    
    fbshipit-source-id: 159eb7a7fba1dc37dad0516efc463fea60a28815

--- fb_sweep/sweep_translation_multi_simple_epoch.py
+++ fb_sweep/sweep_translation_multi_simple_epoch.py
@@ -7,9 +7,31 @@
 import sweep
 from sweep import hyperparam
 
+PREDIFINED_GRID_FUNCTION = {}
+
+
+def register_grid(name):
+    def register_grid_func(fn):
+        if name not in PREDIFINED_GRID_FUNCTION:
+            PREDIFINED_GRID_FUNCTION[name] = fn
+        return fn
+    return register_grid_func
+
+
+def get_predefined_grid(name):
+    if name not in PREDIFINED_GRID_FUNCTION:
+        return []
+    else:
+        return PREDIFINED_GRID_FUNCTION[name]()
+
 
 def add_extra_options_func(parser):
     parser.add_argument('--max-update', help='max update', default=40000)
+
+    parser.add_argument('--lang-dict', help='a file containing a list of languages to support', type=str, default=None)
+    parser.add_argument('--max-tokens', help='max tokens per batch', type=int, default=None)
+    parser.add_argument('--arch', default='transformer')
+    parser.add_argument('--task', default='translation_multi_simple_epoch')
 
     parser.add_argument('--lang-pairs', help='lang pairs for multilingual training', type=str)
     parser.add_argument('--sampling-method', help='sampling method', default='temperature')
@@ -24,18 +46,69 @@ def add_extra_options_func(parser):
     # per token learning should be approximately constant;
     # ideally momentent and 2nd momentent of adam should be adjusted accordingly but less important
     parser.add_argument('--lr', default=10e-4)
+    parser.add_argument('--ddp-backend', default=None, )
+
+
+@register_grid('transformer_16_16')
+def get_transformer_16_16_grid():
+    return [
+        hyperparam('--arch', "transformer", save_dir_key=lambda val: val),
+
+        hyperparam('--share-all-embeddings', True, binary_flag=True, save_dir_key=lambda val: 'shem'),
+        hyperparam('--encoder-layers', 16, save_dir_key=lambda val: f'ELS{val}'),
+        hyperparam('--decoder-layers', 16, save_dir_key=lambda val: f'DLS{val}'),
+        # this is a multiplier of embed dim
+        hyperparam('--encoder-ffn-embed-dim', 4 * 1024, save_dir_key=lambda val: f'encffnx{val}'),
+        hyperparam('--decoder-ffn-embed-dim', 4 * 1024, save_dir_key=lambda val: f'decffnx{val}'),
+
+        hyperparam('--encoder-embed-dim', 1024, save_dir_key=lambda val: f'E{val}'),
+        hyperparam('--decoder-embed-dim', 1024),
+
+        hyperparam('--encoder-attention-heads', 16, save_dir_key=lambda val: f'H{val}'),
+        hyperparam('--decoder-attention-heads', 16),
+
+        hyperparam('--encoder-normalize-before', True, binary_flag=True, save_dir_key=lambda _: 'NBF'),
+        hyperparam('--decoder-normalize-before', True, binary_flag=True),
+
+        hyperparam('--attention-dropout', 0.1, save_dir_key=lambda val: f'ATTDRP{val}'),
+        hyperparam('--relu-dropout', 0.0, save_dir_key=lambda val: f'RELDRP{val}'),
+    ]
+
+
+@register_grid('transformer_12_12')
+def get_transformer_12_12_grid():
+    return [
+        hyperparam('--arch', "transformer", save_dir_key=lambda val: val),
+        hyperparam('--share-all-embeddings', True, binary_flag=True, save_dir_key=lambda val: 'shem'),
+        hyperparam('--encoder-layers', 12, save_dir_key=lambda val: f'ELS{val}'),
+        hyperparam('--decoder-layers', 12, save_dir_key=lambda val: f'DLS{val}'),
+        # this is a multiplier of embed dim
+        hyperparam('--encoder-ffn-embed-dim', 4 * 1024, save_dir_key=lambda val: f'encffnx{val}'),
+        hyperparam('--decoder-ffn-embed-dim', 4 * 1024, save_dir_key=lambda val: f'decffnx{val}'),
+
+        hyperparam('--encoder-embed-dim', 1024, save_dir_key=lambda val: f'E{val}'),
+        hyperparam('--decoder-embed-dim', 1024),
+
+        hyperparam('--encoder-attention-heads', 16, save_dir_key=lambda val: f'H{val}'),
+        hyperparam('--decoder-attention-heads', 16),
+
+        hyperparam('--encoder-normalize-before', True, binary_flag=True, save_dir_key=lambda _: 'NBF'),
+        hyperparam('--decoder-normalize-before', True, binary_flag=True),
+
+        hyperparam('--attention-dropout', 0.1, save_dir_key=lambda val: f'ATTDRP{val}'),
+        hyperparam('--relu-dropout', 0.0, save_dir_key=lambda val: f'RELDRP{val}'),
+    ]
 
 
 def get_grid(args):
     max_update = args.max_update
-    task = 'translation_multi_simple_epoch'
+    task = args.task
     sampling_method = args.sampling_method
     sampling_temperature = args.sampling_temperature
     encoder_langtok = args.encoder_langtok
 
     grids = [
         hyperparam('--fp16', save_dir_key=lambda val: 'fp16'),
-        hyperparam('--ddp-backend', 'no_c10d', save_dir_key=lambda val: 'no_c10d'),
         hyperparam('--max-update', max_update),
 
         hyperparam('--update-freq', args.update_freq),
@@ -50,7 +123,6 @@ def get_grid(args):
                 '--sampling-temperature',
                 sampling_temperature,
                 save_dir_key=lambda val: f'tmp{val}'),
-        hyperparam('--arch', 'transformer', save_dir_key=lambda val: val),
         hyperparam('--share-all-embeddings', [True], binary_flag=True, save_dir_key=lambda val: 'shareemb'),
 
         hyperparam('--optimizer', 'adam', save_dir_key=lambda val: val),
@@ -74,6 +146,17 @@ def get_grid(args):
         hyperparam('--log-interval', 100 if not args.local else 10),
     ]
 
+    arch_grid = get_predefined_grid(args.arch)
+    arch_grid = arch_grid if arch_grid else [
+        hyperparam('--arch', args.arch, save_dir_key=lambda val: val),
+    ]
+
+    grids += arch_grid
+    if args.ddp_backend:
+        grids.append(
+            hyperparam('--ddp-backend', args.ddp_backend, save_dir_key=lambda val: f'{val}')
+        )
+
     if args.decoder_langtok:
         grids.append(
             hyperparam('--decoder-langtok', [True], binary_flag=True, save_dir_key=lambda val: 'det')
@@ -85,6 +168,14 @@ def get_grid(args):
     if args.virtual_epoch_size:
         grids.append(
             hyperparam('--virtual-epoch-size', args.virtual_epoch_size)
+        )
+    if args.lang_dict:
+        grids.append(
+            hyperparam('--lang-dict', args.lang_dict)
+        )
+    if args.max_tokens:
+        grids.append(
+            hyperparam('--max-tokens', args.max_tokens)
         )
 
     return grids
