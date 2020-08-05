860dc0fff599642962b6eaa303cf656cbfdcf675
Mandeep Baines <msb@fb.com>
2020-08-05 15:23:41 -0700

    fix LegacyDDP to work with gpipe (#1213)
    Summary:
    Neither c10d or no_c10d work with pipeline parallelism and gradient checkpointing.
    Added minimal set of changes that get no_c10d working with gpipe.
    
    1.  use per-GPU batch buffers (this was not sufficient)
    2. simplify when all_reduce happens via an explicit all_reduce call after backward
    
    Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/1213
    
    Test Plan:
    fairseq-train --task language_modeling   data-bin/wikitext-103   --save-dir checkpoints/transformer_wikitext-103   --arch transformer_lm --share-decoder-input-output-embed   --dropout 0.1   --optimizer adam --adam-betas '(0.9, 0.98)' --weight-decay 0.01 --clip-norm 0.0   --lr 0.0005 --lr-scheduler inverse_sqrt --warmup-updates 4000 --warmup-init-lr 1e-07   --tokens-per-sample 512 --sample-break-mode none   --max-tokens 2048 --update-freq 16   --max-update 50000  --memory-efficient-fp16 --no-progress-bar --log-interval 1 --seed 4 --max-epoch 1 --max-update 50 --ddp-backend no_c10d
    
    Before:
    
    2020-07-23 20:22:54 | INFO | train_inner | epoch 001:     50 / 394 loss=18.787, ppl=452312, wps=263610, ups=1.01, wpb=262144, bsz=512, num_updates=46, lr=5.84885e-06, gnorm=5.512, loss_scale=8, train_wall=1, wall=66
    2020-07-23 20:22:55 | INFO | train_inner | epoch 001:     51 / 394 loss=18.74, ppl=437735, wps=259423, ups=0.99, wpb=262144, bsz=512, num_updates=47, lr=5.97383e-06, gnorm=5.298, loss_scale=8, train_wall=1, wall=67
    2020-07-23 20:22:56 | INFO | train_inner | epoch 001:     52 / 394 loss=18.683, ppl=420727, wps=256937, ups=0.98, wpb=262144, bsz=512, num_updates=48, lr=6.0988e-06, gnorm=5.094, loss_scale=8, train_wall=1, wall=68
    2020-07-23 20:22:57 | INFO | train_inner | epoch 001:     53 / 394 loss=18.623, ppl=403792, wps=260323, ups=0.99, wpb=262144, bsz=512, num_updates=49, lr=6.22378e-06, gnorm=4.893, loss_scale=8, train_wall=1, wall=69
    2020-07-23 20:22:58 | INFO | train_inner | epoch 001:     54 / 394 loss=18.574, ppl=390255, wps=258095, ups=0.98, wpb=262144, bsz=512, num_updates=50, lr=6.34875e-06, gnorm=4.684, loss_scale=8, train_wall=1, wall=70
    
    After:
    
    2020-07-23 20:20:14 | INFO | train_inner | epoch 001:     50 / 394 loss=18.787, ppl=452312, wps=268872, ups=1.03, wpb=262144, bsz=512, num_updates=46, lr=5.84885e-06, gnorm=5.512, loss_scale=8, train_wall=1, wall=66
    2020-07-23 20:20:15 | INFO | train_inner | epoch 001:     51 / 394 loss=18.74, ppl=437735, wps=263022, ups=1, wpb=262144, bsz=512, num_updates=47, lr=5.97383e-06, gnorm=5.298, loss_scale=8, train_wall=1, wall=67
    2020-07-23 20:20:16 | INFO | train_inner | epoch 001:     52 / 394 loss=18.683, ppl=420727, wps=265523, ups=1.01, wpb=262144, bsz=512, num_updates=48, lr=6.0988e-06, gnorm=5.094, loss_scale=8, train_wall=1, wall=68
    2020-07-23 20:20:17 | INFO | train_inner | epoch 001:     53 / 394 loss=18.623, ppl=403792, wps=259082, ups=0.99, wpb=262144, bsz=512, num_updates=49, lr=6.22378e-06, gnorm=4.893, loss_scale=8, train_wall=1, wall=69
    2020-07-23 20:20:18 | INFO | train_inner | epoch 001:     54 / 394 loss=18.574, ppl=390255, wps=250407, ups=0.95, wpb=262144, bsz=512, num_updates=50, lr=6.34875e-06, gnorm=4.684, loss_scale=8, train_wall=1, wall=70
    
    # Before submitting
    
    - [ ] Was this discussed/approved via a Github issue? (no need for typos, doc improvements)
    - [ ] Did you read the [contributor guideline](https://github.com/pytorch/fairseq/blob/master/CONTRIBUTING.md)?
    - [ ] Did you make sure to update the docs?
    - [ ] Did you write any new necessary tests?
    
    ## What does this PR do?
    Fixes # (issue).
    
    ## PR review
    Anyone in the community is free to review the PR once the tests have passed.
    If we didn't discuss your PR in Github issues there's a high chance it will not be merged.
    
    ## Did you have fun?
    Make sure you had fun coding �
    
    Reviewed By: myleott
    
    Differential Revision: D22488572
    
    Pulled By: msbaines
    
    fbshipit-source-id: a6ab5185fa69c5e0508cdca469e692d1caa8add1

--- fairseq/legacy_distributed_data_parallel.py
+++ fairseq/legacy_distributed_data_parallel.py
@@ -14,6 +14,7 @@ This version also supports the *no_sync* context manager, which allows faster
 training with `--update-freq`.
 """
 
+from collections import OrderedDict
 from contextlib import contextmanager
 import copy
 
@@ -60,11 +61,15 @@ class LegacyDistributedDataParallel(nn.Module):
         # all-reduce at some later time
         self.accumulate_grads = False
 
-        # For NCCL backend, since every single NCCL call is asynchoronous, we
-        # therefore directly enqueue all the NCCL reduction calls to the
-        # default CUDA stream without spawning up other reduction threads.
-        # This achieves the best performance.
-        self._register_grad_hook()
+        # make per-device lists of parameters
+        paramlists = OrderedDict()
+        for param in self.module.parameters():
+            device = param.device
+            if paramlists.get(device) is None:
+                paramlists[device] = []
+            paramlists[device] += [param]
+        self.per_device_params = list(paramlists.values())
+
 
     def __getstate__(self):
         attrs = copy.copy(self.__dict__)
@@ -72,7 +77,6 @@ class LegacyDistributedDataParallel(nn.Module):
 
     def __setstate__(self, state):
         super().__setstate__(state)
-        self._register_grad_hook()
 
     @contextmanager
     def no_sync(self):
@@ -83,17 +87,22 @@ class LegacyDistributedDataParallel(nn.Module):
         self.accumulate_grads = old_accumulate_grads
 
     def forward(self, *inputs, **kwargs):
+        if self.need_reduction:
+            raise RuntimeError(
+                'LegacyDistributedDataParallel requires explicit reduction, '
+                'must call LegacyDistributedDataParallel.all_reduce'
+            )
+        if not self.accumulate_grads:
+            self.need_reduction = True
         return self.module(*inputs, **kwargs)
 
-    def _register_grad_hook(self):
+    def all_reduce(self):
         """
-        This function registers the callback all-reduction function for the
-        NCCL backend. All gradients will be all reduced in one single step.
-        The NCCL reduction will directly be enqueued into the default CUDA
-        stream. Therefore, no synchronization is needed.
+        This function must be called explicitly after backward to reduce
+        gradients. There is no automatic hook like c10d.
         """
 
-        def all_reduce(params):
+        def all_reduce_params(params):
             buffer = self.buffer
             nonzero_buffer = False
             if len(params) > 1:
@@ -142,39 +151,32 @@ class LegacyDistributedDataParallel(nn.Module):
             if self.buffer is None:
                 self.buffer = next(self.module.parameters()).new(self.buffer_size)
 
-            # All-reduce the gradients in buckets
-            offset = 0
-            buffered_params = []
-            for param in self.module.parameters():
-                if not param.requires_grad:
-                    continue
-                if param.grad is None:
-                    param.grad = torch.zeros_like(param)
-                if param.grad.requires_grad:
-                    raise RuntimeError("DistributedDataParallel only works "
-                                       "with gradients that don't require "
-                                       "grad")
-                sz = param.numel()
-                if sz > self.buffer.numel():
-                    # all-reduce big params directly
-                    all_reduce([param])
-                else:
-                    if offset + sz > self.buffer.numel():
-                        all_reduce(buffered_params)
-                        offset = 0
-                        buffered_params.clear()
-                    buffered_params.append(param)
-                    offset += sz
+            for params in self.per_device_params:
+                # All-reduce the gradients in buckets
+                offset = 0
+                buffered_params = []
+                for param in params:
+                    if not param.requires_grad:
+                        continue
+                    if param.grad is None:
+                        param.grad = torch.zeros_like(param)
+                    if param.grad.requires_grad:
+                        raise RuntimeError("DistributedDataParallel only works "
+                                           "with gradients that don't require "
+                                           "grad")
+                    sz = param.numel()
+                    if sz > self.buffer.numel():
+                        # all-reduce big params directly
+                        all_reduce_params([param])
+                    else:
+                        if offset + sz > self.buffer.numel():
+                            all_reduce_params(buffered_params)
+                            offset = 0
+                            buffered_params.clear()
+                        buffered_params.append(param)
+                        offset += sz
 
-            if len(buffered_params) > 0:
-                all_reduce(buffered_params)
+                if len(buffered_params) > 0:
+                    all_reduce_params(buffered_params)
 
-        # Now register the reduction hook on the parameters
-        for p in self.module.parameters():
-
-            def allreduce_hook(*unused):
-                self.need_reduction = True
-                Variable._execution_engine.queue_callback(reduction_fn)
-
-            if p.requires_grad:
-                p.register_hook(allreduce_hook)
+        reduction_fn()
