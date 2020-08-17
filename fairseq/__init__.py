53e3b39b5beec5f6fff02af8dd3fbe47ed12eb6a
Myle Ott <myleott@fb.com>
2020-08-17 15:23:28 -0700

    Add fairseq.scoring to fairseq/__init__.py

--- fairseq/__init__.py
+++ fairseq/__init__.py
@@ -20,6 +20,7 @@ import fairseq.modules  # noqa
 import fairseq.optim  # noqa
 import fairseq.optim.lr_scheduler  # noqa
 import fairseq.pdb  # noqa
+import fairseq.scoring  # noqa
 import fairseq.tasks  # noqa
 
 import fairseq.benchmark  # noqa
