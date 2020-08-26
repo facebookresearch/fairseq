e2fa049f68f47457b234c98b861083f06c8d4705
Armen Aghajanyan <armen.ag@live.com>
2020-08-26 16:17:58 -0700

    fix import issue

--- examples/rxf/src/label_smoothed_cross_entropy_r3f.py
+++ examples/rxf/src/label_smoothed_cross_entropy_r3f.py
@@ -8,6 +8,7 @@ import torch
 import torch.nn.functional as F
 from fairseq import metrics, utils
 from fairseq.criterions import FairseqCriterion, register_criterion
+from fairseq.criterions.label_smoothed_cross_entropy import label_smoothed_nll_loss
 
 @register_criterion("label_smoothed_cross_entropy_r3f")
 class LabelSmoothedCrossEntropyR3FCriterion(FairseqCriterion):
