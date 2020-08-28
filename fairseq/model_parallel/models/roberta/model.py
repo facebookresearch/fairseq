900f9174fcde1374bbcc46e5078604d1121b19d0
Myle Ott <myleott@fb.com>
2020-08-28 05:59:26 -0700

    Bugfix for model parallel roberta

--- fairseq/model_parallel/models/roberta/model.py
+++ fairseq/model_parallel/models/roberta/model.py
@@ -127,7 +127,7 @@ class ModelParallelRobertaLMHead(nn.Module):
         x = self.activation_fn(x)
         x = self.layer_norm(x)
 
-        features = copy_to_model_parallel_region(features)
+        x = copy_to_model_parallel_region(x)
         # project back to size of vocabulary with bias
         x = F.linear(x, self.weight)
         x = gather_from_model_parallel_region(x).contiguous()
