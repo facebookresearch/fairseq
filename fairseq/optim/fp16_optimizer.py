bdf44727ef951b88a69a326faba7d47519638c35
Myle Ott <myleott@fb.com>
2020-10-02 19:00:29 -0700

    Fix hub (#2687)
    Summary: Pull Request resolved: https://github.com/pytorch/fairseq/pull/2687
    
    Reviewed By: alexeib
    
    Differential Revision: D24095130
    
    Pulled By: myleott
    
    fbshipit-source-id: 7d371bccb550ec68b2b9b39dfa4c0718356508d6

--- fairseq/optim/fp16_optimizer.py
+++ fairseq/optim/fp16_optimizer.py
@@ -34,13 +34,17 @@ class _FP16OptimizerMixin(object):
     def build_fp32_params(cls, args, params, flatten=True):
         # create FP32 copy of parameters and grads
         if flatten:
+            is_pipeline_parallel = (
+                getattr(args, 'pipeline_model_parallel', False)
+                and getattr(args, 'distributed_no_spawn', False)
+            )
             total_param_size = sum(p.data.numel() for p in params)
             devices = [torch.cuda.current_device()]
-            if args.pipeline_model_parallel and args.distributed_no_spawn:
+            if is_pipeline_parallel:
                 devices = list(set(args.pipeline_devices))
             fp32_params = {}
             for device in devices:
-                if args.pipeline_model_parallel and args.distributed_no_spawn:
+                if is_pipeline_parallel:
                     device_param_size = sum(p.data.numel() for p in params if p.device.index == device)
                     device_params = [p for p in params if p.device.index == device]
                 else:
