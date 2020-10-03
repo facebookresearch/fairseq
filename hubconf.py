7c292af66f61b1125854218519bf81d494e5b11e
Myle Ott <myleott@fb.com>
2020-10-02 19:00:29 -0700

    Fix hub (#2687)
    Summary: Pull Request resolved: https://github.com/pytorch/fairseq/pull/2687
    
    Reviewed By: alexeib
    
    Differential Revision: D24095130
    
    Pulled By: myleott
    
    fbshipit-source-id: 7d371bccb550ec68b2b9b39dfa4c0718356508d6

--- hubconf.py
+++ hubconf.py
@@ -4,15 +4,12 @@
 # LICENSE file in the root directory of this source tree.
 
 import functools
-
-from fairseq.hub_utils import BPEHubInterface as bpe  # noqa
-from fairseq.hub_utils import TokenizerHubInterface as tokenizer  # noqa
-from fairseq.models import MODEL_REGISTRY
+import importlib
 
 
 dependencies = [
     'dataclasses',
-    'hydra-core',
+    'hydra',
     'numpy',
     'regex',
     'requests',
@@ -20,31 +17,51 @@ dependencies = [
 ]
 
 
+# Check for required dependencies and raise a RuntimeError if any are missing.
+missing_deps = []
+for dep in dependencies:
+    try:
+        importlib.import_module(dep)
+    except ImportError:
+        # Hack: the hydra package is provided under the "hydra-core" name in
+        # pypi. We don't want the user mistakenly calling `pip install hydra`
+        # since that will install an unrelated package.
+        if dep == 'hydra':
+            dep = 'hydra-core'
+        missing_deps.append(dep)
+if len(missing_deps) > 0:
+    raise RuntimeError('Missing dependencies: {}'.format(', '.join(missing_deps)))
+
+
 # torch.hub doesn't build Cython components, so if they are not found then try
 # to build them here
 try:
-    import fairseq.data.token_block_utils_fast
-except (ImportError, ModuleNotFoundError):
+    import fairseq.data.token_block_utils_fast  # noqa
+except ImportError:
     try:
-        import cython
+        import cython  # noqa
         import os
         from setuptools import sandbox
         sandbox.run_setup(
             os.path.join(os.path.dirname(__file__), 'setup.py'),
             ['build_ext', '--inplace'],
         )
-    except (ImportError, ModuleNotFoundError):
+    except ImportError:
         print(
             'Unable to build Cython components. Please make sure Cython is '
             'installed if the torch.hub model you are loading depends on it.'
         )
 
 
+from fairseq.hub_utils import BPEHubInterface as bpe  # noqa
+from fairseq.hub_utils import TokenizerHubInterface as tokenizer  # noqa
+from fairseq.models import MODEL_REGISTRY  # noqa
+
+
+# automatically expose models defined in FairseqModel::hub_models
 for _model_type, _cls in MODEL_REGISTRY.items():
     for model_name in _cls.hub_models().keys():
         globals()[model_name] = functools.partial(
             _cls.from_pretrained,
             model_name,
         )
-    # to simplify the interface we only expose named models
-    # globals()[_model_type] = _cls.from_pretrained
