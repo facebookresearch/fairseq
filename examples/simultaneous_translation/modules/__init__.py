5f7b47f221653fc7469c3572177ce7b5b5e88263
Xutai Ma <xutai_ma@jhu.edu>
2020-10-07 00:45:26 -0400

    Fix import issue

--- examples/simultaneous_translation/modules/__init__.py
+++ examples/simultaneous_translation/modules/__init__.py
@@ -17,3 +17,7 @@ for file in os.listdir(os.path.dirname(__file__)):
     if file.endswith('.py') and not file.startswith('_'):
         model_name = file[:file.find('.py')]
         importlib.import_module('examples.simultaneous_translation.modules.' + model_name)
+
+from .monotonic_transformer_layer import (
+    TransformerMonotonicEncoderLayer
+)
