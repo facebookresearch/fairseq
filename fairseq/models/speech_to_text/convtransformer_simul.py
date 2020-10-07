cf1fead797ab3aef9ac4251b1d6d399cd39b6fe3
Xutai Ma <xutai_ma@jhu.edu>
2020-10-07 00:28:38 -0400

    Add unidirectional convtransforemr

--- fairseq/models/speech_to_text/convtransformer_simul.py
+++ fairseq/models/speech_to_text/convtransformer_simul.py
@@ -5,6 +5,8 @@
 # the root directory of this source tree. An additional grant of patent rights
 # can be found in the PATENTS file in the same directory.
 
+from torch import nn
+
 from fairseq.models import (
     register_model,
     register_model_architecture,
@@ -12,11 +14,16 @@ from fairseq.models import (
 
 from .convtransformer import (
     ConvTransformerModel,
+    ConvTransformerEncoder,
     convtransformer_espnet
 )
 
 from examples.simultaneous_translation.models import (
     TransformerMonotonicModel,
+)
+
+from examples.simultaneous_translation.modules import (
+    TransformerMonotonicEncoderLayer
 )
 
 from fairseq import checkpoint_utils
@@ -48,9 +55,42 @@ class SimulConvTransformerModel(ConvTransformerModel):
             )
         return decoder
 
+
+@register_model('convtransformer_simul_uni')
+class SimulConvTransformerModelUnidirectional(SimulConvTransformerModel):
+    @classmethod
+    def build_encoder(cls, args):
+        encoder = ConvTransformerEncoderUnidirectional(args)
+        if getattr(args, "load_pretrained_encoder_from", None):
+            encoder = checkpoint_utils.load_pretrained_component_from_model(
+                component=encoder, checkpoint=args.load_pretrained_encoder_from
+            )
+        return encoder
+
+
+class ConvTransformerEncoderUnidirectional(ConvTransformerEncoder):
+    def __init__(self, args):
+        super().__init__(args)
+        self.transformer_layers = nn.ModuleList([])
+        self.transformer_layers.extend(
+            [
+                TransformerMonotonicEncoderLayer(args)
+                for i in range(args.encoder_layers)
+            ]
+        )
+
+
 @register_model_architecture(
     'convtransformer_simul',
     'convtransformer_simul'
 )
 def convtransformer_simul_trans_espnet(args):
+    convtransformer_espnet(args)
+
+
+@register_model_architecture(
+    'convtransformer_simul_uni',
+    'convtransformer_simul_uni'
+)
+def convtransformer_simul_trans_espnet_uni(args):
     convtransformer_espnet(args)
