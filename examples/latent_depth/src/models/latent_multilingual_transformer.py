65c0545dde7dd89223b94ba7969513286f312250
Xian Li <xianl@devfair0251.h2.fair>
2020-10-13 09:59:31 -0700

    add header

--- examples/latent_depth/src/models/latent_multilingual_transformer.py
+++ examples/latent_depth/src/models/latent_multilingual_transformer.py
@@ -1,3 +1,8 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 from fairseq.models import (
     register_model,
     register_model_architecture,
