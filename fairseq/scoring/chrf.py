b05bb150b17af4a719bb2f646733922876240ac1
Changhan Wang <changhan@fb.com>
2020-10-14 12:27:45 -0700

    Update scorer with evaluation-time tokenizer and chrF
    Summary:
    * Added evaluation-time tokenizer (using sacreBLEU's built-in tokenizers): `fairseq.scoring.tokenizer`
    * Added chrF scorer: `fairseq.scoring.chrf`
    * Updated sacreBLEU scorer with evaluation-time tokenizer
    * Updated WER scorer with evaluation-time tokenizer: There are cases where we train ASR models without pre-tokenization or punctuation removal. The tokenization/normalization is done at evaluation time before scoring.
    
    Reviewed By: myleott
    
    Differential Revision: D24219634
    
    fbshipit-source-id: ecde21cb19206b96efff7606e101d476d5687888

---
+++ fairseq/scoring/chrf.py
@@ -0,0 +1,26 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from fairseq.scoring import BaseScorer, register_scorer
+
+
+@register_scorer('chrf')
+class ChrFScorer(BaseScorer):
+    def __init__(self, args):
+        super(ChrFScorer, self).__init__(args)
+        import sacrebleu
+        self.sacrebleu = sacrebleu
+
+    def add_string(self, ref, pred):
+        self.ref.append(ref)
+        self.pred.append(pred)
+
+    def score(self, order=4):
+        return self.result_string(order).score
+
+    def result_string(self, order=4):
+        if order != 4:
+            raise NotImplementedError
+        return self.sacrebleu.corpus_chrf(self.pred, [self.ref]).format()
