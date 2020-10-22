751bcbfcb939b777e61af251d1fce5d4a4dc1f12
Pavel Soriano <pavel.soriano@data.gouv.fr>
2020-10-22 06:25:06 -0700

    Changed EnvironmentError to RuntimeError in get_from_cache (#2767)
    Summary:
    # Before submitting
    
    - [x] Was this discussed/approved via a Github issue? (no need for typos, doc improvements)
    - [x] Did you read the [contributor guideline](https://github.com/pytorch/fairseq/blob/master/CONTRIBUTING.md)?
    - [x] Did you make sure to update the docs?
    No need I believe
    - [x] Did you write any new necessary tests?
    No
    ## What does this PR do?
    Fixes https://github.com/pytorch/fairseq/issues/2724
    
    ## PR review
    Anyone in the community is free to review the PR once the tests have passed.
    If we didn't discuss your PR in Github issues there's a high chance it will not be merged.
    
    ## Did you have fun?
    Yes! It is not a big PR at all but it allowed me to familiarize with the caching/downloading logic used in fairseq (which is very similar to that used in pytorch/transformers)
    
    Pull Request resolved: https://github.com/pytorch/fairseq/pull/2767
    
    Reviewed By: edunov
    
    Differential Revision: D24456055
    
    Pulled By: myleott
    
    fbshipit-source-id: bc634a9b97f957ecc5a8da57b112ff892e492107

--- fairseq/file_utils.py
+++ fairseq/file_utils.py
@@ -286,7 +286,7 @@ def get_from_cache(url, cache_dir=None):
                 etag = None
             else:
                 etag = response.headers.get("ETag")
-        except EnvironmentError:
+        except RuntimeError:
             etag = None
 
     filename = url_to_filename(url, etag)
