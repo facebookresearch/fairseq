cde1c77163a9aad20810828449f19f9ecc989fef
Anuroop Sriram <anuroops@fb.com>
2020-10-29 11:44:46 -0700

    Compute WER for Wav2Vec 2.0 Seq2Seq models (#1376)
    Summary:
    # Before submitting
    
    - [X] Did you read the [contributor guideline](https://github.com/pytorch/fairseq/blob/master/CONTRIBUTING.md)?
    
    ## What does this PR do?
    Adds support to compute WER for wav2vec2.0 seq2seq models.
    
    Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/1376
    
    Reviewed By: alexeib
    
    Differential Revision: D24611516
    
    Pulled By: anuroopsriram
    
    fbshipit-source-id: dd7daab73ebccc21367dd51f41a11e89c404977b

--- fairseq/tasks/audio_pretraining.py
+++ fairseq/tasks/audio_pretraining.py
@@ -5,12 +5,17 @@
 # the root directory of this source tree. An additional grant of patent rights
 # can be found in the PATENTS file in the same directory.
 
+import editdistance
 import os
 import sys
+import torch
 
-from fairseq.data import AddTargetDataset, Dictionary, FileAudioDataset
+from fairseq.data import AddTargetDataset, Dictionary, FileAudioDataset, encoders
+from fairseq.data.data_utils import post_process
 
 from . import LegacyFairseqTask, register_task
+from .. import utils
+from ..logging import metrics
 
 
 class LabelEncoder(object):
@@ -68,11 +73,26 @@ class AudioPretrainingTask(LegacyFairseqTask):
             help="extension of the label file to load, if any",
         )
 
+        # Options for reporting WER metrics during validation. Only applicable to
+        # Seq2Seq models during fine-tuning
+        parser.add_argument(
+            "--eval-wer",
+            action="store_true",
+            help="compute WER for Seq2Seq models",
+        )
+        parser.add_argument(
+            "--eval-wer-remove-bpe",
+            default="letter",
+            help="remove BPE tokens before scoring (can be sentencepiece, letter, and more)",
+        )
+
     def __init__(self, args, source_dictionary=None, target_dictionary=None):
         super().__init__(args)
         self._target_dictionary = target_dictionary
         self._source_dictionary = source_dictionary
         self.is_ctc = args.criterion == "ctc"
+        if getattr(self.args, "eval_wer", False):
+            assert args.labels is not None, "eval_wer can only be set during fine-tuning"
 
     @classmethod
     def setup_task(cls, args, **kwargs):
@@ -149,3 +169,83 @@ class AudioPretrainingTask(LegacyFairseqTask):
     ):
         # we do not need to filter by size in this task as dataloaders take care of this
         return indices
+
+    def valid_step(self, sample, model, criterion):
+        loss, sample_size, logging_output = super().valid_step(sample, model, criterion)
+
+        if getattr(self.args, "eval_wer", False) and not self.is_ctc:
+            metrics = self._inference_with_wer(self.sequence_generator, sample, model)
+            logging_output["_num_char_errors"] = metrics["num_char_errors"]
+            logging_output["_num_chars"] = metrics["num_chars"]
+            logging_output["_num_word_errors"] = metrics["num_word_errors"]
+            logging_output["_num_words"] = metrics["num_words"]
+        return loss, sample_size, logging_output
+
+    def build_model(self, args):
+        model = super().build_model(args)
+
+        if getattr(args, 'eval_wer', False) and not self.is_ctc:
+            self.sequence_generator = self.build_generator([model], args, )
+            self.tokenizer = encoders.build_tokenizer(args)
+        return model
+
+    def _inference_with_wer(self, generator, sample, model):
+        def decode(toks, escape_unk=True):
+            s = self.target_dictionary.string(
+                toks.int().cpu(),
+                self.args.eval_wer_remove_bpe,
+                escape_unk=escape_unk,
+                extra_symbols_to_ignore={generator.eos},
+            )
+            if self.tokenizer:
+                s = self.tokenizer.decode(s)
+            return s
+
+        num_word_errors, num_char_errors = 0, 0
+        num_chars, num_words = 0, 0
+        gen_out = self.inference_step(generator, [model], sample, None)
+        for i in range(len(gen_out)):
+            hyp = decode(gen_out[i][0]["tokens"])
+            ref = decode(
+                utils.strip_pad(sample["target"][i], self.target_dictionary.pad()),
+                escape_unk=True,
+            )
+            hyp = post_process(hyp, self.args.eval_wer_remove_bpe).strip("_")
+            ref = post_process(ref, self.args.eval_wer_remove_bpe).strip("_")
+            num_char_errors += editdistance.eval(hyp, ref)
+            num_chars += len(ref)
+            hyp_words = hyp.split("_")
+            ref_words = ref.split("_")
+            num_word_errors += editdistance.eval(hyp_words, ref_words)
+            num_words += len(ref_words)
+        
+        return {
+            "num_char_errors": num_char_errors,
+            "num_chars": num_chars,
+            "num_word_errors": num_word_errors,
+            "num_words": num_words,
+        }
+
+    def reduce_metrics(self, logging_outputs, criterion):
+        super().reduce_metrics(logging_outputs, criterion)
+
+        zero = torch.scalar_tensor(0.)
+        num_char_errors = sum(log.get("_num_char_errors", zero) for log in logging_outputs)
+        num_chars = sum(log.get("_num_chars", zero) for log in logging_outputs)
+        num_word_errors = sum(log.get("_num_word_errors", zero) for log in logging_outputs)
+        num_words = sum(log.get("_num_words", zero) for log in logging_outputs)
+        metrics.log_scalar("_num_char_errors", num_char_errors)
+        metrics.log_scalar("_num_chars", num_chars)
+        metrics.log_scalar("_num_word_errors", num_word_errors)
+        metrics.log_scalar("_num_words", num_words)
+        if num_words > 0:
+            metrics.log_derived(
+                "uer",
+                lambda meters: meters["_num_char_errors"].sum * 100.0 / meters["_num_chars"].sum
+                if meters["_num_chars"].sum > 0 else float("nan")
+            )
+            metrics.log_derived(
+                "wer",
+                lambda meters: meters["_num_word_errors"].sum * 100.0 / meters["_num_words"].sum
+                if meters["_num_words"].sum > 0 else float("nan")
+            )
