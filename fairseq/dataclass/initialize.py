50422496ac7c3ef2e7dd3818a5126ab9ab37ae29
Myle Ott <myleott@fb.com>
2020-11-07 15:00:00 -0800

    Automatically register components in ConfigStore via register_* functions (#1406)
    Summary:
    We can automatically register everything in ConfigStore on-the-fly. This avoids needing to worry about importing everything in the right order. It also better supports `--user-dir`, since those typically get imported later.
    
    Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/1406
    
    Reviewed By: alexeib
    
    Differential Revision: D24814072
    
    Pulled By: myleott
    
    fbshipit-source-id: 21cfc1a6c497fe98bf4429bfed138030d9999b6a

--- fairseq/dataclass/initialize.py
+++ fairseq/dataclass/initialize.py
@@ -9,30 +9,8 @@ from typing import Dict, Any
 from hydra.core.config_store import ConfigStore
 from fairseq.dataclass.configs import FairseqConfig
 
-# the imports below are necessary so that "REGISTRIES" is correctly populated with all components
-from fairseq.criterions import CRITERION_REGISTRY  # noqa
-from fairseq.optim import OPTIMIZER_REGISTRY  # noqa
-from fairseq.optim.lr_scheduler import LR_SCHEDULER_REGISTRY  # noqa
-from fairseq.scoring import SCORER_REGISTRY  # noqa
-from fairseq.data.encoders import BPE_REGISTRY, TOKENIZER_REGISTRY  # noqa
-
-from fairseq.models import MODEL_DATACLASS_REGISTRY
-from fairseq.tasks import TASK_DATACLASS_REGISTRY
-from fairseq.registry import REGISTRIES
-
 
 logger = logging.getLogger(__name__)
-
-
-def register_module_dataclass(
-    cs: ConfigStore, registry: Dict[str, Any], group: str
-) -> None:
-    """register dataclasses defined in modules in config store, for example, in migrated tasks, models, etc."""
-    # note that if `group == model`, we register all model archs, not the model name.
-    for k, v in registry.items():
-        node_ = v()
-        node_._name = k
-        cs.store(name=k, group=group, node=node_, provider="fairseq")
 
 
 def hydra_init(cfg_name="config") -> None:
@@ -47,9 +25,3 @@ def hydra_init(cfg_name="config") -> None:
         except BaseException:
             logger.error(f"{k} - {v}")
             raise
-
-    register_module_dataclass(cs, TASK_DATACLASS_REGISTRY, "task")
-    register_module_dataclass(cs, MODEL_DATACLASS_REGISTRY, "model")
-
-    for k, v in REGISTRIES.items():
-        register_module_dataclass(cs, v["dataclass_registry"], k)
