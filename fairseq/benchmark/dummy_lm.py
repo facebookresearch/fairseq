82ba9106f0ef7cd6630277dea5fc09e8b6984d74
Myle Ott <myleott@fb.com>
2020-11-09 12:25:01 -0800

    Upgrade DummyLMTask to Hydra (#1415)
    Summary: Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/1415
    
    Test Plan: Imported from OSS
    
    Reviewed By: alexeib
    
    Differential Revision: D24833480
    
    Pulled By: myleott
    
    fbshipit-source-id: 007623168467d18166b20ef99f54388eb9d8008a

--- fairseq/benchmark/dummy_lm.py
+++ fairseq/benchmark/dummy_lm.py
@@ -4,82 +4,75 @@
 # LICENSE file in the root directory of this source tree.
 
 import logging
+from dataclasses import dataclass, field
+from typing import Optional
 
 import numpy as np
 import torch
 from fairseq.data import Dictionary, FairseqDataset
-from fairseq.tasks import LegacyFairseqTask, register_task
+from fairseq.dataclass import FairseqDataclass
+from fairseq.tasks import FairseqTask, register_task
+from omegaconf import II
 
 
 logger = logging.getLogger(__name__)
 
 
-@register_task("dummy_lm")
-class DummyLMTask(LegacyFairseqTask):
-    @staticmethod
-    def add_args(parser):
-        """Add task-specific arguments to the parser."""
-        parser.add_argument("--dict-size", default=49996, type=int)
-        parser.add_argument("--dataset-size", default=100000, type=int)
-        parser.add_argument(
-            "--tokens-per-sample",
-            default=512,
-            type=int,
-            help="max number of total tokens over all segments "
-            "per sample for BERT dataset",
-        )
-        parser.add_argument("--add-bos-token", action="store_true", help="unused")
-        parser.add_argument(
-            "--max-target-positions",
-            default=None,
-            help="max number of tokens in the target sequence",
-        )
+@dataclass
+class DummyLMConfig(FairseqDataclass):
+    dict_size: int = 49996
+    dataset_size: int = 100000
+    tokens_per_sample: int = field(
+        default=512, metadata={"help": "max sequence length"}
+    )
+    add_bos_token: bool = False
+    batch_size: Optional[int] = II("dataset.batch_size")
+    max_tokens: Optional[int] = II("dataset.max_tokens")
+    max_target_positions: int = II("task.tokens_per_sample")
 
-    def __init__(self, args, dictionary):
-        super().__init__(args)
-        self.dictionary = dictionary
-        self.seed = args.seed
 
-        dictionary.pad_to_multiple_(8)  # often faster if divisible by 8
+@register_task("dummy_lm", dataclass=DummyLMConfig)
+class DummyLMTask(FairseqTask):
 
-        seq = torch.arange(args.tokens_per_sample + 1) + dictionary.pad() + 1
+    def __init__(self, cfg: DummyLMConfig):
+        super().__init__(cfg)
+
+        # load dictionary
+        self.dictionary = Dictionary()
+        for i in range(cfg.dict_size):
+            self.dictionary.add_symbol("word{}".format(i))
+        self.dictionary.pad_to_multiple_(8)  # often faster if divisible by 8
+        logger.info("dictionary: {} types".format(len(self.dictionary)))
+
+        seq = torch.arange(cfg.tokens_per_sample + 1) + self.dictionary.pad() + 1
 
         self.dummy_src = seq[:-1]
         self.dummy_tgt = seq[1:]
-
-    @classmethod
-    def setup_task(cls, args, **kwargs):
-        """Setup the task. """
-        dictionary = Dictionary()
-        for i in range(args.dict_size):
-            dictionary.add_symbol("word{}".format(i))
-        logger.info("dictionary: {} types".format(len(dictionary)))
-        return cls(args, dictionary)
 
     def load_dataset(self, split, epoch=1, combine=False, **kwargs):
         """Load a given dataset split.
         Args:
             split (str): name of the split (e.g., train, valid, test)
         """
-        if self.args.batch_size is not None:
-            bsz = self.args.batch_size
+        if self.cfg.batch_size is not None:
+            bsz = self.cfg.batch_size
         else:
-            bsz = max(1, self.args.max_tokens // self.args.tokens_per_sample)
+            bsz = max(1, self.cfg.max_tokens // self.cfg.tokens_per_sample)
         self.datasets[split] = DummyDataset(
             {
                 "id": 1,
                 "net_input": {
                     "src_tokens": torch.stack([self.dummy_src for _ in range(bsz)]),
                     "src_lengths": torch.full(
-                        (bsz,), self.args.tokens_per_sample, dtype=torch.long
+                        (bsz,), self.cfg.tokens_per_sample, dtype=torch.long
                     ),
                 },
                 "target": torch.stack([self.dummy_tgt for _ in range(bsz)]),
                 "nsentences": bsz,
-                "ntokens": bsz * self.args.tokens_per_sample,
+                "ntokens": bsz * self.cfg.tokens_per_sample,
             },
-            num_items=self.args.dataset_size,
-            item_size=self.args.tokens_per_sample,
+            num_items=self.cfg.dataset_size,
+            item_size=self.cfg.tokens_per_sample,
         )
 
     @property
