83e615d66905b8ca7483122a37da1a85f13f4b8e
Myle Ott <myleott@fb.com>
2021-01-05 12:02:23 -0800

    v0.10.1 -> v0.10.2
    Bug fixes (see https://github.com/pytorch/fairseq/issues?q=label:0.10.2)

--- examples/__init__.py
+++ examples/__init__.py
@@ -3,6 +3,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-__version__ = "0.10.1"
+__version__ = "0.10.2"
 
 import examples.noisychannel  # noqa
--- fairseq/__init__.py
+++ fairseq/__init__.py
@@ -5,7 +5,7 @@
 """isort:skip_file"""
 
 __all__ = ["pdb"]
-__version__ = "0.10.1"
+__version__ = "0.10.2"
 
 import sys
 
