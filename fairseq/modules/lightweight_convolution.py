94633344298a68b93aebd25405a107696b1be58f
Jones <45222374+JonnesLin@users.noreply.github.com>
2021-02-04 00:46:33 +0800

    Update lightweight_convolution.py
    It has the same question described in #3198

--- fairseq/modules/lightweight_convolution.py
+++ fairseq/modules/lightweight_convolution.py
@@ -15,7 +15,7 @@ from fairseq.modules.unfold import unfold1d
 def LightweightConv(
     input_size,
     kernel_size=1,
-    padding_l=None,
+    padding_l=0,
     num_heads=1,
     weight_dropout=0.0,
     weight_softmax=False,
