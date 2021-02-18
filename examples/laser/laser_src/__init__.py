7040ce71f3e0e84730adc267df764f48dc483dac
Onur Çelebi <celebio@fb.com>
2021-02-18 03:09:14 -0800

    LASER training code (#1207)
    Summary:
    Integrating LASER (Language-Agnostic SEntence Representations) training code
    
    - [ ] Was this discussed/approved via a Github issue? (no need for typos, doc improvements)
    - [ Y] Did you read the [contributor guideline](https://github.com/pytorch/fairseq/blob/master/CONTRIBUTING.md)?
    - [ N/A] Did you make sure to update the docs?
    - [ Y] Did you write any new necessary tests?  => an additional test in `test_iterators.py`
    
    ## What does this PR do?
    
    This diff introduces the training code for LASER.
    It includes a specific `laser` task in `laser_task.py` which reads a
    json configuration file describing the binarized datasets of language
    pairs.
    
    `multitask_data_utils.py` defines dataset wrappers and iterators used by
    `laser` task.
    
    ## PR review
    Anyone in the community is free to review the PR once the tests have passed.
    If we didn't discuss your PR in Github issues there's a high chance it will not be merged.
    
    ## Did you have fun?
    Yes. �
    
    Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/1207
    
    Reviewed By: myleott
    
    Differential Revision: D26454296
    
    Pulled By: Celebio
    
    fbshipit-source-id: c987672aa66abf31b039ee11867b06912d3486e5

---
+++ examples/laser/laser_src/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .laser_task import *  # noqa
+from .laser_lstm import *  # noqa
+from .laser_transformer import *  # noqa
