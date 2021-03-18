d61033062ac7c528d2899fc9c6bed06caefdfc0b
William E <willells@stanford.edu>
2021-03-18 16:41:38 -0400

    Implement 1 and 4 bit quantization for quant noise
    Co-authored-by: Sergio Charles <sergiogcharles@gmail.com>

--- fairseq/modules/quantization/scalar/ops.py
+++ fairseq/modules/quantization/scalar/ops.py
@@ -5,45 +5,74 @@
 
 import torch
 
+import logging
 
 def emulate_int(w, bits, method, scale=None, zero_point=None):
     q = globals()[f"emulate_int{bits}_{method}"]
     return q(w, scale=scale, zero_point=zero_point)
 
-
-def quantize(w, scale, zero_point):
+def quantize(w, scale, zero_point, size=8):
     return (
-        torch.clamp(torch.round(w / scale + zero_point), 0, 255) - zero_point
+        torch.clamp(torch.round(w / scale + zero_point), 0, 2**size - 1) - zero_point
     ) * scale
 
-
-def emulate_int8_histogram(w, scale=None, zero_point=None):
-    if scale is None:
+def get_obs(size, observer_type):
+    if observer_type == "hist":
         obs = torch.quantization.observer.HistogramObserver()
-        _ = obs(w.float())
-        scale, zero_point = obs.calculate_qparams()
-        scale = scale.cuda().type_as(w)
-        zero_point = zero_point.cuda().type_as(w)
-    return quantize(w, scale, zero_point), scale, zero_point
-
-
-def emulate_int8_channel(w, scale=None, zero_point=None):
-    if scale is None:
-        obs = torch.quantization.observer.PerChannelMinMaxObserver(
+    elif observer_type == "channel":
+        obs = torch.quantization.observer_type.PerChannelMinMaxObserver(
             ch_axis=-1, qscheme=torch.per_channel_symmetric
         )
+    elif observer_type == "tensor":
+        obs = torch.quantization.observer.MinMaxObserver()
+    if size != 8:
+        obs.quant_min, obs.quant_max = 0, 2**size - 1
+        obs.has_customized_qrange = True
+    return obs
+
+def get_scale_zero_point(w, obs, observer_type):
+    if observer_type == "hist":
+        _ = obs(w.float())
+        scale, zero_point = obs.calculate_qparams()
+    elif observer_type == "channel":
         _ = obs(w)
         scale, zero_point, ch_axis = obs.get_qparams()
-        scale = scale.cuda().type_as(w)
-        zero_point = zero_point.cuda().type_as(w)
-    return quantize(w, scale, zero_point), scale, zero_point
-
-
-def emulate_int8_tensor(w, scale=None, zero_point=None):
-    if scale is None:
-        obs = torch.quantization.observer.MinMaxObserver()
+    elif observer_type == "tensor":
         _ = obs(w)
         scale, zero_point = obs.calculate_qparams()
-        scale = scale.cuda().type_as(w)
-        zero_point = zero_point.cuda().type_as(w)
-    return quantize(w, scale, zero_point), scale, zero_point
+    scale = scale.cuda().type_as(w)
+    zero_point = zero_point.cuda().type_as(w)
+    return scale, zero_point
+
+def emulate_func(w, scale, zero_point, size, observer_type):
+    if scale is None:
+        obs = get_obs(size, observer_type)
+        scale, zero_point = get_scale_zero_point(w, obs, observer_type)
+    return quantize(w, scale, zero_point, size), scale, zero_point
+
+def emulate_int1_histogram(w, scale=None, zero_point=None):
+    return emulate_func(w, scale, zero_point, size=1, observer_type="hist")
+
+def emulate_int1_channel(w, scale=None, zero_point=None):
+    return emulate_func(w, scale, zero_point, size=1, observer_type="channel")
+
+def emulate_int1_tensor(w, scale=None, zero_point=None):
+    return emulate_func(w, scale, zero_point, size=1, observer_type="tensor")
+
+def emulate_int4_histogram(w, scale=None, zero_point=None):
+    return emulate_func(w, scale, zero_point, size=4, observer_type="hist")
+
+def emulate_int4_channel(w, scale=None, zero_point=None):
+    return emulate_func(w, scale, zero_point, size=4, observer_type="channel")
+
+def emulate_int4_tensor(w, scale=None, zero_point=None):
+    return emulate_func(w, scale, zero_point, size=1, observer_type="tensor")
+
+def emulate_int8_histogram(w, scale=None, zero_point=None):
+    return emulate_func(w, scale, zero_point, size=8, observer_type="hist")
+
+def emulate_int8_channel(w, scale=None, zero_point=None):
+    return emulate_func(w, scale, zero_point, size=1, observer_type="channel")
+
+def emulate_int8_tensor(w, scale=None, zero_point=None):
+    return emulate_func(w, scale, zero_point, size=1, observer_type="tensor")
