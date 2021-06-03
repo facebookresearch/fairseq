e9c7b780f644e2bc1450405a58f1f230942099de
Diana Liskovich <dianaml@devfair0471.h2.fair>
2021-06-02 17:38:50 -0700

    move out common code to single dummy dataset

--- fairseq/benchmark/dummy_masked_lm.py
+++ fairseq/benchmark/dummy_masked_lm.py
@@ -7,11 +7,11 @@ import logging
 from dataclasses import dataclass, field
 from typing import Optional
 
-import numpy as np
 import torch
 from omegaconf import II
 
-from fairseq.data import Dictionary, FairseqDataset
+from .dummy_dataset import DummyDataset
+from fairseq.data import Dictionary
 from fairseq.dataclass import FairseqDataclass
 from fairseq.tasks import FairseqTask, register_task
 
@@ -91,37 +91,3 @@ class DummyMaskedLMTask(FairseqTask):
     @property
     def target_dictionary(self):
         return self.dictionary
-
-
-class DummyDataset(FairseqDataset):
-    def __init__(self, batch, num_items, item_size):
-        super().__init__()
-        self.batch = batch
-        self.num_items = num_items
-        self.item_size = item_size
-
-    def __getitem__(self, index):
-        return index
-
-    def __len__(self):
-        return self.num_items
-
-    def collater(self, samples):
-        return self.batch
-
-    @property
-    def sizes(self):
-        return np.array([self.item_size] * self.num_items)
-
-    def num_tokens(self, index):
-        return self.item_size
-
-    def size(self, index):
-        return self.item_size
-
-    def ordered_indices(self):
-        return np.arange(self.num_items)
-
-    @property
-    def supports_prefetch(self):
-        return False
