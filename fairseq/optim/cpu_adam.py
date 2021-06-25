01eb6fbde2e2fd1326f3f9df994661bb30ed27be
liangluofb <82682482+liangluofb@users.noreply.github.com>
2021-06-25 14:06:31 -0700

    Update cpu_adam.py
    block adam from proceeding until H2D transfer is completed.

--- fairseq/optim/cpu_adam.py
+++ fairseq/optim/cpu_adam.py
@@ -126,7 +126,8 @@ class CPUAdam(torch.optim.Optimizer):
         if closure is not None:
             with torch.enable_grad():
                 loss = closure()
-
+                
+        torch.cuda.synchronize()        
         for group_id, group in enumerate(self.param_groups):
             for param_id, p in enumerate(group["params"]):
                 if p.grad is None:
