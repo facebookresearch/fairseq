79f35a525f2f23563a04024130dbd0df908ea443
Colin Sinclair <csinclair0@gmail.com>
2021-06-30 19:28:13 -0700

    add loggin

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -122,10 +122,11 @@ def main(cfg: FairseqConfig) -> None:
     model = task.build_model(cfg.model)
     from torch.nn import Embedding, Linear
     embed_length = 250053 + added_embeddings
+    logger.info(f"embed_length {embed_length}")
     model.encoder.embed_tokens.weight = Embedding(embed_length, 1024)
     model.decoder.embed_tokens.weight = Embedding(embed_length, 1024)
     model.decoder.output_projection.weight = Linear(1024, embed_length, False)
-
+    logger.info(f"encoder {model.encoder.embed_tokens.weight}")
     # Build model and criterion
     if cfg.distributed_training.ddp_backend == "fully_sharded":
         with fsdp_enable_wrap(cfg.distributed_training):
