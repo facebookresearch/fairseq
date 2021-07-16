bc1504d4d709fd2157b6ba15f754c0307eb734f3
Pierre Andrews <mortimer@fb.com>
2021-07-16 04:54:46 -0700

    Hierarchical Configs
    Summary:
    This is a precursor to D29232595
    
    The current behaviour to convert a dataclass to a namespace is that all the fields from all DCs in the field hierarchy are flattened at the top. This is also the legacy behaviour with `add_args`.
    
    This is kind of cumbersome to build reusable Dataclasses as we need to make sure that each field has a unique  name. In the case of Transformer for instance, we have a Decoder and Encoder config that share a large part of their fields (embed_dim, layers, etc.). We can build a single dataclass for this that can be reused and extended in other implementations. To be then able to have  a flat namespace, instead of adding all subfields as is to the root namespace, we introduce the name of the field as prefix to the arg in the namespace.
    
    So:
    `model.decoder.embed_dim` becomes `decoder_embed_dim` and `model.encoder.embed_dim` becomes `encoder_embed_dim`.
    
    Reviewed By: myleott, dianaml0
    
    Differential Revision: D29521386
    
    fbshipit-source-id: f4bef036f0eeb620c6d8709ce97f96ae288848ef

--- fairseq/dataclass/utils.py
+++ fairseq/dataclass/utils.py
@@ -54,17 +54,27 @@ def gen_parser_from_dataclass(
     parser: ArgumentParser,
     dataclass_instance: FairseqDataclass,
     delete_default: bool = False,
+    with_prefix: Optional[str] = None,
 ) -> None:
-    """convert a dataclass instance to tailing parser arguments"""
+    """
+        convert a dataclass instance to tailing parser arguments.
+
+        If `with_prefix` is provided, prefix all the keys in the resulting parser with it. It means that we are
+        building a flat namespace from a structured dataclass (see transformer_config.py for example).
+    """
 
     def argparse_name(name: str):
-        if name == "data":
-            # normally data is positional args
+        if name == "data" and (with_prefix is None or with_prefix == ''):
+            # normally data is positional args, so we don't add the -- nor the prefix
             return name
         if name == "_name":
             # private member, skip
             return None
-        return "--" + name.replace("_", "-")
+        full_name = "--" + name.replace("_", "-")
+        if with_prefix is not None and with_prefix != '':
+            # if a prefix is specified, construct the prefixed arg name
+            full_name = with_prefix + "-" + full_name[2:]  # strip -- when composing
+        return full_name
 
     def get_kwargs_from_dc(
         dataclass_instance: FairseqDataclass, k: str
@@ -132,6 +142,10 @@ def gen_parser_from_dataclass(
                 if field_default is not MISSING:
                     kwargs["default"] = field_default
 
+        # build the help with the hierarchical prefix
+        if with_prefix is not None and with_prefix != '' and field_help is not None:
+            field_help = with_prefix[2:] + ': ' + field_help
+
         kwargs["help"] = field_help
         if field_const is not None:
             kwargs["const"] = field_const
@@ -145,7 +159,14 @@ def gen_parser_from_dataclass(
         if field_name is None:
             continue
         elif inspect.isclass(field_type) and issubclass(field_type, FairseqDataclass):
-            gen_parser_from_dataclass(parser, field_type(), delete_default)
+            # for fields that are of type FairseqDataclass, we can recursively
+            # add their fields to the namespace (so we add the args from model, task, etc. to the root namespace)
+            prefix = None
+            if with_prefix is not None:
+                # if a prefix is specified, then we don't want to copy the subfields directly to the root namespace
+                # but we prefix them with the name of the current field.
+                prefix = field_name
+            gen_parser_from_dataclass(parser, field_type(), delete_default, prefix)
             continue
 
         kwargs = get_kwargs_from_dc(dataclass_instance, k)
