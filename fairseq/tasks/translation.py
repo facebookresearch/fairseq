a3ba03929ff8f8f8bc755f94fd788742da938457
Colin Sinclair <csinclair0@gmail.com>
2021-07-20 17:25:06 -0700

    add bpe gate

--- fairseq/tasks/translation.py
+++ fairseq/tasks/translation.py
@@ -515,9 +515,14 @@ class TranslationTask(FairseqTask):
         model = super().build_model(cfg)
         if self.cfg.eval_bleu:
             detok_args = json.loads(self.cfg.eval_bleu_detok_args)
-            self.tokenizer = encoders.build_tokenizer(
-                Namespace(tokenizer=self.cfg.eval_bleu_detok, **detok_args)
-            )
+            if detok_args.get("bpe") is not None:
+                self.tokenizer = encoders.build_bpe(
+                    Namespace(tokenizer=self.cfg.eval_bleu_detok, **detok_args)
+                )
+            else:
+                self.tokenizer = encoders.build_tokenizer(
+                    Namespace(tokenizer=self.cfg.eval_bleu_detok, **detok_args)
+                )
 
             gen_args = json.loads(self.cfg.eval_bleu_args)
             self.sequence_generator = self.build_generator(
