120fdd90c2ce993ced87ab02a543e652bc91a2f7
Colin Sinclair <csinclair0@gmail.com>
2021-07-22 08:18:17 -0700

    add logging

--- fairseq/tasks/translation.py
+++ fairseq/tasks/translation.py
@@ -28,6 +28,7 @@ from fairseq.data import (
 from fairseq.data.indexed_dataset import get_available_dataset_impl
 from fairseq.dataclass import ChoiceEnum, FairseqDataclass
 from fairseq.tasks import FairseqTask, register_task
+from fairseq_cli.generate import get_symbols_to_strip_from_output
 
 
 EVAL_BLEU_ORDER = 4
@@ -516,7 +517,7 @@ class TranslationTask(FairseqTask):
         if self.cfg.eval_bleu:
             detok_args = json.loads(self.cfg.eval_bleu_detok_args)
             if self.cfg.eval_bleu_detok == 'sentencepiece':
-                self.tokenizer = encoders.build_bpe(
+                self.bpe = encoders.build_bpe(
                     Namespace(bpe=self.cfg.eval_bleu_detok, **detok_args)
                 )
             else:
@@ -528,7 +529,7 @@ class TranslationTask(FairseqTask):
             self.sequence_generator = self.build_generator(
                 [model], Namespace(**gen_args)
             )
-            logger.info(self.tokenizer)
+            
         return model
 
     def valid_step(self, sample, model, criterion):
@@ -604,29 +605,45 @@ class TranslationTask(FairseqTask):
 
     def _inference_with_bleu(self, generator, sample, model):
         import sacrebleu
-        def decode(toks, escape_unk=False):
+        def decode(toks):
             s = self.tgt_dict.string(
                 toks.int().cpu(),
                 self.cfg.eval_bleu_remove_bpe,
+                escape_unk=True, 
                 # The default unknown string in fairseq is `<unk>`, but
                 # this is tokenized by sacrebleu as `< unk >`, inflating
                 # BLEU scores. Instead, we use a somewhat more verbose
                 # alternative that is unlikely to appear in the real
                 # reference, but doesn't get split into multiple tokens.
-                unk_string=("UNKNOWNTOKENINREF" if escape_unk else "UNKNOWNTOKENINHYP"),
+                extra_symbols_to_ignore=get_symbols_to_strip_from_output(
+                            generator
+                        )
             )
             if self.tokenizer:
                 s = self.tokenizer.decode(s)
             return s
-
-        gen_out = self.inference_step(generator, [model], sample, prefix_tokens=None)
+        gen_out = self.inference_step(generator, [model], sample)
         hyps, refs = [], []
         for i in range(len(gen_out)):
-            hyps.append(decode(gen_out[i][0]["tokens"]))
+            src_tokens = utils.strip_pad(
+                    sample["net_input"]["src_tokens"][i, :], self.target_dictionary.pad()
+                )
+            src_str = self.source_dictionary.string(src_tokens, 'sentencepiece')
+            hypo_tokens, hypo_str, alignment = utils.post_process_prediction(
+                    hypo_tokens=gen_out[i][0]["tokens"],
+                    src_str=src_str, 
+                    alignment=gen_out[i][0]["alignment"],
+                    align_dict=None,
+                    tgt_dict=self.target_dictionary,
+                    remove_bpe='sentencepiece',
+                    extra_symbols_to_ignore=get_symbols_to_strip_from_output(generator),
+                )
+            detok_hypo_str = self.tokenizer.decode(hypo_str)
+            hyps.append(detok_hypo_str)
             refs.append(
                 decode(
                     utils.strip_pad(sample["target"][i], self.tgt_dict.pad()),
-                    escape_unk=True,  # don't count <unk> as matches to the hypo
+                    #escape_unk=False,  # don't count <unk> as matches to the hypo
                 )
             )
         if self.cfg.eval_bleu_print_samples:
--- test.sh
+++ test.sh
@@ -2,17 +2,32 @@ fairseq-train /Users/csinclair/src/translation-models-data/data/datasets/process
 --decoder-normalize-before --maximize-best-checkpoint-metric \
 --log-interval 10 --adam-betas '(0.9, 0.98)' --save-interval 1000 --lr 3e-05 --warmup-updates 2500 \
 --save-interval-updates 1000  --weight-decay 0.0 --keep-best-checkpoints 1 \
---validate-interval 10000 --lr-scheduler inverse_sqrt --max-source-positions 1024 --eval-bleu-args '{"bpe": "sentencepiece","beam": 5,"sentencepiece-model": "/Users/csinclair/Downloads/mbart50.ft.1n/sentence.bpe.model"}' \
+--validate-interval 10000 --lr-scheduler inverse_sqrt --max-source-positions 1024 \
+--eval-bleu-detok 'sentencepiece' \
+--eval-bleu-detok-args '{"sentencepiece_model": "/Users/csinclair/Downloads/mbart50.ft.1n/sentence.bpe.model"}' \
+--eval-bleu-remove-bpe 'sentencepiece' \
 --task translation_from_pretrained_bart --adam-eps 1e-06 --dropout 0.3 \
 --encoder-normalize-before --no-save-optimizer-state --log-format simple \
 --max-target-positions 1024 --best-checkpoint-metric bleu \
+--max-sentences 1 \
 --criterion label_smoothed_cross_entropy --seed 42 \
 --finetune-from-model /Users/csinclair/Downloads/mbart50.ft.1n/model.pt \
 --attention-dropout 0.1 --source-lang en_XX --target-lang es_XX \
 --optimizer adam --layernorm-embedding \
+--cpu \
 --patience 5  --keep-interval-updates 1 \
 --langs ar_AR,cs_CZ,de_DE,en_XX,es_XX,et_EE,fi_FI,fr_XX,gu_IN,hi_IN,it_IT,ja_XX,kk_KZ,ko_KR,lt_LT,lv_LV,my_MM,ne_NP,nl_XX,ro_RO,ru_RU,si_LK,tr_TR,vi_VN,zh_CN,af_ZA,az_AZ,bn_IN,fa_IR,he_IL,hr_HR,id_ID,ka_GE,km_KH,mk_MK,ml_IN,mn_MN,mr_IN,pl_PL,ps_AF,pt_XX,sv_SE,sw_KE,ta_IN,te_IN,th_TH,tl_XX,uk_UA,ur_PK,xh_ZA,gl_ES,sl_SI \
---max-tokens 1024 --fp16 --no-epoch-checkpoints --skip-invalid-size-inputs-valid-test --scoring sacrebleu \
---max-update 1000 --validate-interval-updates 1000  --max-tokens-valid 1024 \
---eval-bleu  --label-smoothing 0.2 --update-freq 2 --arch mbart_large \
---no-last-checkpoints
+--max-tokens 1024 --no-epoch-checkpoints --skip-invalid-size-inputs-valid-test --scoring sacrebleu \
+--max-update 0 --validate-interval-updates 1 --max-tokens-valid 1024 \
+--eval-bleu  --label-smoothing 0.2 --update-freq 1 --arch mbart_large \
+--no-last-checkpoints --eval-bleu-print-samples 
+
+
+
+fairseq-interactive /Users/csinclair/src/translation-models-data/data/datasets/processed/fairseq/mbart/baseline/bin \
+--path /Users/csinclair/Downloads/model_serving/checkpoint_best.pt \
+  --task translation_from_pretrained_bart \
+  --target-lang es_XX --source-lang en_XX \
+  --bpe 'sentencepiece' --sentencepiece-model /Users/csinclair/Downloads/model_serving/sentence.bpe.model \
+   --remove-bpe 'sentencepiece' \
+--langs ar_AR,cs_CZ,de_DE,en_XX,es_XX,et_EE,fi_FI,fr_XX,gu_IN,hi_IN,it_IT,ja_XX,kk_KZ,ko_KR,lt_LT,lv_LV,my_MM,ne_NP,nl_XX,ro_RO,ru_RU,si_LK,tr_TR,vi_VN,zh_CN,af_ZA,az_AZ,bn_IN,fa_IR,he_IL,hr_HR,id_ID,ka_GE,km_KH,mk_MK,ml_IN,mn_MN,mr_IN,pl_PL,ps_AF,pt_XX,sv_SE,sw_KE,ta_IN,te_IN,th_TH,tl_XX,uk_UA,ur_PK,xh_ZA,gl_ES,sl_SI 
