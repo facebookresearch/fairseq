292707e9d06c3e1f7134a68745d8489f1d3b69ae
Colin Sinclair <csinclair0@gmail.com>
2021-07-26 12:57:06 -0700

    add test gen

--- test.sh
+++ test.sh
@@ -71,3 +71,34 @@ fairseq-interactive /Users/csinclair/src/translation-models-data/data/datasets/p
   --decoder-langtok \
   --lang-pairs "en_XX-es_XX" \
 --langs ar_AR,cs_CZ,de_DE,en_XX,es_XX,et_EE,fi_FI,fr_XX,gu_IN,hi_IN,it_IT,ja_XX,kk_KZ,ko_KR,lt_LT,lv_LV,my_MM,ne_NP,nl_XX,ro_RO,ru_RU,si_LK,tr_TR,vi_VN,zh_CN,af_ZA,az_AZ,bn_IN,fa_IR,he_IL,hr_HR,id_ID,ka_GE,km_KH,mk_MK,ml_IN,mn_MN,mr_IN,pl_PL,ps_AF,pt_XX,sv_SE,sw_KE,ta_IN,te_IN,th_TH,tl_XX,uk_UA,ur_PK,xh_ZA,gl_ES,sl_SI 
+
+
+
+
+fairseq-generate processed \
+  --path model/mbart50.ft.1n/model.pt \
+  --task translation_multi_simple_epoch \
+  --gen-subset test \
+  --source-lang en_XX \
+  --target-lang es_XX \
+  --sacrebleu --remove-bpe 'sentencepiece'\
+  --batch-size 14 \
+  --encoder-langtok "src" \
+  --decoder-langtok \
+  --lang-dict model/mbart50.ft.1n/ML50_langs.txt \
+ --lang-pairs 'en_XX-af_ZA,en_XX-ar_AR,en_XX-az_AZ,en_XX-bn_IN,en_XX-cs_CZ,en_XX-de_DE,en_XX-es_XX,en_XX-et_EE,en_XX-fa_IR,en_XX-fi_FI,en_XX-fr_XX,en_XX-gl_ES,en_XX-gu_IN,en_XX-he_IL,en_XX-hi_IN,en_XX-hr_HR,en_XX-id_ID,en_XX-it_IT,en_XX-ja_XX,en_XX-ka_GE,en_XX-kk_KZ,en_XX-km_KH,en_XX-ko_KR,en_XX-lt_LT,en_XX-lv_LV,en_XX-mk_MK,en_XX-ml_IN,en_XX-mn_MN,en_XX-mr_IN,en_XX-my_MM,en_XX-ne_NP,en_XX-nl_XX,en_XX-pl_PL,en_XX-ps_AF,en_XX-pt_XX,en_XX-ro_RO,en_XX-ru_RU,en_XX-si_LK,en_XX-sl_SI,en_XX-sv_SE,en_XX-ta_IN,en_XX-te_IN,en_XX-th_TH,en_XX-tr_TR,en_XX-uk_UA,en_XX-ur_PK,en_XX-vi_VN,en_XX-xh_ZA,en_XX-zh_CN' > roblox_en_XX-es_XX.tgt &&
+
+
+fairseq-generate test_set \
+  --path model/mbart50.ft.1n/model.pt \
+  --task translation_multi_simple_epoch \
+  --gen-subset test \
+  --source-lang en_XX \
+  --target-lang es_XX \
+  --sacrebleu --remove-bpe 'sentencepiece'\
+  --batch-size 14 \
+  --encoder-langtok "src" \
+  --decoder-langtok \
+  --lang-dict model/mbart50.ft.1n/ML50_langs.txt \
+ --lang-pairs 'en_XX-af_ZA,en_XX-ar_AR,en_XX-az_AZ,en_XX-bn_IN,en_XX-cs_CZ,en_XX-de_DE,en_XX-es_XX,en_XX-et_EE,en_XX-fa_IR,en_XX-fi_FI,en_XX-fr_XX,en_XX-gl_ES,en_XX-gu_IN,en_XX-he_IL,en_XX-hi_IN,en_XX-hr_HR,en_XX-id_ID,en_XX-it_IT,en_XX-ja_XX,en_XX-ka_GE,en_XX-kk_KZ,en_XX-km_KH,en_XX-ko_KR,en_XX-lt_LT,en_XX-lv_LV,en_XX-mk_MK,en_XX-ml_IN,en_XX-mn_MN,en_XX-mr_IN,en_XX-my_MM,en_XX-ne_NP,en_XX-nl_XX,en_XX-pl_PL,en_XX-ps_AF,en_XX-pt_XX,en_XX-ro_RO,en_XX-ru_RU,en_XX-si_LK,en_XX-sl_SI,en_XX-sv_SE,en_XX-ta_IN,en_XX-te_IN,en_XX-th_TH,en_XX-tr_TR,en_XX-uk_UA,en_XX-ur_PK,en_XX-vi_VN,en_XX-xh_ZA,en_XX-zh_CN' > generic_en_XX-es_XX.tgt
+
