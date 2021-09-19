4247203622da81b110c24a5f0a584716c28a218b
Chih-Chiang Chang <cc.chang0828@gmail.com>
2021-09-19 18:00:59 +0800

    Fix incremental_state being ignored by p_choose during inference (#3892)

--- examples/simultaneous_translation/modules/monotonic_multihead_attention.py
+++ examples/simultaneous_translation/modules/monotonic_multihead_attention.py
@@ -148,8 +148,8 @@ class MonotonicAttention(MultiheadAttention):
         )
         return p_choose
 
-    def p_choose(self, query, key, key_padding_mask, incremental_states=None):
-        return self.p_choose_from_qk(query, key, key_padding_mask)
+    def p_choose(self, query, key, key_padding_mask, incremental_state=None):
+        return self.p_choose_from_qk(query, key, key_padding_mask, incremental_state=incremental_state)
 
     def monotonic_attention_process_infer(
         self,
