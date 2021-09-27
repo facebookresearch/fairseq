baf5c1bb6e68deac9c67473177161047bc5d128c
Xu Song <xusong.vip@gmail.com>
2021-09-27 14:32:23 +0800

    Update checkpoint_utils.py
    "optimizer" and "extra_state" are not required in state.

--- fairseq/checkpoint_utils.py
+++ fairseq/checkpoint_utils.py
@@ -528,7 +528,7 @@ def _upgrade_state_dict(state):
     """Helper for upgrading old model checkpoints."""
 
     # add optimizer_history
-    if "optimizer_history" not in state:
+    if "optimizer" in state and "optimizer_history" not in state:
         state["optimizer_history"] = [
             {"criterion_name": "CrossEntropyCriterion", "best_loss": state["best_loss"]}
         ]
@@ -545,24 +545,25 @@ def _upgrade_state_dict(state):
         del state["epoch"]
         del state["batch_offset"]
         del state["val_loss"]
-    # reduce optimizer history's memory usage (only keep the last state)
-    if "optimizer" in state["optimizer_history"][-1]:
-        state["last_optimizer_state"] = state["optimizer_history"][-1]["optimizer"]
-        for optim_hist in state["optimizer_history"]:
-            del optim_hist["optimizer"]
-    # record the optimizer class name
-    if "optimizer_name" not in state["optimizer_history"][-1]:
-        state["optimizer_history"][-1]["optimizer_name"] = "FairseqNAG"
-    # move best_loss into lr_scheduler_state
-    if "lr_scheduler_state" not in state["optimizer_history"][-1]:
-        state["optimizer_history"][-1]["lr_scheduler_state"] = {
-            "best": state["optimizer_history"][-1]["best_loss"]
-        }
-        del state["optimizer_history"][-1]["best_loss"]
-    # keep track of number of updates
-    if "num_updates" not in state["optimizer_history"][-1]:
-        state["optimizer_history"][-1]["num_updates"] = 0
-    # old model checkpoints may not have separate source/target positions
+    if "optimizer_history" in state:
+        # reduce optimizer history's memory usage (only keep the last state)
+        if "optimizer" in state["optimizer_history"][-1]:
+            state["last_optimizer_state"] = state["optimizer_history"][-1]["optimizer"]
+            for optim_hist in state["optimizer_history"]:
+                del optim_hist["optimizer"]
+        # record the optimizer class name
+        if "optimizer_name" not in state["optimizer_history"][-1]:
+            state["optimizer_history"][-1]["optimizer_name"] = "FairseqNAG"
+        # move best_loss into lr_scheduler_state
+        if "lr_scheduler_state" not in state["optimizer_history"][-1]:
+            state["optimizer_history"][-1]["lr_scheduler_state"] = {
+                "best": state["optimizer_history"][-1]["best_loss"]
+            }
+            del state["optimizer_history"][-1]["best_loss"]
+        # keep track of number of updates
+        if "num_updates" not in state["optimizer_history"][-1]:
+            state["optimizer_history"][-1]["num_updates"] = 0
+        # old model checkpoints may not have separate source/target positions
     if (
         "args" in state
         and hasattr(state["args"], "max_positions")
@@ -571,7 +572,7 @@ def _upgrade_state_dict(state):
         state["args"].max_source_positions = state["args"].max_positions
         state["args"].max_target_positions = state["args"].max_positions
     # use stateful training data iterator
-    if "train_iterator" not in state["extra_state"]:
+    if "extra_state" in state and "train_iterator" not in state["extra_state"]:
         state["extra_state"]["train_iterator"] = {
             "epoch": state["extra_state"]["epoch"],
             "iterations_in_epoch": state["extra_state"].get("batch_offset", 0),
@@ -588,7 +589,7 @@ def _upgrade_state_dict(state):
         elif getattr(state["args"], "lazy_load", False):
             state["args"].dataset_impl = "lazy"
         # epochs start at 1
-        if state["extra_state"]["train_iterator"] is not None:
+        if "extra_state" in state and state["extra_state"]["train_iterator"] is not None:
             state["extra_state"]["train_iterator"]["epoch"] = max(
                 state["extra_state"]["train_iterator"].get("epoch", 1), 1
             )
