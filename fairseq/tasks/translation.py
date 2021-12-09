776f009a2a72bcb3a1497e0c3f26d9c664aea088
Yixin <beyondhyx@gmail.com>
2021-12-09 11:36:09 +0800

    debug compute bleu

--- fairseq/tasks/translation.py
+++ fairseq/tasks/translation.py
@@ -438,8 +438,8 @@ class TranslationTask(FairseqTask):
                     bleu = comp_bleu(
                         correct=meters["_bleu_counts"].sum,
                         total=meters["_bleu_totals"].sum,
-                        sys_len=meters["_bleu_sys_len"].sum,
-                        ref_len=meters["_bleu_ref_len"].sum,
+                        sys_len=int(meters["_bleu_sys_len"].sum),
+                        ref_len=int(meters["_bleu_ref_len"].sum),
                         **smooth,
                     )
                     return round(bleu.score, 2)
