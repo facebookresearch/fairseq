1e4055da7d14355821d2d72b40b1e6d463fe4ecd
Shreyan Bakshi <shreyanb@fb.com>
2021-12-16 12:53:32 -0800

    Fix Tracing mode TS export for LayerNorm layer
    Summary: When under TorchScript Tracing (instead of only doing this for Scripting) we set `export=True` for `LayerNorm` as `FusedLayerNorm `doesn't work with JIT yet (see `torch.jit.unused decorator`).
    
    Reviewed By: cndn
    
    Differential Revision: D33103054
    
    fbshipit-source-id: f8c24a4a30a89dd4c70b19362fd60c51fcb9a1f0

--- fairseq/modules/layer_norm.py
+++ fairseq/modules/layer_norm.py
@@ -21,12 +21,13 @@ try:
                 with torch.cuda.device(x.device):
                     return super().forward(x)
 
+
 except ImportError:
     has_fused_layernorm = False
 
 
 def LayerNorm(normalized_shape, eps=1e-5, elementwise_affine=True, export=False):
-    if torch.jit.is_scripting():
+    if torch.jit.is_scripting() or torch.jit.is_tracing():
         export = True
     if not export and torch.cuda.is_available() and has_fused_layernorm:
         return FusedLayerNorm(normalized_shape, eps, elementwise_affine)
