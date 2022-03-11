facebookresearch/fairseq

commit e55e094b969df89d43e68b797707ee8d3bac1c5a

AddTargetDataset now first adds EOS then pads target sequences (#4243)
Summary:
# Before submitting

- [x] Was this discussed/approved via a Github issue? (no need for typos, doc improvements)

https://groups.google.com/g/fairseq-users/c/YoSm5J2To1A

- [ ] Did you read the [contributor guideline](https://github.com/pytorch/fairseq/blob/main/CONTRIBUTING.md)?
- [ ] Did you make sure to update the docs?
- [ ] Did you write any new necessary tests?

## What does this PR do?
Fixes https://github.com/pytorch/fairseq/issues/4242

## PR review
Anyone in the community is free to review the PR once the tests have passed.
If we didn't discuss your PR in Github issues there's a high chance it will not be merged.

## Did you have fun?
Make sure you had fun coding �

Pull Request resolved: https://github.com/pytorch/fairseq/pull/4243

Reviewed By: arbabu123

Differential Revision: D34538164

Pulled By: alexeib

fbshipit-source-id: cf2fdaa7663bee34571fb3d3bd9bdaf79d756206

fairseq/data/add_target_dataset.py
@@ -54,22 +54,25 @@ class AddTargetDataset(BaseWrapperDataset):
         indices = set(collated["id"].tolist())
         target = [s["label"] for s in samples if s["id"] in indices]
 
+        if self.add_to_input:
+            eos = torch.LongTensor([self.eos])
+            prev_output_tokens = [torch.cat([eos, t], axis=-1) for t in target]
+            target = [torch.cat([t, eos], axis=-1) for t in target]
+            collated["net_input"]["prev_output_tokens"] = prev_output_tokens
+
         if self.batch_targets:
             collated["target_lengths"] = torch.LongTensor([len(t) for t in target])
             target = data_utils.collate_tokens(target, pad_idx=self.pad, left_pad=False)
             collated["ntokens"] = collated["target_lengths"].sum().item()
+            collated["net_input"]["prev_output_tokens"] = data_utils.collate_tokens(
+                collated["net_input"]["prev_output_tokens"],
+                pad_idx=self.pad,
+                left_pad=False
+            )
         else:
             collated["ntokens"] = sum([len(t) for t in target])
 
         collated["target"] = target
-
-        if self.add_to_input:
-            eos = target.new_full((target.size(0), 1), self.eos)
-            collated["target"] = torch.cat([target, eos], dim=-1).long()
-            collated["net_input"]["prev_output_tokens"] = torch.cat(
-                [eos, target], dim=-1
-            ).long()
-            collated["ntokens"] += target.size(0)
         return collated
 
     def filter_indices_by_size(self, indices, max_sizes):

commit d03f4e771484a433f025f47744017c2eb6e9c6bc

Minor fixes (#3198)
Summary:
- Fix error introduced in https://github.com/pytorch/fairseq/commit/e55e094b969df89d43e68b797707ee8d3bac1c5a in the case where net_input doesn't have prev_output_tokens key
- Fix typo in covost README.

X-link: https://github.com/fairinternal/fairseq-py/pull/3198

Reviewed By: cndn, kahne

Differential Revision: D34810092

Pulled By: sravyapopuri388

fbshipit-source-id: 9be6e6f06586cd2a2d44415ebf7c3596a5334b81

fairseq/data/add_target_dataset.py
@@ -64,11 +64,12 @@ class AddTargetDataset(BaseWrapperDataset):
             collated["target_lengths"] = torch.LongTensor([len(t) for t in target])
             target = data_utils.collate_tokens(target, pad_idx=self.pad, left_pad=False)
             collated["ntokens"] = collated["target_lengths"].sum().item()
-            collated["net_input"]["prev_output_tokens"] = data_utils.collate_tokens(
-                collated["net_input"]["prev_output_tokens"],
-                pad_idx=self.pad,
-                left_pad=False,
-            )
+            if getattr(collated["net_input"], "prev_output_tokens", None):
+                collated["net_input"]["prev_output_tokens"] = data_utils.collate_tokens(
+                    collated["net_input"]["prev_output_tokens"],
+                    pad_idx=self.pad,
+                    left_pad=False,
+                )
         else:
             collated["ntokens"] = sum([len(t) for t in target])
 