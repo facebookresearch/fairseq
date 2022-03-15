21c0169ac3bed99a8188d2f2ebfceb6fa506d292
rekriz11 <rekriz@seas.upenn.edu>
2022-03-15 14:26:59 -0400

    Update sequence_risk_criterion.py

--- fairseq/criterions/sequence_risk_criterion.py
+++ fairseq/criterions/sequence_risk_criterion.py
@@ -10,8 +10,8 @@ import torch
 from torch.autograd import Variable
 import torch.nn.functional as F
 
-from .fairseq_sequence_criterion import FairseqSequenceCriterion, register_criterion
-#from fairseq.criterions import FairseqCriterion, register_criterion
+from .fairseq_sequence_criterion import FairseqSequenceCriterion
+from fairseq.criterions import FairseqCriterion, register_criterion
 
 @register_criterion("sequence_risk")
 class SequenceRiskCriterion(FairseqSequenceCriterion):
