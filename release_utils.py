73e971ddb7bd4eed9e7fb3aacbc641c8f8829f04
Diana Liskovich <dianaml@devfair0471.h2.fair>
2022-06-02 16:04:25 -0700

    Build and Release

--- release_utils.py
+++ release_utils.py
@@ -2,8 +2,6 @@ import argparse
 import re
 from typing import Tuple
 
-from setup import find_version
-
 
 def get_next_version(release_type) -> Tuple[Tuple[int, int, int], str, str]:
     current_ver = find_version("fairseq/version.txt")
@@ -18,12 +16,20 @@ def get_next_version(release_type) -> Tuple[Tuple[int, int, int], str, str]:
         major += 1
         minor = patch = 0
     else:
-        raise ValueError("Incorrect release type specified. Acceptable types are major, minor and patch.")
+        raise ValueError(
+            "Incorrect release type specified. Acceptable types are major, minor and patch."
+        )
 
     new_version_tuple = (major, minor, patch)
     new_version_str = ".".join([str(x) for x in new_version_tuple])
     new_tag_str = "v" + new_version_str
     return new_version_tuple, new_version_str, new_tag_str
+
+
+def find_version(version_file_path) -> str:
+    with open(version_file_path) as f:
+        version = f.read().strip()
+        return version
 
 
 def update_version(new_version_str) -> None:
@@ -50,9 +56,17 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Versioning utils")
-    parser.add_argument("--release-type", type=str, required=True, help="type of release = major/minor/patch")
     parser.add_argument(
-        "--update-version", action="store_true", required=False, help="updates the version in fairscale/version.py"
+        "--release-type",
+        type=str,
+        required=True,
+        help="type of release = major/minor/patch",
+    )
+    parser.add_argument(
+        "--update-version",
+        action="store_true",
+        required=False,
+        help="updates the version in fairseq/version.txt",
     )
 
     args = parser.parse_args()
