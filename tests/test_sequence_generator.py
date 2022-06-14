a0130fa9adaeac9b01a9a86d7abf6266a7e4f575
Wei Wei <wwei6@fb.com>
2022-06-14 12:07:34 -0700

    bug fix

--- tests/test_sequence_generator.py
+++ tests/test_sequence_generator.py
@@ -25,7 +25,7 @@ DEFAULT_TEST_VOCAB_SIZE = 100
 def version_check():
     # check Nested Tensor available. Make sure version >= '1.13.0.dev20220613'
     if 'fb' in torch.__version__:
-        return True
+        return False
     else:
         if "+" in torch.__version__:
             torch_version = torch.__version__.split('+')[0]
@@ -36,11 +36,11 @@ def version_check():
         int_version = int(torch_version[0])*1000 + int(torch_version[1])*10 + int(torch_version[2])
         if len(torch_version) == 3:
             if int_version >= 1131:
-                return True
+                return False
         elif len(torch_version) == 4:
             if int_version >= 1131 or (int_version == 1130 and torch_version[3][3:] >= "20220613"):
-                return True
-        return False
+                return False
+        return True
 
 
 class DummyTask(LegacyFairseqTask):
@@ -134,7 +134,7 @@ class TestJitSequenceGeneratorBase(unittest.TestCase):
 JIT_MSG = "Targeting OSS scriptability for the 1.6 release"
 
 
-@unittest.skipIf(version_check(), JIT_MSG)
+@unittest.skipIf(version_check(), "Targeting OSS scriptability for the 1.13.0.dev20220613 release")
 class TestJitSequenceGenerator(TestJitSequenceGeneratorBase):
     def test_export_transformer(self):
         model = self.transformer_model
