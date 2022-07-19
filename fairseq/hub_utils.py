aec9b408f9cedff67936a7ce659ba3dc7654a2c9
Sravya Popuri <spopuri@fb.com>
2022-07-19 12:52:50 -0700

    HF interface update

--- fairseq/hub_utils.py
+++ fairseq/hub_utils.py
@@ -70,10 +70,19 @@ def from_pretrained(
     if "user_dir" in kwargs:
         utils.import_user_module(argparse.Namespace(user_dir=kwargs["user_dir"]))
 
-    models, args, task = checkpoint_utils.load_model_ensemble_and_task(
-        [os.path.join(model_path, cpt) for cpt in checkpoint_file.split(os.pathsep)],
-        arg_overrides=kwargs,
-    )
+    model_path = [
+        os.path.join(model_path, cpt) for cpt in checkpoint_file.split(os.pathsep)
+    ]
+
+    if "is_vocoder" in kwargs:
+        args = {"data": kwargs["data"], "model_path": model_path}
+        task = None
+        models = None
+    else:
+        models, args, task = checkpoint_utils.load_model_ensemble_and_task(
+            model_path,
+            arg_overrides=kwargs,
+        )
 
     return {
         "args": args,
--- fairseq/models/speech_to_text/xm_transformer.py
+++ fairseq/models/speech_to_text/xm_transformer.py
@@ -487,6 +487,9 @@ class XMTransformerModel(FairseqEncoderDecoderModel):
             "xm_transformer-21_en-xls_r_2b",
             "xm_transformer-en_15-xls_r_2b",
             "xm_transformer-22_16-xls_r_2b",
+            "xm_transformer_s2ut_800m-es-en-st-asr-bt_h1_2022",
+            "xm_transformer_s2ut_800m-en-es-st_plus_asr",
+            "xm_transformer_s2ut_es_en_st_asr_test"
         ]
         return {i: f"{base_url}/{i}.tar.gz" for i in model_ids}
 
