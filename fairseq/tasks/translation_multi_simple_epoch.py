eaab668ff52d0da31be8d12d678782addd3939f8
Colin Sinclair <csinclair0@gmail.com>
2022-08-07 17:38:00 -0700

    hard code shard epoch

--- fairseq/tasks/translation_multi_simple_epoch.py
+++ fairseq/tasks/translation_multi_simple_epoch.py
@@ -162,7 +162,7 @@ class TranslationMultiSimpleEpochTask(LegacyFairseqTask):
         Args:
             split (str): name of the split (e.g., train, valid, test)
         """
-        shard_epoch = epoch
+        shard_epoch = 1
         if split in self.datasets:
             dataset = self.datasets[split]
             if self.has_sharded_data(split):
