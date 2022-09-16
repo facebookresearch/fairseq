ff23233f69c8874429352c3b31fb9fdf73b12205
Colin Sinclair <csinclair0@gmail.com>
2022-09-16 16:59:43 -0700

    add tokenizers

---
+++ fairseq/data/encoders/kytea.py
@@ -0,0 +1,76 @@
+from dataclasses import dataclass, field
+from fairseq.data.encoders import register_tokenizer
+from fairseq.dataclass import FairseqDataclass
+from typing import List
+import six
+import re 
+
+@dataclass
+class KyteaTokenizerConfig(FairseqDataclass):
+    model_file: str = field(default="en", metadata={"help": "where is the model file located"})
+    decode_only: bool = field(default = True,  metadata={"help": "detokenize only"})
+    encode_only: bool = field(default = False,  metadata={"help": "tokenize only"})
+
+
+def to_unicode(unicode_or_str):
+    if six.PY3:
+        return unicode_or_str
+    if isinstance(unicode_or_str, str):
+        value = unicode_or_str.decode('utf-8')
+    else:
+        value = unicode_or_str
+    return value  # Instance of unicode
+
+
+def force_to_unicode(s):
+    """ Returns the joined string if s is a list. """
+    if isinstance(s, list):
+        s = " ".join(s)
+    assert isinstance(s, six.string_types)
+    return to_unicode(s)
+
+
+def chinese_deseg(words):
+    """ Recovers the result of `tokenize(words)`.
+
+    Args:
+        words: A list of strings, i.e. tokenized text.
+
+    Returns: The recovered sentence string.
+    """
+    words = force_to_unicode(words)
+    re_space = re.compile(r"(?<![a-zA-Z])\s(?![a-zA-Z])", flags=re.UNICODE)
+    re_final_comma = re.compile("\.$")
+    
+    words = re_space.sub("", words)
+    # words = words.replace(",", u"\uFF0C")
+    words = re_final_comma.sub(u"\u3002", words)
+    return words
+
+
+@register_tokenizer("kytea", dataclass=KyteaTokenizerConfig)
+class KyteaTokenizer(object):
+    def __init__(self, cfg: KyteaTokenizerConfig):
+        try: 
+            from Mykytea import Mykytea
+        except: 
+            raise ImportError 
+        self.cfg = cfg
+        _param = u"-model {}".format(cfg.model_file)
+        self._kt = Mykytea(_param)
+
+    def encode(self, text: str) -> str:
+        if self.cfg.decode_only:
+            return text
+        w_list = []
+        for w in self._kt.getWS(text):
+            w_list.append(w)
+        return u" ".join(w_list)
+
+ 
+    def decode(self, text:str) -> str:
+        if self.cfg.encode_only:
+            return text
+        res = chinese_deseg(text.split(" "))
+        res = re.sub(r" +", u" ", res)
+        return res
---
+++ fairseq/data/encoders/pangu.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass, field
+from fairseq.data.encoders import register_tokenizer
+from fairseq.dataclass import FairseqDataclass
+from typing import List
+import sys, os, re
+from pangu import spacing
+
+RE_WS_IN_FW = re.compile(r'([\u2018\u2019\u201c\u201d\u2e80-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff00-\uffef])\s+(?=[\u2018\u2019\u201c\u201d\u2e80-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff00-\uffef])')
+
+
+@dataclass
+class PanguTokenizerConfig(FairseqDataclass):
+    target_lang: str = field(default="zh", metadata={"help": "target language"})
+    decode_only: bool = field(default = True,  metadata={"help": "detokenize only"})
+    encode_only: bool = field(default = False,  metadata={"help": "tokenize only"})
+
+
+@register_tokenizer("pangu", dataclass=PanguTokenizerConfig)
+class PanguTokenizer(object):
+    def __init__(self, cfg: PanguTokenizerConfig):
+        self.cfg = cfg
+
+    def encode(self, text: str) -> str:
+        return text
+
+    def decode(self, text: str) -> str:
+        text = spacing(RE_WS_IN_FW.sub(r'\1', text)).strip()
+        return text
