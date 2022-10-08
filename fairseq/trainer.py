38a04e303c7341d9583151e9168934051f2e224c
VarunGumma <varun230999@gmail.com>
2022-10-08 22:07:29 +0530

    fixed bug in trainer.py

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -1122,6 +1122,7 @@ class Trainer(object):
             self.criterion.eval()
 
             sample, is_dummy_batch = self._prepare_sample(sample)
+            sample["teacher_output"] = self.teacher_model(**sample["net_input"])
 
             try:
                 _loss, sample_size, logging_output = self.task.valid_step(
