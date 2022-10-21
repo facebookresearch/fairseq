07ad2f58e80aaeae7e8179e95db319700f594394
Colin Sinclair <csinclair0@gmail.com>
2022-10-20 17:14:34 -0700

    use set experts

--- fairseq/modules/transformer_layer.py
+++ fairseq/modules/transformer_layer.py
@@ -248,9 +248,11 @@ class TransformerEncoderLayerBase(nn.Module):
                 self.moe_layer = MoE(
                     hidden_size = self.embed_dim,
                     expert = fc3, 
-                    num_experts = 4, 
-                    ep_size = 2, 
-                    k = 2
+                    num_experts = cfg.moe_expert_count, 
+                    ep_size = cfg.eps_size, 
+                    k = 1 if cfg.moe_top1_expert else 2, 
+                    use_residual=cfg.use_residual, 
+                    use_tutel = cfg.use_tutel_moe
                 )
 
         self.final_layer_norm = LayerNorm(self.embed_dim, export=cfg.export)
