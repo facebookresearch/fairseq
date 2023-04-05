cdb7f2dca65c33b52911fa8987a6bb2c58fc8e0b
colin sinclair <csinclair0@gmail.com>
2023-04-04 20:28:45 -0700

    filter max

--- fairseq/tasks/fairseq_task.py
+++ fairseq/tasks/fairseq_task.py
@@ -184,7 +184,11 @@ class FairseqTask(object):
         Returns:
             np.array: array of filtered sample indices
         """
-        indices, ignored = dataset.filter_indices_by_size(indices, int(max_positions * 0.7))
+        try:
+            max_positions = int(int(max_positions) * 0.7)
+        except:
+            max_positions =716
+        indices, ignored = dataset.filter_indices_by_size(indices, max_positions)
         if len(ignored) > 0:
             if not ignore_invalid_inputs:
                 raise Exception(
