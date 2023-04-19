c7df6a77088f33174cfadbf20c6b184c869baa15
colin sinclair <csinclair0@gmail.com>
2023-04-19 15:03:12 -0700

    dont update null state dict

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -480,7 +480,8 @@ class Trainer(object):
                 # keep params in FP16 when training with --memory-efficient-fp16
                 cast_to_fp32=not self.cfg.common.memory_efficient_fp16,
             )
-            state_dict["extra_state"].update(extra_state)
+            if state_dict is not None:
+                state_dict["extra_state"].update(extra_state)
             if self.should_save_checkpoint_on_current_rank:
                 checkpoint_utils.torch_persistent_save(
                     state_dict,
