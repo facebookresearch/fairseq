be226c948acde15b52800d5f00d7f55ce19f1ff9
Bowen Shi <bshi@meta.com>
2023-05-23 15:59:09 -0700

    remove mps support to accommodate all pytorch versions

--- examples/mms/tts/infer.py
+++ examples/mms/tts/infer.py
@@ -86,8 +86,6 @@ def generate():
 
     if torch.cuda.is_available():
         device = torch.device("cuda")
-    elif torch.backends.mps.is_available() and torch.backends.mps.is_built():
-        device = torch.device("mps")
     else:
         device = torch.device("cpu")
 
