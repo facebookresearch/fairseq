654fd1a025ecb56e40c0fc4f4a2489fe8eaf9590
chaos130 <1540467815@qq.com>
2023-08-30 15:08:45 +0800

    2

--- fairseq/criterions/label_smoothed_cross_entropy.py
+++ fairseq/criterions/label_smoothed_cross_entropy.py
@@ -36,19 +36,23 @@ def label_smoothed_nll_loss(lprobs, target, epsilon, ignore_index=None, reduce=T
     if target.dim() == lprobs.dim() - 1:
         target = target.unsqueeze(-1)
     nll_loss = -lprobs.gather(dim=-1, index=target)
+    
+    
+    
     smooth_loss = -lprobs.sum(dim=-1, keepdim=True)
     if ignore_index is not None:
         pad_mask = target.eq(ignore_index)
         nll_loss.masked_fill_(pad_mask, 0.0)
+        
         smooth_loss.masked_fill_(pad_mask, 0.0)
     else:
         nll_loss = nll_loss.squeeze(-1)
         smooth_loss = smooth_loss.squeeze(-1)
     if reduce:
-        nll_loss = (nll_loss.sum()) / 16
-        smooth_loss = smooth_loss.sum()
+        nll_loss = (nll_loss[:,0:1].sum()) 
+        smooth_loss = smooth_loss[:,0:1].sum()
     eps_i = epsilon / (lprobs.size(-1) - 1)
-    loss = ((1.0 - epsilon - eps_i) * nll_loss + eps_i * smooth_loss) / 16
+    loss = ((1.0 - epsilon - eps_i) * nll_loss + eps_i * smooth_loss) 
     return loss, nll_loss
 #
 def ranking_loss(lprobs,target,score):
