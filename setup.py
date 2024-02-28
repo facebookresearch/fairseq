07b25ae75f638e376436fba75683db35ac441673
snigdhaAgarwal <snigdhaagarwal93@gmail.com>
2024-02-28 15:34:28 -0800

    Update setup.py
    For hydra-core to worj with bh-ai

--- setup.py
+++ setup.py
@@ -179,8 +179,8 @@ def do_setup(package_data):
         install_requires=[
             "cffi",
             "cython",
-            "hydra-core>=1.0.7,<1.1",
-            "omegaconf<2.1",
+            "hydra-core>=1.3.2,<2.0",
+            "omegaconf>=2.2,<2.4",
             "numpy>=1.21.3",
             "regex",
             "sacrebleu>=1.4.12",
